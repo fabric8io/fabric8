0bfd738b69e5f9ea3dd84d5a1e170642a5f7a4f7
Guillaume Nodet <gnodet@gmail.com>
2011-04-21 15:32:44 +0200

    Fix problem in the codec logic

--- fabric-dosgi/src/main/java/org/fusesource/fabric/dosgi/tcp/LengthPrefixedCodec.java
+++ fabric-dosgi/src/main/java/org/fusesource/fabric/dosgi/tcp/LengthPrefixedCodec.java
@@ -66,7 +66,7 @@ public class LengthPrefixedCodec implements ProtocolCodec {
     public BufferState flush() throws IOException {
         if (isEmpty() && next_write_size > 0) {
             if( next_write_buffers.size()==1 ) {
-                write_buffer = next_write_buffers.get(0).toByteBuffer();
+                write_buffer = next_write_buffers.remove(0).toByteBuffer();
             } else {
                 // consolidate the buffers into 1 big buffer to reduce
                 // the number of system calls we do.
