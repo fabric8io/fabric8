627f71a8a2aadce010696e00dc669b064ef5c62c
Hiram Chirino <hiram@hiramchirino.com>
2011-05-07 22:00:55 -0400

    Add equals/hash code + tostring for more case class like access.

--- fabric-monitor/src/main/scala/org/fusesource/fabric/monitor/api/DataSourceViewDTO.java
+++ fabric-monitor/src/main/scala/org/fusesource/fabric/monitor/api/DataSourceViewDTO.java
@@ -13,6 +13,7 @@ package org.fusesource.fabric.monitor.api;
 
 import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -44,4 +45,43 @@ public class DataSourceViewDTO {
     @XmlElement(name="data")
     public double data[] = new double[]{};
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DataSourceViewDTO that = (DataSourceViewDTO) o;
+
+        if (consolidation != null ? !consolidation.equals(that.consolidation) : that.consolidation != null)
+            return false;
+        if (!Arrays.equals(data, that.data)) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null)
+            return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (label != null ? !label.equals(that.label) : that.label != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (label != null ? label.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (consolidation != null ? consolidation.hashCode() : 0);
+        result = 31 * result + (data != null ? Arrays.hashCode(data) : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DataSourceViewDTO{" +
+                "consolidation='" + consolidation + '\'' +
+                ", id='" + id + '\'' +
+                ", label='" + label + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- fabric-monitor/src/main/scala/org/fusesource/fabric/monitor/api/MonitoredViewDTO.java
+++ fabric-monitor/src/main/scala/org/fusesource/fabric/monitor/api/MonitoredViewDTO.java
@@ -34,4 +34,38 @@ public class MonitoredViewDTO {
     @XmlElement(name="data_source")
     public List<DataSourceViewDTO> data_sources = new ArrayList<DataSourceViewDTO>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MonitoredViewDTO that = (MonitoredViewDTO) o;
+
+        if (end != that.end) return false;
+        if (step != that.step) return false;
+        if (start != that.start) return false;
+        if (data_sources != null ? !data_sources.equals(that.data_sources) : that.data_sources != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (start ^ (start >>> 32));
+        result = 31 * result + (int) (end ^ (end >>> 32));
+        result = 31 * result + (int) (step ^ (step >>> 32));
+        result = 31 * result + (data_sources != null ? data_sources.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MonitoredViewDTO{" +
+                "start=" + start +
+                ", end=" + end +
+                ", step=" + step +
+                ", data_sources=" + data_sources +
+                '}';
+    }
 }
