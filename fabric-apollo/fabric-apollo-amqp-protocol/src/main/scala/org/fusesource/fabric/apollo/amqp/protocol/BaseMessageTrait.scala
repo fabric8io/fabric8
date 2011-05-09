cb1cf0c843798dae0918e912ded00d2742299d58
Stan Lewis <slewis@fusesource.com>
2011-05-09 15:47:24 -0400

    Flesh out new message stuff more

--- fabric-apollo/fabric-apollo-amqp-protocol/src/main/scala/org/fusesource/fabric/apollo/amqp/protocol/BaseMessageTrait.scala
+++ fabric-apollo/fabric-apollo-amqp-protocol/src/main/scala/org/fusesource/fabric/apollo/amqp/protocol/BaseMessageTrait.scala
@@ -14,6 +14,12 @@ import org.fusesource.hawtbuf.Buffer
 import org.fusesource.fabric.apollo.amqp.api._
 import org.fusesource.fabric.apollo.amqp.codec.types._
 import org.fusesource.fabric.apollo.amqp.codec.types.TypeFactory._
+import java.util.LinkedList
+import org.fusesource.hawtdispatch._
+import collection.mutable.ListBuffer
+import org.fusesource.fabric.apollo.amqp.codec.CodecUtils
+
+// TODO - Assemble message from list of fragments
 
 /**
  *
@@ -26,6 +32,9 @@ trait BaseMessageTrait extends BaseMessage {
   var _delivery_annotations:Option[AmqpFields] = None
   var _application_properties:Option[AmqpFields] = None
   var _footer:Option[AmqpFooter] = None
+
+  val _onAckTasks:LinkedList[Runnable] = new LinkedList[Runnable]()
+  val _onSendTasks:LinkedList[Runnable] = new LinkedList[Runnable]()
 
   def header:AmqpHeader = _header.getOrElse {
     _header = Option(createAmqpHeader)
@@ -63,6 +72,49 @@ trait BaseMessageTrait extends BaseMessage {
   def getDeliveryAnnotations = delivery_annotations
   def getApplicationProperties = application_properties
   def getFooter = footer
+
+  def getOnSendTasks = _onAckTasks
+  def getOnAckTasks = _onSendTasks
+
+  def clearTasks(queue:DispatchQueue, tasks:LinkedList[Runnable]) = {
+    while (!tasks.isEmpty) {
+      queue << tasks.pop
+    }
+  }
+
+  def executeOnAck(queue:DispatchQueue) = clearTasks(queue, _onAckTasks)
+  def executeOnSend(queue:DispatchQueue) = clearTasks(queue, _onSendTasks)
+
+  def marshal_single_fragment_section(section_body:AmqpType[_, _], section_code:Long) = {
+    val rc = createAmqpFragment
+    rc.setPayload(new Buffer(CodecUtils.marshal(section_body)))
+    rc.setSectionCode(section_code)
+    // TODO - section offset, section number?
+    rc.setFirst(true)
+    rc.setLast(true)
+    rc
+  }
+
+  def maybe_add(l:ListBuffer[AmqpFragment], o:Option[AmqpType[_, _]], section_code:Long) = o.foreach((x) => l.append(marshal_single_fragment_section(x, section_code)))
+
+  def marshal_to_amqp_fragments = {
+    val rc = ListBuffer[AmqpFragment]()
+    // need to check section codes...
+    maybe_add(rc, _header, 0)
+    maybe_add(rc, _message_annotations, 1)
+    maybe_add(rc, _delivery_annotations, 2)
+    maybe_add(rc, _properties, 3)
+    maybe_add(rc, _application_properties, 4)
+    // add body here...
+
+    maybe_add(rc, _footer, 6)
+
+    rc.toList
+  }
+
+
+  def marshal_application_data_section
+
 }
 
 trait GenericMessageTrait[T] extends GenericMessage[T] {
@@ -72,15 +124,28 @@ trait GenericMessageTrait[T] extends GenericMessage[T] {
   def setBody(body:T) = this.body = body
 }
 
-class DataMessageImpl extends DataMessage with BaseMessageTrait with GenericMessageTrait[Buffer]
+class DataMessageImpl extends DataMessage with BaseMessageTrait with GenericMessageTrait[Buffer] {
+  override def marshal_application_data_section = {
+    // TODO
+  }
+}
 
+// Same as above but body section code is different
 class AmqpDataMessageImpl extends DataMessageImpl with BaseMessageTrait with GenericMessageTrait[Buffer]
 
 class AmqpMapMessageImpl extends AmqpMapMessage with BaseMessageTrait with GenericMessageTrait[AmqpFields] {
   body = createAmqpFields
+
+  override def marshal_application_data_section = {
+    // TODO
+  }
 }
 
 class AmqpListMessageImpl extends AmqpListMessage with BaseMessageTrait with GenericMessageTrait[AmqpList] {
   body = createAmqpList
+
+  override def marshal_application_data_section = {
+    // TODO
+  }
 }
 
