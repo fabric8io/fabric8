3d5f013c0635613683dcc15fec00b72a66873b3d
Lukasz Dywicki <luke@code-house.org>
2011-05-11 13:56:16 +0200

    Jmx domain to zookeeper synchronization.

--- fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/KarafAgentRegistration.java
+++ fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/KarafAgentRegistration.java
@@ -14,22 +14,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.Stat;
 import org.linkedin.zookeeper.client.IZKClient;
 import org.linkedin.zookeeper.client.LifecycleListener;
+import org.osgi.framework.ServiceException;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.management.*;
 
 import static org.fusesource.fabric.zookeeper.ZkPath.*;
 
-public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware {
+public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware, NotificationListener {
+
+    private transient Logger logger = LoggerFactory.getLogger(KarafAgentRegistration.class);
 
     private ConfigurationAdmin configurationAdmin;
     private IZKClient zooKeeper;
+    private MBeanServer mbeanServer;
 
     public IZKClient getZooKeeper() {
         return zooKeeper;
@@ -47,6 +57,10 @@ public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware
         this.configurationAdmin = configurationAdmin;
     }
 
+    public void setMbeanServer(MBeanServer mbeanServer) {
+        this.mbeanServer = mbeanServer;
+    }
+
     public void onConnected() {
         try {
             String name = System.getProperty("karaf.name");
@@ -60,6 +74,13 @@ public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware
             } else {
                 zooKeeper.createWithParents(nodeAlive, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
             }
+
+            String domainsNode = AGENT_DOMAINS.getPath(name);
+            stat = zooKeeper.exists(domainsNode);
+            if (stat != null) {
+                zooKeeper.deleteWithChildren(domainsNode);
+            }
+
             String jmxUrl = getJmxUrl();
             if (jmxUrl != null) {
                 zooKeeper.createOrSetWithParents(AGENT_JMX.getPath(name), getJmxUrl(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
@@ -70,6 +91,12 @@ public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware
             }
             zooKeeper.createOrSetWithParents(AGENT_IP.getPath(name), getLocalHostAddress(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             zooKeeper.createOrSetWithParents(AGENT_ROOT.getPath(name), getRootName(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+            for (String domain : mbeanServer.getDomains()) {
+                zooKeeper.createOrSetWithParents(AGENT_DOMAIN.getPath(name, domain), null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+
+            mbeanServer.addNotificationListener(new ObjectName("JMImplementation:type=MBeanServerDelegate"), this, null, name);
         } catch (Exception e) {
             // TODO
             e.printStackTrace();
@@ -84,6 +111,7 @@ public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware
         }
         File f = new File(home, "etc/system.properties");
         InputStream is = new FileInputStream(f);
+
         try {
             Properties p = new Properties();
             p.load(is);
@@ -130,5 +158,37 @@ public class KarafAgentRegistration implements LifecycleListener, ZooKeeperAware
     }
 
     public void onDisconnected() {
+        try {
+            mbeanServer.removeNotificationListener(new ObjectName("JMImplementation:type=MBeanServerDelegate"), this);
+        } catch (ServiceException e) {
+            logger.trace("Mbean server is no longer available", e);
+        } catch (Exception e) {
+            logger.warn("An error occured during disconnecting to zookeeper", e);
+        }
+    }
+
+    @Override
+    public void handleNotification(Notification notif, Object o) {
+        // handle mbeans registration and de-registration events
+        if (notif instanceof MBeanServerNotification) {
+            MBeanServerNotification notification = (MBeanServerNotification) notif;
+            String domain = notification.getMBeanName().getDomain();
+            String path = AGENT_DOMAIN.getPath((String) o, domain);
+
+            try {
+                if (MBeanServerNotification.REGISTRATION_NOTIFICATION.equals(notification.getType())) {
+                    if (zooKeeper.exists(path) == null) {
+                        zooKeeper.create(path, "", ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                    }
+                } else if (MBeanServerNotification.UNREGISTRATION_NOTIFICATION.equals(notification.getType())) {
+                    if (Arrays.binarySearch(mbeanServer.getDomains(), domain) == -1) {
+                        // domain is no present any more
+                        zooKeeper.delete(path);
+                    }
+                }
+            } catch (Exception e) {
+                logger.warn("Exception while jmx domain synchronization", e);
+            }
+        }
     }
 }
