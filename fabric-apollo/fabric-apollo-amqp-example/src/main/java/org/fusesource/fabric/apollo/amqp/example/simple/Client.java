a0c29bd56f90c6bef8914b15a9d46a1eb47cf045
Stan Lewis <slewis@fusesource.com>
2011-05-13 09:03:33 -0400

    Add example classes, tidy up pom

---
+++ fabric-apollo/fabric-apollo-amqp-example/src/main/java/org/fusesource/fabric/apollo/amqp/example/simple/Client.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2010-2011, FuseSource Corp.  All rights reserved.
+ *
+ *     http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * CDDL license a copy of which has been included with this distribution
+ * in the license.txt file.
+ */
+
+package org.fusesource.fabric.apollo.amqp.example.simple;
+
+import org.fusesource.fabric.apollo.amqp.codec.AmqpDefinitions;
+
+/**
+ *
+ */
+public class Client {
+
+    protected int port = AmqpDefinitions.PORT;
+    protected String hostname = "localhost";
+}
