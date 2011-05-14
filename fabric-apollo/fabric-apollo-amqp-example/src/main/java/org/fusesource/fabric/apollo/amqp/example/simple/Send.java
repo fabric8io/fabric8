a863acf0e9f246740ac42030af5205522c5774fb
Stan Lewis <slewis@fusesource.com>
2011-05-14 08:14:25 -0400

    Sender example works

--- fabric-apollo/fabric-apollo-amqp-example/src/main/java/org/fusesource/fabric/apollo/amqp/example/simple/Send.java
+++ fabric-apollo/fabric-apollo-amqp-example/src/main/java/org/fusesource/fabric/apollo/amqp/example/simple/Send.java
@@ -10,10 +10,8 @@
 
 package org.fusesource.fabric.apollo.amqp.example.simple;
 
-import org.fusesource.fabric.apollo.amqp.api.AmqpConnectionFactory;
-import org.fusesource.fabric.apollo.amqp.api.Connection;
-import org.fusesource.fabric.apollo.amqp.api.Sender;
-import org.fusesource.fabric.apollo.amqp.api.Session;
+import org.fusesource.fabric.apollo.amqp.api.*;
+import org.fusesource.hawtbuf.Buffer;
 
 /**
  *
@@ -38,19 +36,60 @@ public class Send extends Client {
         });
         connection.connect(getConnectionURI(), new Runnable() {
             public void run() {
-                final Session session = connection.createSession();
-                final Sender sender = session.createSender();
-                sender.setAddress(address);
+                onConnect(connection);
+            }
+        });
+        try {
+            System.out.println("Sending messages");
+            exit_latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-                sender.attach(new Runnable() {
+    public void onConnect(final Connection connection) {
+        final Session session = connection.createSession();
+        final Sender sender = session.createSender();
+        sender.setOnDetach(new Runnable() {
+            public void run() {
+                session.end(new Runnable() {
                     public void run() {
-
-
+                        connection.close();
                     }
                 });
             }
         });
+        sender.setAddress(address);
+        sender.attach(new Runnable() {
+            public void run() {
+                onAttach(connection, session, sender);
+            }
+        });
+    }
 
+    public void onAttach(final Connection connection, final Session session, final Sender sender) {
+        sendMessage(session, sender, 0);
+    }
+
+    public void sendMessage(final Session session, final Sender sender, final int current_count) {
+        Message message = session.createMessage();
+        message.addBodyPart(Buffer.ascii(message_prefix + current_count));
+
+        if (current_count >= count) {
+            message.onAck(new Runnable() {
+                public void run() {
+                    sender.detach();
+                }
+            });
+        } else {
+            message.onAck(new Runnable() {
+                public void run() {
+                    sendMessage(session, sender, current_count + 1);
+                }
+            });
+        }
+        System.out.println("Sending message " + (current_count + 1));
+        sender.put(message);
     }
 
     @Override
