fafd817ba0d2ca39b5c929a44c2613cdd3c558bb
Stan Lewis <slewis@fusesource.com>
2011-06-13 17:04:13 -0400

    Add getters/setters, read/write/encode/decode base interface

---
+++ fabric-apollo/fabric-apollo-amqp-codec2/src/main/java/org/fusesource/fabric/apollo/amqp/codec/interfaces/AmqpType.java
@@ -0,0 +1,16 @@
+package org.fusesource.fabric.apollo.amqp.codec.interfaces;
+
+import org.fusesource.hawtbuf.Buffer;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+
+public interface AmqpType {
+
+    public void write(DataOutput out) throws Exception;
+    public void read(DataInput in) throws Exception;
+
+    public void encodeTo(Buffer buffer, int offset) throws Exception;
+    public void decodeFrom(Buffer buffer, int offset) throws Exception;
+
+}
--- fabric-apollo/fabric-apollo-amqp-generator/src/main/java/org/fusesource/fabric/apollo/amqp/generator/DescribedTypeGenerator.java
+++ fabric-apollo/fabric-apollo-amqp-generator/src/main/java/org/fusesource/fabric/apollo/amqp/generator/DescribedTypeGenerator.java
@@ -4,6 +4,7 @@ import com.sun.codemodel.*;
 import org.fusesource.fabric.apollo.amqp.jaxb.schema.Descriptor;
 import org.fusesource.fabric.apollo.amqp.jaxb.schema.Field;
 import org.fusesource.fabric.apollo.amqp.jaxb.schema.Type;
+import org.fusesource.hawtbuf.Buffer;
 
 import java.util.Set;
 
@@ -23,6 +24,25 @@ public class DescribedTypeGenerator {
 
             String className = generator.getDescribedJavaClass().get(key);
             JDefinedClass cls = generator.getCm()._getClass(className);
+            cls._implements(generator.getCm().ref("org.fusesource.fabric.apollo.amqp.codec.interfaces.AmqpType"));
+
+            JMethod write = cls.method(JMod.PUBLIC, generator.getCm().VOID, "write");
+            write._throws(java.lang.Exception.class);
+            write.param(java.io.DataOutput.class, "out");
+
+            JMethod read = cls.method(JMod.PUBLIC, generator.getCm().VOID, "read");
+            read._throws(java.lang.Exception.class);
+            read.param(java.io.DataInput.class, "in");
+
+            JMethod encodeTo = cls.method(JMod.PUBLIC, generator.getCm().VOID, "encodeTo");
+            encodeTo._throws(java.lang.Exception.class);
+            encodeTo.param(Buffer.class, "buffer");
+            encodeTo.param(generator.getCm().INT, "offset");
+
+            JMethod decodeFrom = cls.method(JMod.PUBLIC, generator.getCm().VOID, "decodeFrom");
+            decodeFrom._throws(java.lang.Exception.class);
+            decodeFrom.param(Buffer.class, "buffer");
+            decodeFrom.param(generator.getCm().INT, "offset");
 
             Type type = generator.getDescribed().get(key);
 
@@ -32,7 +52,6 @@ public class DescribedTypeGenerator {
             for ( Object obj : type.getEncodingOrDescriptorOrFieldOrChoiceOrDoc() ) {
                 if ( obj instanceof Field ) {
                     Field field = (Field) obj;
-                    Log.info("Field name=%s type=%s", field.getName(), field.getType());
                     String fieldType = field.getType();
                     String fieldName = sanitize(field.getName());
 
@@ -51,8 +70,6 @@ public class DescribedTypeGenerator {
                         fieldType = generator.getRestrictedMapping().get(field.getType());
                     }
 
-
-
                     if ( fieldType != null ) {
                         boolean array = false;
                         if ( field.getMultiple() != null && field.getMultiple().equals("true") ) {
@@ -63,11 +80,6 @@ public class DescribedTypeGenerator {
                         JClass c = null;
                         if ( clazz == null ) {
                             c = generator.getCm()._getClass(fieldType);
-                            /*
-                            if ( c == null ) {
-                                c = generator.getCm()._class(fieldType);
-                            }
-                            */
                         } else {
                             c = generator.getCm().ref(clazz.getName());
                         }
@@ -83,6 +95,13 @@ public class DescribedTypeGenerator {
                             doc += " - " + field.getLabel();
                         }
                         fieldVar.javadoc().add(doc);
+
+                        JMethod getter = cls.method(JMod.PUBLIC, fieldVar.type(), "get" + toJavaClassName(fieldName));
+                        getter.body()._return(JExpr._this().ref(fieldVar));
+
+                        JMethod setter = cls.method(JMod.PUBLIC, generator.getCm().VOID, "set" + toJavaClassName(fieldName));
+                        JVar param = setter.param(fieldVar.type(), fieldName);
+                        setter.body().assign(JExpr._this().ref(fieldVar), param);
 
                     } else {
                         Log.info("Skipping field %s, type not found", field.getName());
@@ -92,99 +111,6 @@ public class DescribedTypeGenerator {
         }
 
     }
-        /*
-        for ( Type type : generator.getComposites() ) {
-            //String sectionPackage = sanitize(sections.get(type.getName()));
-            String name = toJavaClassName(type.getName());
-            //name = packagePrefix + "." + sectionPackage + "." + name;
-            name = generator.getPackagePrefix() + "." + generator.getTypes() + "." + name;
-
-            JDefinedClass cls = generator.getCm()._getClass(name);
-            if ( cls == null ) {
-                cls = generator.getCm()._class(name);
-            }
-
-            if ( type.getProvides() != null && !type.getProvides().equals(type.getName()) ) {
-                cls._implements(generator.getCm().ref(generator.getPackagePrefix() + "." + generator.getInterfaces() + "." + toJavaClassName(type.getProvides())));
-            }
-
-            Log.info("");
-            Log.info("Generating %s", cls.binaryName());
-
-            for ( Object obj : type.getEncodingOrDescriptorOrFieldOrChoiceOrDoc() ) {
-                if ( obj instanceof Descriptor ) {
-                    Descriptor desc = (Descriptor) obj;
-                    int mods = JMod.PUBLIC | JMod.STATIC | JMod.FINAL;
-                    cls.field(mods, String.class, "SYMBOLIC_ID", JExpr.lit(desc.getName()));
-
-                    String code = desc.getCode();
-                    String category = code.split(":")[0];
-                    String descriptorId = code.split(":")[1];
-
-                    cls.field(mods, long.class, "CATEGORY", JExpr.lit(Integer.parseInt(category.substring(2), 16)));
-                    cls.field(mods, long.class, "DESCRIPTOR_ID", JExpr.lit(Integer.parseInt(descriptorId.substring(2), 16)));
-                    cls.field(mods, long.class, "NUMERIC_ID", JExpr.direct("CATEGORY << 32 | DESCRIPTOR_ID"));
-
-                } else if ( obj instanceof Field ) {
-                    Field field = (Field) obj;
-                    Log.info("Field name=%s type=%s", field.getName(), field.getType());
-                    String fieldType = field.getType();
-                    String fieldName = sanitize(field.getName());
-
-                    if ( fieldType.equals("*") && field.getRequires() != null ) {
-                        fieldType = generator.getPackagePrefix() + "." + generator.getInterfaces() + "." + toJavaClassName(field.getRequires());
-                        Log.info("Trying required type %s", fieldType);
-                    } else {
-                        while (!generator.getMapping().containsKey(fieldType)) {
-                            fieldType = generator.getRestrictedMapping().get(fieldType);
-                            if ( fieldType == null ) {
-                                break;
-                            }
-                            Log.info("Trying field type %s for field %s", fieldType, fieldName);
-                        }
-                    }
-
-                    if ( fieldType == null ) {
-                        fieldType = generator.getCompositeMapping().get(field.getType());
-                    }
-
-                    if ( fieldType != null ) {
-
-                        boolean array = false;
-                        if ( field.getMultiple() != null && field.getMultiple().equals("true") ) {
-                            array = true;
-                        }
-
-                        Class clazz = generator.getMapping().get(fieldType);
-                        JClass c = null;
-                        if ( clazz == null ) {
-                            c = generator.getCm()._getClass(fieldType);
-                            if ( c == null ) {
-                                c = generator.getCm()._class(fieldType);
-                            }
-                        } else {
-                            c = generator.getCm().ref(clazz.getName());
-                        }
-                        if ( array ) {
-                            c = c.array();
-                        }
-                        Log.info("%s %s", c.binaryName(), fieldName);
-                        JFieldVar fieldVar = cls.field(JMod.PROTECTED, c, fieldName);
-
-                        String doc = field.getName() + ":" + field.getType();
-
-                        if ( field.getLabel() != null ) {
-                            doc += " - " + field.getLabel();
-                        }
-                        fieldVar.javadoc().add(doc);
-
-                    } else {
-                        Log.info("Skipping field %s, type not found", field.getName());
-                    }
-                }
-            }
-        }
-    */
 
     void createDescribedClasses(Generator generator) throws JClassAlreadyExistsException {
         Set<String> keys = generator.getDescribed().keySet();
