2f7f38931603b66d7c3c25cfd94cecb27de3812f
Stan Lewis <slewis@fusesource.com>
2011-06-15 16:56:49 -0400

    Implement encoding picker

--- fabric-apollo/fabric-apollo-amqp-codec2/src/main/java/org/fusesource/fabric/apollo/amqp/codec/marshaller/AmqpEncodingPicker.java
+++ fabric-apollo/fabric-apollo-amqp-codec2/src/main/java/org/fusesource/fabric/apollo/amqp/codec/marshaller/AmqpEncodingPicker.java
@@ -11,6 +11,7 @@
 package org.fusesource.fabric.apollo.amqp.codec.marshaller;
 
 import org.fusesource.fabric.apollo.amqp.codec.interfaces.EncodingPicker;
+import org.fusesource.fabric.apollo.amqp.codec.types.*;
 import org.fusesource.hawtbuf.Buffer;
 
 import java.math.BigDecimal;
@@ -31,94 +32,195 @@ public class AmqpEncodingPicker implements EncodingPicker {
     }
 
     public byte chooseArrayEncoding(Object[] value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPArray.ARRAY_ARRAY32_CODE;
     }
 
     public byte chooseBinaryEncoding(Buffer value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value.length() > 255) {
+            return AMQPBinary.BINARY_VBIN32_CODE;
+        } else {
+            return AMQPBinary.BINARY_VBIN8_CODE;
+        }
     }
 
     public byte chooseBooleanEncoding(Boolean value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value == Boolean.TRUE) {
+            return AMQPBoolean.BOOLEAN_TRUE_CODE;
+        } else {
+            return AMQPBoolean.BOOLEAN_FALSE_CODE;
+        }
     }
 
     public byte chooseByteEncoding(Byte value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPByte.BYTE_CODE;
     }
 
     public byte chooseCharEncoding(Character value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPChar.CHAR_UTF32_CODE;
     }
 
     public byte chooseDecimal128Encoding(BigDecimal value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPDecimal128.DECIMAL128_IEEE_754_CODE;
     }
 
     public byte chooseDecimal32Encoding(BigDecimal value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPDecimal32.DECIMAL32_IEEE_754_CODE;
     }
 
     public byte chooseDecimal64Encoding(BigDecimal value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPDecimal64.DECIMAL64_IEEE_754_CODE;
     }
 
     public byte chooseDoubleEncoding(Double value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPDouble.DOUBLE_IEEE_754_CODE;
     }
 
     public byte chooseFloatEncoding(Float value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPFloat.FLOAT_IEEE_754_CODE;
     }
 
     public byte chooseIntEncoding(Integer value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value < 128 && value > -127) {
+            return AMQPInt.INT_SMALLINT_CODE;
+        }
+        return AMQPInt.INT_CODE;
     }
 
     public byte chooseListEncoding(List value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPList.LIST_LIST32_CODE;
     }
 
     public byte chooseLongEncoding(Long value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value < 128 && value > -127) {
+            return AMQPLong.LONG_SMALLLONG_CODE;
+        }
+        return AMQPLong.LONG_CODE;
     }
 
     public byte chooseMapEncoding(Map value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPMap.MAP_MAP32_CODE;
     }
 
     public byte chooseShortEncoding(Short value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPShort.SHORT_CODE;
     }
 
     public byte chooseStringEncoding(String value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value.length() < 255) {
+            return AMQPString.STRING_STR8_UTF8_CODE;
+        }
+        return AMQPString.STRING_STR32_UTF8_CODE;
     }
 
     public byte chooseSymbolEncoding(Buffer value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value.length()  < 255) {
+            return AMQPSymbol.SYMBOL_SYM8_CODE;
+        }
+        return AMQPSymbol.SYMBOL_SYM32_CODE;
     }
 
     public byte chooseTimestampEncoding(Date value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPTimestamp.TIMESTAMP_MS64_CODE;
     }
 
     public byte chooseUByteEncoding(Byte value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPUByte.UBYTE_CODE;
     }
 
     public byte chooseUIntEncoding(Integer value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value == 0) {
+            return AMQPUInt.UINT_UINT0_CODE;
+        }
+        if (value < 255 && value > 0) {
+            return AMQPUInt.UINT_SMALLUINT_CODE;
+        }
+        return AMQPUInt.UINT_CODE;
     }
 
     public byte chooseULongEncoding(Long value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        if (value == 0) {
+            return AMQPULong.ULONG_ULONG0_CODE;
+        }
+        if (value < 255 && value > 0) {
+            return AMQPULong.ULONG_SMALLULONG_CODE;
+        }
+        return AMQPULong.ULONG_CODE;
     }
 
     public byte chooseUShortEncoding(Short value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPUShort.USHORT_CODE;
     }
 
     public byte chooseUUIDEncoding(UUID value) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (value == null) {
+            return TypeRegistry.NULL_FORMAT_CODE;
+        }
+        return AMQPUUID.UUID_CODE;
     }
 }
