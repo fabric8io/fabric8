1d6e5b498dd36dcd2894fc5f6a40b0b28107a40a
Stan Lewis <slewis@fusesource.com>
2011-07-15 09:34:35 -0400

    Add simple helper function to encode AMQP types

---
+++ fabric-apollo/fabric-apollo-amqp/fabric-apollo-amqp-codec/src/main/java/org/fusesource/fabric/apollo/amqp/codec/Codec.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2010-2011, FuseSource Corp.  All rights reserved.
+ *
+ *     http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * CDDL license a copy of which has been included with this distribution
+ * in the license.txt file.
+ */
+
+package org.fusesource.fabric.apollo.amqp.codec;
+
+import org.fusesource.fabric.apollo.amqp.codec.interfaces.AMQPType;
+import org.fusesource.hawtbuf.Buffer;
+import org.fusesource.hawtbuf.DataByteArrayOutputStream;
+
+/**
+ *
+ */
+public class Codec {
+
+    public static Buffer toBuffer(AMQPType type) throws Exception {
+        DataByteArrayOutputStream out = new DataByteArrayOutputStream((int)type.size());
+        type.write(out);
+        return out.toBuffer();
+    }
+}
