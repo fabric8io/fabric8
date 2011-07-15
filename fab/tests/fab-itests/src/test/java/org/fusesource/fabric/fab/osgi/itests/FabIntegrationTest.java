ed8d8dea3705217746b81461774b647ef9bb59d7
James Strachan <james.strachan@gmail.com>
2011-07-15 10:52:27 +0100

    refactored the integration tests so they are easier to reuse for different FABs - plus added FAB test for shared osgi blueprint stuff for camel

--- fab/tests/fab-itests/src/test/java/org/fusesource/fabric/fab/osgi/itests/FabIntegrationTest.java
+++ fab/tests/fab-itests/src/test/java/org/fusesource/fabric/fab/osgi/itests/FabIntegrationTest.java
@@ -21,79 +21,18 @@ import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
 
 @RunWith(JUnit4TestRunner.class)
-public class FabIntegrationTest extends IntegrationTestSupport {
+public class FabIntegrationTest extends FabIntegrationTestSupport {
 
-    @Test
-    public void testRun() throws Exception {
-        Thread.sleep(10000);
-
-        // Make sure the command services are available
-        assertNotNull(getOsgiService(BlueprintContainer.class, "osgi.blueprint.container.symbolicname=org.apache.karaf.shell.obr", 20000));
-        assertNotNull(getOsgiService(BlueprintContainer.class, "osgi.blueprint.container.symbolicname=org.apache.karaf.shell.wrapper", 20000));
-
-        CommandProcessor cp = getOsgiService(CommandProcessor.class);
-        CommandSession cs = cp.createSession(System.in, System.out, System.err);
-
-        cs.execute("osgi:list");
-
-        assertStartBundle("org.apache.karaf.shell.log");
-
-        assertStartBundle("org.fusesource.fabric.fab.fabric-fab-osgi");
+    @Override
+    protected void doInstallFabricBundles() throws Exception {
+        commandSession.execute("osgi:install fab:mvn:org.fusesource.fabric.fab.tests/fab-sample-camel-noshare");
 
         Thread.sleep(1000);
-
-        cs.execute("osgi:install fab:mvn:org.fusesource.fabric.fab.tests/fab-sample-camel-noshare");
-
-        Thread.sleep(1000);
-
-        cs.execute("osgi:list");
 
         assertStartBundle("org.fusesource.fabric.fab.tests.fab-sample-camel-noshare");
-
-        Thread.sleep(3000);
-
-        stopBundles();
-
-        cs.close();
     }
 
 
-    @Configuration
-    public static Option[] configuration() throws Exception {
-        Option[] options = combine(
-                // Default karaf environment
-                Helper.getDefaultOptions(
-                        // this is how you set the default log level when using pax logging (logProfile)
-                        //Helper.setLogLevel("TRACE")
-                        Helper.setLogLevel("INFO")
-                ),
-
-                // add karaf features
-                Helper.loadKarafStandardFeatures("obr", "wrapper"),
-
-                // add fab features
-                scanFeatures(
-                        maven().groupId("org.fusesource.fabric").artifactId("fabric-distro").type("xml").classifier("features").versionAsInProject(),
-                        "fabric-fab"
-                ),
-
-                workingDirectory("target/paxrunner/core/"),
-
-                waitForFrameworkStartup(),
-
-                // TODO Test on both equinox and felix
-                // TODO: pax-exam does not support the latest felix version :-(
-                // TODO: so we use the higher supported which should be the same
-                // TODO: as the one specified in itests/dependencies/pom.xml
-                //equinox(), felix().version("3.0.2")
-                felix().version("3.0.2"),
-
-                felixProvisionalApis()
-        );
-        // Stop the shell log bundle
-        //Helper.findMaven(options, "org.apache.karaf.shell", "org.apache.karaf.shell.log").noStart();
-        return options;
-    }
 
 
 }
---
+++ fab/tests/fab-itests/src/test/java/org/fusesource/fabric/fab/osgi/itests/FabIntegrationTestSupport.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (C) 2010, FuseSource Corp.  All rights reserved.
+ * http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * AGPL license a copy of which has been included with this distribution
+ * in the license.txt file.
+ */
+package org.fusesource.fabric.fab.osgi.itests;
+
+import org.apache.felix.service.command.CommandProcessor;
+import org.apache.felix.service.command.CommandSession;
+import org.apache.karaf.testing.Helper;
+import org.junit.Test;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.service.blueprint.container.BlueprintContainer;
+
+import static org.apache.karaf.testing.Helper.felixProvisionalApis;
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
+
+/**
+ */
+public abstract class FabIntegrationTestSupport extends IntegrationTestSupport {
+    protected CommandProcessor commandProcessor;
+    protected CommandSession commandSession;
+
+    @Test
+    public void testInstallFabricBundles() throws Exception {
+        Thread.sleep(10000);
+
+        // Make sure the command services are available
+        assertNotNull(getOsgiService(BlueprintContainer.class, "osgi.blueprint.container.symbolicname=org.apache.karaf.shell.obr", 20000));
+        assertNotNull(getOsgiService(BlueprintContainer.class, "osgi.blueprint.container.symbolicname=org.apache.karaf.shell.wrapper", 20000));
+
+        commandProcessor = getOsgiService(CommandProcessor.class);
+        commandSession = commandProcessor.createSession(System.in, System.out, System.err);
+
+        commandSession.execute("osgi:list");
+        assertStartBundle("org.apache.karaf.shell.log");
+
+        assertStartBundle("org.fusesource.fabric.fab.fabric-fab-osgi");
+        Thread.sleep(1000);
+
+        doInstallFabricBundles();
+
+        Thread.sleep(1000);
+
+        commandSession.execute("osgi:list");
+
+        Thread.sleep(2000);
+
+        stopBundles();
+
+        commandSession.close();
+    }
+
+    protected abstract void doInstallFabricBundles() throws Exception;
+
+
+    @Configuration
+    public static Option[] configuration() throws Exception {
+        Option[] options = combine(
+                // Default karaf environment
+                Helper.getDefaultOptions(
+                        // this is how you set the default log level when using pax logging (logProfile)
+                        //Helper.setLogLevel("TRACE")
+                        Helper.setLogLevel("INFO")
+                ),
+
+                // add karaf features
+                Helper.loadKarafStandardFeatures("obr", "wrapper"),
+
+                // add fab features
+                scanFeatures(
+                        maven().groupId("org.fusesource.fabric").artifactId("fabric-distro").type("xml").classifier("features").versionAsInProject(),
+                        "fabric-fab"
+                ),
+
+                workingDirectory("target/paxrunner/core/"),
+
+                waitForFrameworkStartup(),
+
+                // TODO Test on both equinox and felix
+                // TODO: pax-exam does not support the latest felix version :-(
+                // TODO: so we use the higher supported which should be the same
+                // TODO: as the one specified in itests/dependencies/pom.xml
+                //equinox(), felix().version("3.0.2")
+                felix().version("3.0.2"),
+
+                felixProvisionalApis()
+        );
+        // Stop the shell log bundle
+        //Helper.findMaven(options, "org.apache.karaf.shell", "org.apache.karaf.shell.log").noStart();
+        return options;
+    }
+}
