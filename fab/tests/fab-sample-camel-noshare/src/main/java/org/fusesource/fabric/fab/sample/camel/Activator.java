6a8dfa7621da7256f700a4f578c04c18d82d9c63
James Strachan <james.strachan@gmail.com>
2011-07-15 09:44:05 +0100

    made the sample a little simpler

--- fab/tests/fab-sample-camel-noshare/src/main/java/org/fusesource/fabric/fab/sample/camel/Activator.java
+++ fab/tests/fab-sample-camel-noshare/src/main/java/org/fusesource/fabric/fab/sample/camel/Activator.java
@@ -8,7 +8,6 @@
  */
 package org.fusesource.fabric.fab.sample.camel;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.converter.CorePackageScanClassResolver;
@@ -18,20 +17,10 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
-
     private DefaultCamelContext camelContext;
 
     public void start(BundleContext context) throws Exception {
-        // inside OSGi the thread context class loader isn't set to the right class loader so lets try that now
-        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
-        try {
-            ClassLoader classLoader = DefaultCamelContext.class.getClassLoader();
-            Thread.currentThread().setContextClassLoader(classLoader);
-            System.out.println("Setting the context class loader to: " + classLoader);
-            startCamel();
-        } finally {
-            Thread.currentThread().setContextClassLoader(oldClassLoader);
-        }
+        startCamel();
     }
 
     public void startCamel() throws Exception {
@@ -39,6 +28,7 @@ public class Activator implements BundleActivator {
         camelContext = new DefaultCamelContext();
 
         // TODO package scannning doesn't work in most containers so lets work around it.
+        // until Camel supports non-package scanning by default...
 
         CorePackageScanClassResolver corePackageScanClassResolver = new CorePackageScanClassResolver();
         TypeConverterRegistry typeConverterRegistry = new DefaultTypeConverter(new CorePackageScanClassResolver(),
