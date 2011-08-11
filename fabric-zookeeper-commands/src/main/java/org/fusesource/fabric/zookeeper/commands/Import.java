9b852b75e32c387d1229f47059b6a120bfc066e9
Stan Lewis <slewis@fusesource.com>
2011-08-11 15:32:50 -0400

    Import is in place, just need to add regex/filtering

--- fabric-zookeeper-commands/src/main/java/org/fusesource/fabric/zookeeper/commands/Import.java
+++ fabric-zookeeper-commands/src/main/java/org/fusesource/fabric/zookeeper/commands/Import.java
@@ -8,26 +8,109 @@
  */
 package org.fusesource.fabric.zookeeper.commands;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.Properties;
-
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
 
 @Command(name = "import", scope = "zk", description = "Import data into zookeeper")
 public class Import extends ZooKeeperCommandSupport {
 
-    @Argument(description = "URL of the file to load")
-    String url;
+    @Argument(description = "Location of the file or filesystem to load")
+    String source = "." + File.separator + "import";
+
+    @Option(name="-t", aliases={"--target"}, description="Target location in ZooKeeper tree to import to")
+    String target = "/";
+
+    @Option(name="-props", aliases={"--properties"}, description="Argument is URL pointing to a properties file")
+    boolean properties = false;
+
+    @Option(name="-fs", aliases={"--filesystem"}, description="Argument is the top level directory of a local filesystem tree")
+    boolean filesystem = true;
 
     @Override
     protected Object doExecute() throws Exception {
-        InputStream in = new BufferedInputStream(new URL(url).openStream());
+        if (properties == true) {
+            filesystem = false;
+        }
+        if (filesystem == true) {
+            properties = false;
+        }
+        if (properties) {
+            readPropertiesFile();
+        }
+        if (filesystem) {
+            readFileSystem();
+        }
+        System.out.println("Successfully imported settings from " + source);
+        return null;
+    }
+
+    private String stripPath(String path) {
+        String strs[] = path.split(source);
+        if (strs.length == 0) {
+            return "";
+        }
+        return strs[strs.length - 1].substring(0, strs[1].length() - ".cfg".length());
+    }
+
+    private String buildZKPath(File parent, File current) {
+        String rc = "";
+        if (current != null && !parent.equals(current)) {
+            rc = buildZKPath(parent, current.getParentFile()) + "/" + current.getName();
+        }
+        return rc;
+    }
+
+    private void getCandidates(File parent, File current, Map<String, String> settings) throws Exception {
+        if (current.isDirectory()) {
+            for (File child : current.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+                    if (file.isDirectory() || file.getName().endsWith(".cfg")) {
+                        return true;
+                    }
+                    return false;
+                }
+            })) {
+                getCandidates(parent, child, settings);
+            }
+            String p = buildZKPath(parent, current);
+            settings.put(p, null);
+        } else {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(current));
+            byte[] contents = new byte[in.available()];
+            in.read(contents);
+            in.close();
+            String p = buildZKPath(parent, current);
+            if (p.endsWith(".cfg")) {
+                p = p.substring(0, p.length() - ".cfg".length());
+            }
+            settings.put(p, new String(contents));
+        }
+
+    }
+
+    private void readFileSystem() throws Exception {
+        Map<String, String> settings = new TreeMap<String, String>();
+        File s = new File(source);
+        getCandidates(s, s, settings);
+
+        for(String key : settings.keySet()) {
+            String data = settings.get(key);
+            getZooKeeper().createOrSetWithParents(key, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+
+    }
+
+    private void readPropertiesFile() throws IOException, InterruptedException, KeeperException {
+        InputStream in = new BufferedInputStream(new URL(source).openStream());
         try {
             Properties props = new Properties();
             props.load(in);
@@ -37,12 +120,15 @@ public class Import extends ZooKeeperCommandSupport {
                 if (value != null && value.isEmpty()) {
                     value = null;
                 }
+                if (!name.startsWith("/")) {
+                    name = "/" + name;
+                }
+                name = target + name;
                 getZooKeeper().createOrSetWithParents(name, value, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
         } finally {
             in.close();
         }
-        return null;
     }
 
 }
