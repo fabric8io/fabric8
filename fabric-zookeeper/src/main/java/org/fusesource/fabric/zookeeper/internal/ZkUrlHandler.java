505cfaf78221743a7d628a6069f3d6831a3a6691
Hiram Chirino <hiram@hiramchirino.com>
2011-09-23 10:20:30 -0400

    Allow the ZK url handler to dig into .json and .properties files to extract nested values.  Configured via the URL fragment.

--- fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/ZkUrlHandler.java
+++ fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/ZkUrlHandler.java
@@ -15,7 +15,12 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Properties;
 
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.linkedin.zookeeper.client.IZKClient;
 import org.osgi.service.url.AbstractURLStreamHandlerService;
 import org.slf4j.Logger;
@@ -53,22 +58,28 @@ public class ZkUrlHandler extends AbstractURLStreamHandlerService {
      */
     @Override
     public URLConnection openConnection(URL url) throws IOException {
-        if (url.getPath() == null || url.getPath().trim().length() == 0) {
-            throw new MalformedURLException("Path can not be null or empty. Syntax: " + SYNTAX );
-        }
-        if ((url.getHost() != null && url.getHost().length() > 0) || url.getPort() != -1) {
-            throw new MalformedURLException("Unsupported host/port in zookeeper url");
-        }
-        if (url.getQuery() != null && url.getQuery().length() > 0) {
-            throw new MalformedURLException("Unsupported query in zookeeper url");
-        }
         return new Connection(url);
     }
 
     public class Connection extends URLConnection {
 
-        public Connection(URL url) {
+        public Connection(URL url) throws MalformedURLException {
             super(url);
+            if (url.getPath() == null || url.getPath().trim().length() == 0) {
+                throw new MalformedURLException("Path can not be null or empty. Syntax: " + SYNTAX );
+            }
+            if ((url.getHost() != null && url.getHost().length() > 0) || url.getPort() != -1) {
+                throw new MalformedURLException("Unsupported host/port in zookeeper url");
+            }
+            if (url.getQuery() != null && url.getQuery().length() > 0) {
+                throw new MalformedURLException("Unsupported query in zookeeper url");
+            }
+            if( url.getRef()!=null ) {
+                String path = url.getPath().trim();
+                if( !path.endsWith(".properties") && !path.endsWith(".json") ) {
+                    throw new MalformedURLException("Fragments are only supported for '.properties' and '.json' files.");
+                }
+            }
         }
 
         @Override
@@ -77,12 +88,53 @@ public class ZkUrlHandler extends AbstractURLStreamHandlerService {
 
         @Override
         public InputStream getInputStream() throws IOException {
+            ByteArrayInputStream rc = null;
             try {
-                return new ByteArrayInputStream(zooKeeper.getData(url.getPath()));
+              rc = new ByteArrayInputStream(zooKeeper.getData(url.getPath()));
             } catch (Exception e) {
                 logger.error("Error opening zookeeper url", e);
                 throw (IOException) new IOException("Error opening zookeeper url").initCause(e);
             }
+            if( url.getRef()!=null ) {
+                String path = url.getPath().trim();
+                if( path.endsWith(".properties") ) {
+                    Properties properties = new Properties();
+                    properties.load(rc);
+                    String property = properties.getProperty(url.getRef());
+                    if( property==null ) {
+                        throw  new IOException("Property '"+url.getRef()+"' is not set in the properties file.");
+                    }
+                    rc = new ByteArrayInputStream(property.getBytes("UTF-8"));
+                } else if( path.endsWith(".json") ) {
+                    String[] fields = url.getRef().split("\\.");
+                    ObjectMapper mapper = new ObjectMapper();
+                    JsonFactory factory = mapper.getJsonFactory();
+                    JsonParser jp = factory.createJsonParser(rc);
+                    JsonNode node = mapper.readTree(jp);
+                    for(String field: fields) {
+                        if(!field.isEmpty()) {
+                            if( node.isObject() ) {
+                                node = node.get(field);
+                            } else if (node.isArray()) {
+                                node = node.get(Integer.parseInt(field));
+                            } else {
+                                throw  new IOException("Path '"+url.getRef()+"' is not set in the json file.");
+                            }
+                            if( node == null ) {
+                                throw  new IOException("Path '"+url.getRef()+"' is not set in the json file.");
+                            }
+                        }
+                    }
+                    if( node.isContainerNode() ) {
+                        throw new IOException("Path '"+url.getRef()+"' is not a value in the json file.");
+                    }
+                    String textValue = node.getValueAsText();
+                    rc = new ByteArrayInputStream(textValue.getBytes("UTF-8"));
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+            return rc;
         }
     }
 
