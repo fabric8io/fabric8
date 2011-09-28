75470c1e8207d91fb2e11600d5a0cccc6e6418b8
Scott England-Sullivan <sully6768@yahoo.com>
2011-09-28 09:55:22 -0500

    add configuration tunning ability to pax exam and probe functions

--- fab/tests/fab-itests/src/test/java/org/fusesource/fabric/fab/osgi/itests/IntegrationTestSupport.java
+++ fab/tests/fab-itests/src/test/java/org/fusesource/fabric/fab/osgi/itests/IntegrationTestSupport.java
@@ -8,15 +8,20 @@
  */
 package org.fusesource.fabric.fab.osgi.itests;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+import java.util.Dictionary;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Set;
+
 import org.apache.karaf.testing.AbstractIntegrationTest;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.LinkedList;
-
-import static org.junit.Assert.assertNotNull;
 
 /**
  */
@@ -62,5 +67,32 @@ public class IntegrationTestSupport extends AbstractIntegrationTest {
 
     protected void println(Object value) {
         System.out.println("======================== " + value);
+    }    
+    
+    @SuppressWarnings("unchecked")
+    protected void configurePid( String pid ) throws Exception {
+        ConfigurationAdmin cs = getOsgiService(ConfigurationAdmin.class, 20000);
+        
+        org.osgi.service.cm.Configuration conf = cs.getConfiguration(pid, null);
+
+        Dictionary<Object, Object> props = conf.getProperties();
+        if (props == null) {
+                props = new Properties();
+        }
+
+        // Load log4j properties
+        Properties properties = new Properties();
+        InputStream propertiesStream = IntegrationTestSupport.class.getClassLoader().getResourceAsStream(pid + ".cfg");
+        
+        // Add properties to the configuration
+        if(propertiesStream != null){
+            properties.load( propertiesStream );
+    
+            Set<Object> keys = properties.keySet();
+            for (Object key : keys) {
+                props.put(key, properties.get(key));
+            }
+            conf.update(props);
+        }
     }
 }
