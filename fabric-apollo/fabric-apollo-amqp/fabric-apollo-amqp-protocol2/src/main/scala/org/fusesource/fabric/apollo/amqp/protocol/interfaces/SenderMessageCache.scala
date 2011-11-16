381e2087cb65f68dca62bd79b9f7ddad54a75601
Stan Lewis <slewis@fusesource.com>
2011-11-16 09:54:23 -0500

    Outgoing message cache, could be made pluggable

---
+++ fabric-apollo/fabric-apollo-amqp/fabric-apollo-amqp-protocol2/src/main/scala/org/fusesource/fabric/apollo/amqp/protocol/interfaces/SenderMessageCache.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2010-2011, FuseSource Corp.  All rights reserved
+ *
+ *    http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * CDDL license, a copy of which has been included with this distribution
+ * in the license.txt file
+ */
+
+package org.fusesource.fabric.apollo.amqp.protocol.interfaces
+
+import org.fusesource.hawtbuf.Buffer
+
+trait SenderMessageCache {
+
+  def cache(message:Buffer)
+
+  def uncache:Buffer
+
+}
