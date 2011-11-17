9507817ee4db095ed50fae2216fe706a3c31477f
dbokde <dbokde@progress.com>
2011-11-16 19:25:07 -0800

    Fixed ProfileOverlay supplement calls

--- fabric-core/src/main/scala/org/fusesource/fabric/internal/ProfileOverlayImpl.java
+++ fabric-core/src/main/scala/org/fusesource/fabric/internal/ProfileOverlayImpl.java
@@ -113,7 +113,7 @@ public class ProfileOverlayImpl implements Profile {
     public Map<String, byte[]> getFileConfigurations() {
         try {
             Map<String, SupplementControl> aggregate = new HashMap<String, SupplementControl>();
-            supplement(this, aggregate);
+            supplement(self, aggregate);
 
             Map<String, byte[]> rc = new HashMap<String, byte[]>();
             for (Map.Entry<String, SupplementControl> entry : aggregate.entrySet()) {
@@ -134,6 +134,7 @@ public class ProfileOverlayImpl implements Profile {
             supplement(p, aggregate);
         }
 
+        // TODO fix this, should this every happen???
         if (profile instanceof ProfileOverlayImpl) {
             if (((ProfileOverlayImpl)profile).self.equals(self)) {
                 return;
@@ -183,7 +184,7 @@ public class ProfileOverlayImpl implements Profile {
     public Map<String, Map<String, String>> getConfigurations() {
         try {
             Map<String, SupplementControl> aggregate = new HashMap<String, SupplementControl>();
-            supplement(this, aggregate);
+            supplement(self, aggregate);
 
             Map<String, Map<String, String>> rc = new HashMap<String, Map<String, String>>();
             for (Map.Entry<String, SupplementControl> entry : aggregate.entrySet()) {
