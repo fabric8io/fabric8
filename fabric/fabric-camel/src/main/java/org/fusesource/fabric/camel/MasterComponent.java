0c4f60b07c2031ce88e1841cf059f433e5af33f8
James Strachan <james.strachan@gmail.com>
2012-01-18 17:17:30 +0000

    first spike of the master endpoint

---
+++ fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/MasterComponent.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2011, FuseSource Corp.  All rights reserved.
+ * http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * CDDL license a copy of which has been included with this distribution
+ * in the license.txt file.
+ */
+package org.fusesource.fabric.camel;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.impl.DefaultComponent;
+import org.apache.camel.impl.ProducerCache;
+import org.apache.camel.util.ObjectHelper;
+import org.apache.camel.util.ServiceHelper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.ACL;
+import org.fusesource.fabric.groups.Group;
+import org.fusesource.fabric.groups.ZooKeeperGroupFactory;
+import org.linkedin.util.clock.Timespan;
+import org.linkedin.zookeeper.client.IZKClient;
+import org.linkedin.zookeeper.client.ZKClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The MASTER camel component ensures that only a single endpoint in a cluster is active at any
+ * point in time with all other JVMs being hot standbys which wait until the master JVM dies before
+ * taking over to provide high availability of a single consumer.
+ */
+public class MasterComponent extends DefaultComponent {
+    private static final transient Log LOG = LogFactory.getLog(MasterComponent.class);
+
+    @Autowired
+    private IZKClient zkClient;
+    private String zkRoot = "/fabric/camel/endpoints";
+    private List<ACL> accessControlList = ZooDefs.Ids.OPEN_ACL_UNSAFE;
+    private ProducerCache producerCache;
+    private int cacheSize = 1000;
+
+
+    public IZKClient getZkClient() {
+        return zkClient;
+    }
+
+    public void setZkClient(IZKClient zkClient) {
+        this.zkClient = zkClient;
+    }
+
+    public String getZkRoot() {
+        return zkRoot;
+    }
+
+    public void setZkRoot(String zkRoot) {
+        this.zkRoot = zkRoot;
+    }
+
+    public List<ACL> getAccessControlList() {
+        return accessControlList;
+    }
+
+    public void setAccessControlList(List<ACL> accessControlList) {
+        this.accessControlList = accessControlList;
+    }
+
+    //  Implementation methods
+    //-------------------------------------------------------------------------
+
+
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+        if (zkClient == null) {
+            zkClient = (IZKClient) getCamelContext().getRegistry().lookup(IZKClient.class.getName());
+            LOG.debug("IZKClient find in camel registry.");
+        }
+        if (zkClient == null) {
+            ZKClient client = new ZKClient(System.getProperty("zookeeper.url", "localhost:2181"), Timespan.parse("10s"), null);
+            LOG.debug("IZKClient not find in camel registry and created.");
+            client.start();
+            zkClient = client;
+        }
+        checkZkConnected();
+        if (producerCache == null) {
+            producerCache = new ProducerCache(this, getCamelContext(), cacheSize);
+        }
+        ServiceHelper.startService(producerCache);
+    }
+
+    protected void checkZkConnected() throws Exception {
+        if (!zkClient.isConnected()) {
+            throw new Exception("Could not connect to ZooKeeper " + zkClient);
+        }
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+        if (zkClient != null) {
+            zkClient.close();
+        }
+        ServiceHelper.stopService(producerCache);
+    }
+
+    @Override
+    protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> params) throws Exception {
+        int idx = remaining.indexOf(':');
+        if (idx <= 0) {
+            throw new IllegalArgumentException("Missing : in URI so can't split the group name from the actual URI for '" + remaining + "'");
+        }
+        // we are registering a regular endpoint
+        String name = remaining.substring(0, idx);
+        String fabricPath = getFabricPath(name);
+        String childUri = remaining.substring(idx + 1);
+
+        Group group = ZooKeeperGroupFactory.create(getZkClient(), fabricPath, accessControlList);
+        return new MasterEndpoint(uri, this, group, childUri);
+    }
+
+    protected String getFabricPath(String name) {
+        String path = name;
+        if (ObjectHelper.isNotEmpty(zkRoot)) {
+            path = zkRoot + "/" + name;
+        }
+        return path;
+    }
+
+}
---
+++ fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/MasterConsumer.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2010, FuseSource Corp.  All rights reserved.
+ * http://fusesource.com
+ *
+ * The software in this package is published under the terms of the
+ * AGPL license a copy of which has been included with this distribution
+ * in the license.txt file.
+ */
+package org.fusesource.fabric.camel;
+
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.SuspendableService;
+import org.apache.camel.impl.DefaultConsumer;
+import org.fusesource.fabric.groups.ChangeListener;
+import org.fusesource.fabric.groups.ClusteredSingleton;
+import org.fusesource.fabric.groups.Group;
+import org.fusesource.fabric.groups.TextNodeState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A consumer which is only really active while it holds the master lock
+ */
+public class MasterConsumer extends DefaultConsumer {
+    private static final transient Logger LOG = LoggerFactory.getLogger(MasterConsumer.class);
+
+    private final MasterEndpoint endpoint;
+    private final Processor processor;
+    private Consumer delegate;
+    private SuspendableService delegateService;
+
+    public MasterConsumer(MasterEndpoint endpoint, Processor processor) {
+        super(endpoint, processor);
+        this.endpoint = endpoint;
+        this.processor = processor;
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+
+        tryAcquireLock();
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+
+        stopConsumer();
+
+        releaseLock();
+    }
+
+    protected void stopConsumer() throws Exception {
+        if (delegate != null) {
+            try {
+                delegate.stop();
+            } finally {
+                delegate = null;
+                delegateService = null;
+            }
+        }
+    }
+
+    @Override
+    protected void doResume() throws Exception {
+        if (delegateService != null) {
+            delegateService.resume();
+        }
+        super.doResume();
+    }
+
+    @Override
+    protected void doSuspend() throws Exception {
+        if (delegateService != null) {
+            delegateService.suspend();
+        }
+        super.doSuspend();
+    }
+
+    protected void onLockOwned() {
+        if (delegate == null) {
+            try {
+                delegate = endpoint.getChildEndpoint().createConsumer(processor);
+                delegateService = null;
+                if (delegate instanceof SuspendableService) {
+                    delegateService = (SuspendableService) delegate;
+                }
+                if (delegate != null) {
+                    delegate.start();
+                }
+            } catch (Exception e) {
+                LOG.error("Failed to start master consumer for: " + endpoint + ". Reason: " + e, e);
+            }
+        }
+    }
+
+    protected void tryAcquireLock() {
+        final ClusteredSingleton<TextNodeState> cluster = getCluster();
+        TextNodeState state = new TextNodeState();
+        state.setId(endpoint.getId());
+        cluster.join(state);
+        cluster.add(new ChangeListener() {
+            @Override
+            public void connected() {
+                System.out.println("=============== cluster connected!");
+            }
+
+            @Override
+            public void changed() {
+                System.out.println("=============== cluster changed!");
+                if (cluster.isMaster()) {
+                    System.out.println("=============== is master!");
+                    onLockOwned();
+                } else {
+                    System.out.println("=============== not master!");
+                }
+            }
+
+            @Override
+            public void disconnected() {
+                try {
+                    stopConsumer();
+                } catch (Exception e) {
+                    LOG.error("Failed to stop master consumer for: " + endpoint + ". Reason: " + e, e);
+                }
+            }
+        });
+    }
+
+    protected ClusteredSingleton<TextNodeState> getCluster() {
+        return endpoint.getCluster();
+    }
+
+    protected void releaseLock() {
+        getCluster().leave();
+    }
+
+
+}
