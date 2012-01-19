342f0c30f06b3924843b40ca944079aca74899f7
James Strachan <james.strachan@gmail.com>
2012-01-19 12:07:44 +0000

    refactored code to provide a common ZKComponentSupport class which wraps up the ZK related stuff and documented more why we need to wait between zk.start() and asserting zk.isConnected()

--- fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/MasterComponent.java
+++ fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/MasterComponent.java
@@ -31,32 +31,8 @@ import java.util.Map;
  * point in time with all other JVMs being hot standbys which wait until the master JVM dies before
  * taking over to provide high availability of a single consumer.
  */
-public class MasterComponent extends DefaultComponent {
-    private static final transient Log LOG = LogFactory.getLog(MasterComponent.class);
-
-    private IZKClient zkClient;
+public class MasterComponent extends ZKComponentSupport {
     private String zkRoot = "/fabric/camel/master";
-    private List<ACL> accessControlList = ZooDefs.Ids.OPEN_ACL_UNSAFE;
-    private boolean shouldCloseZkClient = false;
-    private long maximumConnectionTimeout = 10 * 1000L;
-    private long connectionRetryTime = 100L;
-
-
-    public IZKClient getZkClient() {
-        return zkClient;
-    }
-
-    public void setZkClient(IZKClient zkClient) {
-        this.zkClient = zkClient;
-    }
-
-    public boolean isShouldCloseZkClient() {
-        return shouldCloseZkClient;
-    }
-
-    public void setShouldCloseZkClient(boolean shouldCloseZkClient) {
-        this.shouldCloseZkClient = shouldCloseZkClient;
-    }
 
     public String getZkRoot() {
         return zkRoot;
@@ -66,87 +42,9 @@ public class MasterComponent extends DefaultComponent {
         this.zkRoot = zkRoot;
     }
 
-    public List<ACL> getAccessControlList() {
-        return accessControlList;
-    }
-
-    public void setAccessControlList(List<ACL> accessControlList) {
-        this.accessControlList = accessControlList;
-    }
-
-    public long getConnectionRetryTime() {
-        return connectionRetryTime;
-    }
-
-    public void setConnectionRetryTime(long connectionRetryTime) {
-        this.connectionRetryTime = connectionRetryTime;
-    }
-
-    public long getMaximumConnectionTimeout() {
-        return maximumConnectionTimeout;
-    }
-
-    public void setMaximumConnectionTimeout(long maximumConnectionTimeout) {
-        this.maximumConnectionTimeout = maximumConnectionTimeout;
-    }
-    
     //  Implementation methods
     //-------------------------------------------------------------------------
 
-
-    @Override
-    protected void doStart() throws Exception {
-        super.doStart();
-        if (zkClient == null) {
-            zkClient = (IZKClient) getCamelContext().getRegistry().lookup("zkClient");
-            if (zkClient != null) {
-                LOG.debug("IZKClient found in camel registry. " + zkClient);
-            }
-        }
-        if (zkClient == null) {
-            String connectString = System.getProperty("zookeeper.url", "localhost:2181");
-            ZKClient client = new ZKClient(connectString, Timespan.parse("10s"), null);
-            LOG.debug("IZKClient not find in camel registry, creating new with connection " + connectString);
-            zkClient = client;
-            setShouldCloseZkClient(true);
-        }
-
-        // ensure we are started
-        if (zkClient instanceof ZKClient) {
-            if (!zkClient.isConnected()) {
-                LOG.debug("Staring IZKClient " + zkClient);
-                ((ZKClient) zkClient).start();
-            }
-        }
-        checkZkConnected();
-
-    }
-
-    protected void checkZkConnected() throws Exception {
-        long start = System.currentTimeMillis();
-        do {
-            if (zkClient.isConnected()) {
-                return;
-            }
-            try {
-                Thread.sleep(getConnectionRetryTime());
-            } catch (InterruptedException e) {
-                // ignore
-            }
-        } while (System.currentTimeMillis() < start + getMaximumConnectionTimeout());
-
-        if (!zkClient.isConnected()) {
-            throw new Exception("Could not connect to ZooKeeper " + zkClient + " at " + zkClient.getConnectString());
-        }
-    }
-
-    @Override
-    protected void doStop() throws Exception {
-        super.doStop();
-        if (zkClient != null && isShouldCloseZkClient()) {
-            zkClient.close();
-        }
-    }
 
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> params) throws Exception {
