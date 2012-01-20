48d37ac5d5d4b29c05ceaf45d9ecfe8c1a9d2640
davsclaus <claus.ibsen@gmail.com>
2012-01-20 17:29:06 +0100

    Prepare soure for camel 2.9 upgrade due API changes

--- fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/ClusteredSingletonLifecycleStrategy.java
+++ fabric/fabric-camel/src/main/java/org/fusesource/fabric/camel/ClusteredSingletonLifecycleStrategy.java
@@ -32,6 +32,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class ClusteredSingletonLifecycleStrategy implements LifecycleStrategy {
 
+    // TODO: Work in progress
+
     public static Log LOG = LogFactory.getLog(ClusteredSingletonLifecycleStrategy.class);
 
     Group group;
@@ -183,6 +185,9 @@ public class ClusteredSingletonLifecycleStrategy implements LifecycleStrategy {
     }
     public void onErrorHandlerAdd(RouteContext routeContext, Processor processor, ErrorHandlerBuilder errorHandlerBuilder) {
     }
+    // TODO: When upgrading to Camel 2.9
+    // public void onErrorHandlerAdd(RouteContext routeContext, Processor processor, ErrorHandlerFactory errorHandlerFactory) {
+    //}
     public void onThreadPoolAdd(CamelContext camelContext, ThreadPoolExecutor threadPoolExecutor, String s, String s1, String s2, String s3) {
     }
 
