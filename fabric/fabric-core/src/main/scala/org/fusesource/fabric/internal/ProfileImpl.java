f42bd44cbf63152d76a412c1374368d13b033ed7
Ioannis Canellos <iocanel@apache.org>
2012-02-27 20:47:51 +0200

    Added missing import to ProfileImpl.

--- fabric/fabric-core/src/main/scala/org/fusesource/fabric/internal/ProfileImpl.java
+++ fabric/fabric-core/src/main/scala/org/fusesource/fabric/internal/ProfileImpl.java
@@ -20,6 +20,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
