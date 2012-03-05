aa2d3253e14b5a70169a7f5c9f6bbc648b43c7ce
Guillaume Nodet <gnodet@gmail.com>
2012-03-05 15:41:45 +0100

    [FABRICPRV-158]  Raise the zookeeper client timeout to avoid connection loss errors

--- fabric/fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/OsgiZkClient.java
+++ fabric/fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/internal/OsgiZkClient.java
@@ -284,7 +284,7 @@ public class OsgiZkClient extends AbstractZKClient implements Watcher, ManagedSe
     public void updated(Dictionary properties) throws ConfigurationException {
         synchronized (_lock) {
             String url = System.getProperty("zookeeper.url");
-            Timespan timeout = new Timespan(10, Timespan.TimeUnit.SECOND);
+            Timespan timeout = new Timespan(30, Timespan.TimeUnit.SECOND);
             if (properties != null) {
                 if (properties.get("zookeeper.url") != null) {
                     url = (String) properties.get("zookeeper.url");
