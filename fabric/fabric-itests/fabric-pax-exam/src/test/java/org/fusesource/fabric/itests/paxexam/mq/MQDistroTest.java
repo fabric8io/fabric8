4416aa485d8db79bdfa9ae259b6e5571c22961a4
gtully <gary.tully@gmail.com>
2012-03-07 12:17:01 +0000

    validate webconsole and jms openwire connectivity on fusemq distro via message round trip

--- fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/mq/MQDistroTest.java
+++ fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/mq/MQDistroTest.java
@@ -16,6 +16,14 @@
  */
 package org.fusesource.fabric.itests.paxexam.mq;
 
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openengsb.labs.paxexam.karaf.options.LogLevelOption;
@@ -23,14 +31,17 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.ops4j.pax.exam.options.BundleStartLevelOption;
 import org.ops4j.pax.exam.options.DefaultCompositeOption;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
 import java.io.File;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.*;
 import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 
 
 @RunWith(JUnit4TestRunner.class)
@@ -39,16 +50,61 @@ public class MQDistroTest {
 
     static final String MQ_GROUP_ID = "org.fusesource.mq";
     static final String MQ_ARTIFACT_ID = "fuse-mq";
+    static final String WEB_CONSOLE_URL = "http://localhost:8181/activemqweb/";
 
     @Test
     public void testMQ() throws Exception {
 
+        // send message via webconsole, consume from jms openwire
+        HttpClient client = new HttpClient();
+
+        // need to first get the secret
+        GetMethod get = new GetMethod(WEB_CONSOLE_URL + "send.jsp");
+        // Give console some time to start
+        for (int i=0; i<20; i++) {
+            Thread.currentThread().sleep(1000);
+            try {
+                i = client.executeMethod(get);
+            } catch (java.net.ConnectException ignored) {}
+        }
+        assertEquals("get succeeded on " + get, 200, get.getStatusCode());
+
+        String response = get.getResponseBodyAsString();
+        final String secretMarker = "<input type=\"hidden\" name=\"secret\" value=\"";
+        String secret = response.substring(response.indexOf(secretMarker) + secretMarker.length());
+        secret = secret.substring(0, secret.indexOf("\"/>"));
+
+        final String destination = "validate.console.send";
+        final String content = "Hi for the " + Math.random() + "' time";
+
+        PostMethod post = new PostMethod(WEB_CONSOLE_URL + "sendMessage.action");
+        post.addParameter("secret", secret);
+
+        post.addParameter("JMSText", content);
+        post.addParameter("JMSDestination", destination);
+        post.addParameter("JMSDestinationType", "queue");
+
+        // execute the send
+        assertEquals("post succeeded, " + post, 302, client.executeMethod(post));
+
+        // consume what we sent
+        Connection connection = new ActiveMQConnectionFactory().createConnection();
+        connection.start();
+        try {
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            TextMessage textMessage = (TextMessage) session.createConsumer(new ActiveMQQueue(destination)).receive(10*1000);
+            assertNotNull("got a message", textMessage);
+            assertEquals("it is ours", content, textMessage.getText());
+        } finally {
+            connection.close();
+        }
     }
 
     @Configuration
     public Option[] config() {
         return new Option[]{
                 mqDistributionConfiguration(), keepRuntimeFolder(),
+                mavenBundle("commons-httpclient", "commons-httpclient").versionAsInProject().type("jar"),
                 logLevel(LogLevelOption.LogLevel.ERROR)
         };
     }
