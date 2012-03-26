a8f6d8d77dcb42829923f7a6694aece75d1c0286
Willem Jiang <willem.jiang@gmail.com>
2012-03-26 14:34:50 +0800

    ENTESB-102 cxf-jaxrs-security publish url need to be changed to securecrm

--- examples/cxf-jaxrs-security/README.md
+++ examples/cxf-jaxrs-security/README.md
@@ -42,15 +42,15 @@ A full listing of all CXF web services is available at
 
 After you deployed this example, you will see the following endpoint address appear in the 'Available RESTful services' section:
 
-    http://localhost:8181/cxf/crm
+    http://localhost:8181/cxf/securecrm
 
 Just below it, you'll find a link to the WADL describing all the root resources:
 
-    http://localhost:8181/cxf/crm?_wadl
+    http://localhost:8181/cxf/securecrm?_wadl
 
 You can also look at the more specific WADL, the only that only lists information about 'customerservice' itself:
 
-	http://localhost:8181/cxf/crm/customerservice?_wadl&_type=xml
+	http://localhost:8181/cxf/securecrm/customerservice?_wadl&_type=xml
 
 ### Access services using a web browser
 
@@ -58,7 +58,7 @@ You can use any browser to perform a HTTP GET.  This allows you to very easily t
 
 Use this URL to access the XML representation for customer 123:
 
-    http://localhost:8181/cxf/crm/customerservice/customers/123
+    http://localhost:8181/cxf/securecrm/customerservice/customers/123
 
 Because we need to pass along credentials to actually access the service in this security-enabled example, we will get a fault
 message indicating a security exception at this time.
@@ -70,7 +70,7 @@ Note: if you use Safari, you will only see the text elements but not the XML tag
 In this cxf-jaxrs example project, we also developed a Java client which can perform a few HTTP requests to test our web services. We
 configured the exec-java-plugin in Maven to allow us to run the Java client code with a simple Maven command:
 
-1. Change to the <esb_home>/examples/cxf-jaxrsdirectory.
+1. Change to the <esb_home>/examples/cxf-jaxrs-security directory.
 2. Run the following command:
 
         mvn compile exec:java
@@ -82,24 +82,24 @@ The client makes a sequence of RESTful invocations and displays the results.
 You can use a command-line utility, such as cURL or wget, to perform the HTTP requests.  We have provided a few files with sample
 XML representations in src/main/resources/org/fusesource/examples/cxf/jaxrs/client, so we will use those for testing our services.
 
-1. Open a command prompt and change directory to <esb_home>/examples/cxf-jaxrs.
+1. Open a command prompt and change directory to <esb_home>/examples/cxf-jaxrs-security.
 2. Run the following curl commands:
     
     * Create a customer
  
-            curl --basic -u smx:smx -X POST -T src/main/resources/org/fusesource/examples/cxf/jaxrs/client/add_customer.xml -H "Content-Type: text/xml" http://localhost:8181/cxf/crm/customerservice/customers
+            curl --basic -u smx:smx -X POST -T src/main/resources/org/fusesource/examples/cxf/jaxrs/security/client/add_customer.xml -H "Content-Type: text/xml" http://localhost:8181/cxf/securecrm/customerservice/customers
   
     * Retrieve the customer instance with id 123
     
-            curl --basic -u smx:smx http://localhost:8181/cxf/crm/customerservice/customers/123
+            curl --basic -u smx:smx http://localhost:8181/cxf/securecrm/customerservice/customers/123
 
     * Update the customer instance with id 123
   
-            curl --basic -u smx:smx -X PUT -T src/main/resources/org/fusesource/examples/cxf/jaxrs/client/update_customer.xml -H "Content-Type: text/xml" http://localhost:8181/cxf/crm/customerservice/customers
+            curl --basic -u smx:smx -X PUT -T src/main/resources/org/fusesource/examples/cxf/jaxrs/security/client/update_customer.xml -H "Content-Type: text/xml" http://localhost:8181/cxf/securecrm/customerservice/customers
 
     * Delete the customer instance with id 123
   
-             curl --basic -u smx:smx -X DELETE http://localhost:8181/cxf/crm/customerservice/customers/123
+             curl --basic -u smx:smx -X DELETE http://localhost:8181/cxf/securecrm/customerservice/customers/123
 
 ## Additional configuration options
 
--- examples/cxf-jaxrs-security/src/main/java/org/fusesource/examples/cxf/jaxrs/security/client/Client.java
+++ examples/cxf-jaxrs-security/src/main/java/org/fusesource/examples/cxf/jaxrs/security/client/Client.java
@@ -48,13 +48,13 @@ public final class Client {
 
 
         /**
-         * HTTP GET http://localhost:8181/cxf/crm/customerservice/customers/123
+         * HTTP GET http://localhost:8181/cxf/securecrm/customerservice/customers/123
          * returns the XML document representing customer 123
          *
          * On the server side, it matches the CustomerService's getCustomer() method
          */
         System.out.println("Sent HTTP GET request to query customer info with basic authentication info.");
-        GetMethod get = new GetMethod("http://localhost:8181/cxf/crm/customerservice/customers/123");
+        GetMethod get = new GetMethod("http://localhost:8181/cxf/securecrm/customerservice/customers/123");
         get.getHostAuthState().setAuthScheme(scheme);
         try {
             httpClient.executeMethod(get);
@@ -64,12 +64,12 @@ public final class Client {
         }
 
         /**
-         * HTTP GET http://localhost:8181/cxf/crm/customerservice/customers/123
+         * HTTP GET http://localhost:8181/cxf/securecrm/customerservice/customers/123
          * without passing along authentication credentials - this will result in a security exception in the response.
          */
         System.out.println("\n");
         System.out.println("Sent HTTP GET request to query customer info without basic authentication info.");
-        get = new GetMethod("http://localhost:8181/cxf/crm/customerservice/customers/123");
+        get = new GetMethod("http://localhost:8181/cxf/securecrm/customerservice/customers/123");
         try {
             httpClient.executeMethod(get);
             // we should get the security exception here
@@ -79,14 +79,14 @@ public final class Client {
         }
 
         /**
-         * HTTP GET http://localhost:8181/cxf/crm/customerservice/orders/223/products/323
+         * HTTP GET http://localhost:8181/cxf/securecrm/customerservice/orders/223/products/323
          * returns the XML document representing product 323 in order 223
          *
          * On the server side, it matches the Order's getProduct() method
          */
         System.out.println("\n");
         System.out.println("Sent HTTP GET request to query sub resource product info");
-        get = new GetMethod("http://localhost:8181/cxf/crm/customerservice/orders/223/products/323");
+        get = new GetMethod("http://localhost:8181/cxf/securecrm/customerservice/orders/223/products/323");
         get.getHostAuthState().setAuthScheme(scheme);
         try {
             httpClient.executeMethod(get);
@@ -96,7 +96,7 @@ public final class Client {
         }
 
         /**
-         * HTTP PUT http://localhost:8181/cxf/crm/customerservice/customers is used to upload the contents of
+         * HTTP PUT http://localhost:8181/cxf/securecrm/customerservice/customers is used to upload the contents of
          * the update_customer.xml file to update the customer information for customer 123.
          *
          * On the server side, it matches the CustomerService's updateCustomer() method
@@ -106,7 +106,7 @@ public final class Client {
         
         String inputFile = Client.class.getResource("update_customer.xml").getFile();
         File input = new File(inputFile);
-        PutMethod put = new PutMethod("http://localhost:8181/cxf/crm/customerservice/customers");
+        PutMethod put = new PutMethod("http://localhost:8181/cxf/securecrm/customerservice/customers");
         put.getHostAuthState().setAuthScheme(scheme);
         RequestEntity entity = new FileRequestEntity(input, "text/xml; charset=ISO-8859-1");
         put.setRequestEntity(entity);
@@ -124,7 +124,7 @@ public final class Client {
         }
 
         /**
-         * HTTP POST http://localhost:8181/cxf/crm/customerservice/customers is used to upload the contents of
+         * HTTP POST http://localhost:8181/cxf/securecrm/customerservice/customers is used to upload the contents of
          * the add_customer.xml file to add a new customer to the system.
          *
          * On the server side, it matches the CustomerService's addCustomer() method
@@ -133,7 +133,7 @@ public final class Client {
         System.out.println("Sent HTTP POST request to add customer");
         inputFile = Client.class.getResource("add_customer.xml").getFile();
         input = new File(inputFile);
-        PostMethod post = new PostMethod("http://localhost:8181/cxf/crm/customerservice/customers");
+        PostMethod post = new PostMethod("http://localhost:8181/cxf/securecrm/customerservice/customers");
         post.getHostAuthState().setAuthScheme(scheme);
         post.addRequestHeader("Accept" , "text/xml");
         entity = new FileRequestEntity(input, "text/xml; charset=ISO-8859-1");
