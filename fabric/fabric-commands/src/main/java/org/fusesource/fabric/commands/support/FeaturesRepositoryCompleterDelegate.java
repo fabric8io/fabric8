c64cb926fd78b4675c550c8c5fb1d04999dd58ce
Ioannis Canellos <iocanel@apache.org>
2012-03-31 13:30:24 +0300

    [FABRICPRV-226] Fabric commands now optionally import features core and features commands packages. If those packages are not present feature, repo & bundle completion is disabled.

---
+++ fabric/fabric-commands/src/main/java/org/fusesource/fabric/commands/support/FeaturesRepositoryCompleterDelegate.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) FuseSource, Inc.
+ *   http://fusesource.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.fusesource.fabric.commands.support;
+
+import java.util.List;
+import org.apache.karaf.shell.console.Completer;
+
+/**
+ * A delegating completer to Karafs FeatureRepository completer.
+ * The purpose of this class is to provide a completer that will work even if Feature service/commands are not persent.
+ */
+public class FeaturesRepositoryCompleterDelegate implements Completer {
+
+    public static Completer DELEGATE;
+
+    @Override
+    public int complete(String s, int i, List<String> strings) {
+        if (DELEGATE != null) {
+            return DELEGATE.complete(s,i,strings);
+        }
+        return 0;
+    }
+}
