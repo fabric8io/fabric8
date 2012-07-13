96cbb1113e5a7cc37e80f6cf3c75035e954121be
Ioannis Canellos <iocanel@apache.org>
2012-07-13 15:06:53 +0300

    [FABRIC-240] Fixed self update test to properly upgrade container versions. Test now uses perfectus versions.

--- fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/SelfUpdateTest.java
+++ fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/SelfUpdateTest.java
@@ -17,11 +17,13 @@
 
 package org.fusesource.fabric.itests.paxexam;
 
+import java.io.File;
 import org.fusesource.fabric.api.FabricService;
 import org.junit.After;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openengsb.labs.paxexam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
@@ -31,16 +33,20 @@ import org.ops4j.pax.exam.options.DefaultCompositeOption;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
 
-import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.editConfigurationFileExtend;
 import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
-import static org.ops4j.pax.exam.CoreOptions.scanFeatures;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.logLevel;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.useOwnExamBundlesStartLevel;
+import static org.ops4j.pax.exam.CoreOptions.maven;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
 public class SelfUpdateTest extends FabricTestSupport {
 
     private static final String FABRIC_FEATURE_REPO_URL = "mvn:org.fusesource.fabric/fuse-fabric/%s/xml/features";
-    private static final String TARGET_VERSION = "7.0.0.fuse-beta-040";
+    private static final String OLD_VERSION = "7.0.0.fuse-061";
+    private static final String NEW_VERSION = "7.0.1.fuse-%s";
 
     @After
     public void tearDown() throws InterruptedException {
@@ -49,19 +55,21 @@ public class SelfUpdateTest extends FabricTestSupport {
 
     @Ignore // JIRA ESB-1687
     @Test
-    public void testLocalChildCreation() throws Exception {
-        FabricService fabricService = getOsgiService(FabricService.class);
+    public void testDefaultProfileUpgrade() throws Exception {
+        String perfecutsBuild = System.getProperty("perfectus-build");
+        String newVersion = String.format(NEW_VERSION, perfecutsBuild);
 
-        System.err.println(executeCommand("fabric:create -n"));
+        FabricService fabricService = getOsgiService(FabricService.class);
+        System.err.println(executeCommand("fabric:create"));
         addStagingRepoToDefaultProfile();
         createAndAssertChildContainer("child1", "root", "default");
-        String curretRepoURL = String.format(FABRIC_FEATURE_REPO_URL,System.getProperty("fabric.version"));
-        String targetRepoURL = String.format(FABRIC_FEATURE_REPO_URL,TARGET_VERSION);
+        String newRepoURL = String.format(FABRIC_FEATURE_REPO_URL,newVersion);
+        String oldRepoURL = String.format(FABRIC_FEATURE_REPO_URL, OLD_VERSION);
         System.out.println(executeCommand("fabric:version-create --parent 1.0 1.1"));
-        System.out.println(executeCommand("fabric:profile-edit --delete --repositories "+curretRepoURL+" default 1.1"));
-        System.out.println(executeCommand("fabric:profile-edit --repositories "+targetRepoURL+" default 1.1"));
+        System.out.println(executeCommand("fabric:profile-edit --delete --repositories "+oldRepoURL+" default 1.1"));
+        System.out.println(executeCommand("fabric:profile-edit --repositories "+newRepoURL+" default 1.1"));
         System.out.println(executeCommand("fabric:profile-display --version 1.1 default"));
-        System.out.println(executeCommand("fabric:container-upgrade --all 1.1"));
+        System.out.println(executeCommand("fabric:container-upgrade 1.1 child1"));
         Thread.sleep(5000);
         waitForProvisionSuccess(fabricService.getContainer("child1"), PROVISION_TIMEOUT);
         System.out.println(executeCommand("fabric:container-list"));
@@ -70,8 +78,23 @@ public class SelfUpdateTest extends FabricTestSupport {
     @Configuration
     public Option[] config() {
         return new Option[]{
-                new DefaultCompositeOption(fabricDistributionConfiguration()),
+                copySystemProperty("perfectus-build"),
+                new DefaultCompositeOption(oldFabricDistributionConfiguration()),
+                //debugConfiguration("5005",true),
                 editConfigurationFilePut("etc/system.properties", "fabric.version", MavenUtils.getArtifactVersion("org.fusesource.fabric", "fuse-fabric"))
+        };
+    }
+
+    protected Option[] oldFabricDistributionConfiguration() {
+        return new Option[] {
+                karafDistributionConfiguration().frameworkUrl(
+                        maven().groupId(GROUP_ID).artifactId(ARTIFACT_ID).version(OLD_VERSION).type("tar.gz"))
+                        .karafVersion(getKarafVersion()).name("Fabric Karaf Distro").unpackDirectory(new File("target/paxexam/unpack/")),
+                useOwnExamBundlesStartLevel(50),
+                editConfigurationFilePut("etc/config.properties", "karaf.startlevel.bundle", "50"),
+                mavenBundle("org.fusesource.tooling.testing","pax-exam-karaf", MavenUtils.getArtifactVersion("org.fusesource.tooling.testing","pax-exam-karaf")),
+                logLevel(LogLevelOption.LogLevel.ERROR),
+                keepRuntimeFolder()
         };
     }
 }
