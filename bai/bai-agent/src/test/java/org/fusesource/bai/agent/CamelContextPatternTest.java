c63d14a615e47b3e8f83ad899e43b774efa2f21f
James Strachan <james.strachan@gmail.com>
2012-08-23 14:25:57 +0100

    added more test cases for the pattern matching

--- bai/bai-agent/src/test/java/org/fusesource/bai/agent/CamelContextPatternTest.java
+++ bai/bai-agent/src/test/java/org/fusesource/bai/agent/CamelContextPatternTest.java
@@ -17,41 +17,67 @@
 package org.fusesource.bai.agent;
 
 import org.fusesource.bai.agent.filters.CamelContextFilters;
-import org.fusesource.bai.agent.filters.CamelContextServiceFilter;
 import org.fusesource.common.util.Filter;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
  */
 public class CamelContextPatternTest {
     @Test
-    public void testPatterns() throws Exception {
+    public void testMatchesPatterns() throws Exception {
         assertMatchesBundle("com.acme.foo.bar", "myContext",
                 "*",
                 "*:*",
                 "com.acme*",
                 "com.acme*:*",
+                "com.acme.foo.bar",
                 "com.acme.foo.bar:*",
                 "com.acme.foo.bar:my*",
                 "com.acme.foo.bar:myContext",
                 "*:myContext",
                 "*:my*"
-                );
+        );
+    }
+
+    @Test
+    public void testNotMatchesPatterns() throws Exception {
+        assertNotMatchesBundle("com.acme.foo.bar", "myContext",
+                "",
+                "a",
+                "a:*",
+                "a*:*",
+                "*:my",
+                "*:x*",
+                "myContext"
+        );
     }
 
     protected static void assertMatchesBundle(String bundleSymbolicName, String camelContextId, String... patterns) {
+        assertBundlePattern(bundleSymbolicName, camelContextId, true, patterns);
+    }
+
+    protected static void assertNotMatchesBundle(String bundleSymbolicName, String camelContextId, String... patterns) {
+        assertBundlePattern(bundleSymbolicName, camelContextId, false, patterns);
+    }
+
+    private static void assertBundlePattern(String bundleSymbolicName, String camelContextId, boolean expected, String[] patterns) {
         for (String pattern : patterns) {
             CamelContextService camelService = new StubCamelContextService(bundleSymbolicName, camelContextId);
-
             Filter<CamelContextService> filter = CamelContextFilters.createCamelContextFilter(pattern);
 
             boolean matches = filter.matches(camelService);
 
-            assertTrue("Did not match pattern " + pattern +
+            String message = "'" + pattern + "'" +
                     " for bundleSymbolicName: " + bundleSymbolicName
-                    + " camelContextId: " + camelContextId, matches);
+                    + " camelContextId: " + camelContextId;
+            if (expected) {
+                assertTrue("Should match pattern " + message, matches);
+            } else {
+                assertFalse("Should not match pattern " + message, matches);
+            }
         }
     }
 }
