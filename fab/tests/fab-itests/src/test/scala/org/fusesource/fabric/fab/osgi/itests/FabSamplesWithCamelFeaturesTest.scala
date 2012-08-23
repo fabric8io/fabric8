d088802db14dfef37fbdf8c05ee233320d8e8de0
James Strachan <james.strachan@gmail.com>
2012-08-23 13:20:17 +0100

    fixed broken test - bad james!

--- fab/tests/fab-itests/src/test/scala/org/fusesource/fabric/fab/osgi/itests/FabSamplesWithCamelFeaturesTest.scala
+++ fab/tests/fab-itests/src/test/scala/org/fusesource/fabric/fab/osgi/itests/FabSamplesWithCamelFeaturesTest.scala
@@ -80,6 +80,7 @@ class FabSamplesWithCamelFeaturesTest {
 
     // and then add a few extra bundles to it to enable Scala- and FAB-support
     mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.scala-library").versionAsInProject(),
+    mavenBundle("org.fusesource.common", "common-util").versionAsInProject(),
     mavenBundle("org.fusesource.fabric.fab", "fab-osgi").versionAsInProject()
   )
 
