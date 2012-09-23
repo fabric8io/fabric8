294729c39f1dcd387e89d739b86b5efae72765b6
Ioannis Canellos <iocanel@apache.org>
2012-09-23 19:40:56 +0300

    Fixed a node not exists exception.

--- fabric/fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/ZkPath.java
+++ fabric/fabric-zookeeper/src/main/java/org/fusesource/fabric/zookeeper/ZkPath.java
@@ -170,10 +170,10 @@ public enum ZkPath {
         return rc;
     }
 
-    public static void createContainerPaths(IZKClient zooKeeper, String container, String version, String profile) throws InterruptedException, KeeperException {
+    public static void createContainerPaths(IZKClient zooKeeper, String container, String version, String profiles) throws InterruptedException, KeeperException {
         boolean versionProvided = version != null;
         if (version == null) {
-            version = ZooKeeperUtils.get(zooKeeper, CONFIG_DEFAULT_VERSION.getPath());
+            version = ZooKeeperUtils.get(zooKeeper, CONFIG_VERSION.getPath(container));
         }
 
         if (version != null) {
@@ -182,7 +182,7 @@ public enum ZkPath {
             }
 
             if (zooKeeper.exists(ZkPath.CONFIG_VERSIONS_CONTAINER.getPath(version, container)) == null || versionProvided) {
-                ZooKeeperUtils.set(zooKeeper, ZkPath.CONFIG_VERSIONS_CONTAINER.getPath(version, container), profile);
+                ZooKeeperUtils.set(zooKeeper, ZkPath.CONFIG_VERSIONS_CONTAINER.getPath(version, container), profiles);
             }
         }
     }
