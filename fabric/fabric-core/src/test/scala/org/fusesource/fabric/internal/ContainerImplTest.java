cf92e1f7c09103229f6ab564e82f541698e6c7fa
Stan Lewis <slewis@fusesource.com>
2012-11-06 09:41:17 -0500

    Fix ContainerImplTest failure

--- fabric/fabric-core/src/test/scala/org/fusesource/fabric/internal/ContainerImplTest.java
+++ fabric/fabric-core/src/test/scala/org/fusesource/fabric/internal/ContainerImplTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.fusesource.fabric.internal;
 
 
@@ -87,7 +103,7 @@ public class ContainerImplTest {
 
         Profile[] profiles = container.getProfiles();
         assertNotNull(profiles);
-        assertEquals(1, profiles.length);
+        assertEquals(2, profiles.length);
         assertEquals("camel", profiles[0].getId());
         verify(izkClient);
     }
