cac3423d1d97d12ace954f0b9629c7e9c1e43cb2
Dejan Bosanac <dejan@nighttale.net>
2013-03-12 14:39:05 +0100

    [ENTMQ-185] MQProfileTest

--- fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/mq/MQProfileTest.java
+++ fabric/fabric-itests/fabric-pax-exam/src/test/java/org/fusesource/fabric/itests/paxexam/mq/MQProfileTest.java
@@ -5,7 +5,6 @@ import org.fusesource.fabric.api.Container;
 import org.fusesource.fabric.itests.paxexam.FabricFeaturesTest;
 import org.fusesource.fabric.itests.paxexam.support.ContainerBuilder;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
@@ -29,6 +28,10 @@ public class MQProfileTest extends FabricFeaturesTest {
 
     @After
     public void tearDown() throws InterruptedException {
+        for (Container container : containers) {
+            System.out.println("destroying " + container.getId());
+            destroyChildContainer(container.getId());
+        }
         ContainerBuilder.destroy();
     }
 
@@ -58,15 +61,16 @@ public class MQProfileTest extends FabricFeaturesTest {
         assertEquals("Producer not present", 1, bean.getTotalProducerCount());
         assertEquals("Consumer not present", 1, bean.getTotalConsumerCount());
     }
-    
-    
-    @Ignore // JIRA ESB-1687
+
     @Test
     public void testMQCreateBasic() throws Exception {
         System.err.println(executeCommand("fabric:create -n"));
-        addStagingRepoToDefaultProfile();
+        //addStagingRepoToDefaultProfile();
 
-        executeCommand("mq-create --create-container mq1 mq1");
+        System.err.println(executeCommand("mq-create --create-container mq1 --jmx-user admin --jmx-password admin mq1"));
+        // give it a bit time
+        Thread.sleep(5000);
+
         Container container = getFabricService().getContainer("mq1");
         containers.add(container);
         waitForProvisionSuccess(container, PROVISION_TIMEOUT);
@@ -76,20 +80,19 @@ public class MQProfileTest extends FabricFeaturesTest {
 
         // give it a bit time
         Thread.sleep(3000);
+        installAndCheckFeature("activemq");
 
         // check jmx stats
-        BrokerViewMBean bean = (BrokerViewMBean)getMBean(container, new ObjectName("org.apache.activemq:Type=Broker,BrokerName=mq1"), BrokerViewMBean.class);
+        BrokerViewMBean bean = (BrokerViewMBean)getMBean(container, new ObjectName("org.apache.activemq:type=Broker,brokerName=mq1"), BrokerViewMBean.class);
         assertEquals("Producer not present", 1, bean.getTotalProducerCount());
         assertEquals("Consumer not present", 1, bean.getTotalConsumerCount());
     }
 
-    @Ignore // JIRA ESB-1687
     @Test
     public void testMQCreateMS() throws Exception {
         System.err.println(executeCommand("fabric:create -n"));
-        addStagingRepoToDefaultProfile();
 
-        executeCommand("mq-create --create-container broker1,broker2 ms-broker");
+        executeCommand("mq-create --create-container broker1,broker2 --jmx-user admin --jmx-password admin ms-broker");
         Container container1 = getFabricService().getContainer("broker1");
         containers.add(container1);
         waitForProvisionSuccess(container1, PROVISION_TIMEOUT);
@@ -98,34 +101,35 @@ public class MQProfileTest extends FabricFeaturesTest {
         containers.add(container2);
         waitForProvisionSuccess(container2, PROVISION_TIMEOUT);
 
-        BrokerViewMBean broker1 = (BrokerViewMBean)getMBean(container1, new ObjectName("org.apache.activemq:Type=Broker,BrokerName=ms-broker"), BrokerViewMBean.class);
+        installAndCheckFeature("activemq");
+
+        BrokerViewMBean broker1 = (BrokerViewMBean)getMBean(container1, new ObjectName("org.apache.activemq:type=Broker,brokerName=ms-broker"), BrokerViewMBean.class);
 
         assertEquals("ms-broker", broker1.getBrokerName());
         destroyChildContainer("broker1");
         containers.remove(container1);
         Thread.sleep(10000); // TODO implement wait for condition with timeout
 
-        BrokerViewMBean broker2 = (BrokerViewMBean)getMBean(container2, new ObjectName("org.apache.activemq:Type=Broker,BrokerName=ms-broker"), BrokerViewMBean.class);
+        BrokerViewMBean broker2 = (BrokerViewMBean)getMBean(container2, new ObjectName("org.apache.activemq:type=Broker,brokerName=ms-broker"), BrokerViewMBean.class);
         assertEquals("ms-broker", broker2.getBrokerName());
 
         //TODO add example to verify failover
 
     }
 
-    @Ignore  // JIRA ESB-1687
     @Test
     public void testMQCreateNetwork() throws Exception {
         System.err.println(executeCommand("fabric:create -n"));
         addStagingRepoToDefaultProfile();
 
-        executeCommand("mq-create --group east --networks west --create-container east east");
-        Container container1 = getFabricService().getContainer("east");
+        executeCommand("mq-create --group us-east --networks us-west --create-container us-east --jmx-user admin --jmx-password admin --networks-username admin --networks-password admin us-east");
+        Container container1 = getFabricService().getContainer("us-east");
         containers.add(container1);
         waitForProvisionSuccess(container1, PROVISION_TIMEOUT);
 
 
-        executeCommand("mq-create --group west --networks east --create-container west west");
-        Container container2 = getFabricService().getContainer("west");
+        executeCommand("mq-create --group us-west --networks us-east --create-container us-west --jmx-user admin --jmx-password admin --networks-username admin --networks-password admin us-west");
+        Container container2 = getFabricService().getContainer("us-west");
         containers.add(container2);
         waitForProvisionSuccess(container2, PROVISION_TIMEOUT);
 
@@ -133,17 +137,14 @@ public class MQProfileTest extends FabricFeaturesTest {
 
         // give it a bit time
         Thread.sleep(10000);
+        installAndCheckFeature("activemq");
 
-        BrokerViewMBean brokerEast = (BrokerViewMBean)getMBean(container1, new ObjectName("org.apache.activemq:Type=Broker,BrokerName=east"), BrokerViewMBean.class);
-        BrokerViewMBean brokerWest = (BrokerViewMBean)getMBean(container2, new ObjectName("org.apache.activemq:Type=Broker,BrokerName=west"), BrokerViewMBean.class);
+        BrokerViewMBean brokerEast = (BrokerViewMBean)getMBean(container1, new ObjectName("org.apache.activemq:type=Broker,brokerName=us-east"), BrokerViewMBean.class);
+        BrokerViewMBean brokerWest = (BrokerViewMBean)getMBean(container2, new ObjectName("org.apache.activemq:type=Broker,brokerName=us-west"), BrokerViewMBean.class);
 
         assertFalse("Messages not sent", brokerEast.getTotalEnqueueCount() == 0);
 
         assertFalse("Messages not received", brokerWest.getTotalDequeueCount() == 0);
-
-
-
-
 
     }
 
