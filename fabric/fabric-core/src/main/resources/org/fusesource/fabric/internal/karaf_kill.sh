5caefcc5ab5486beb88c0f3001c5ea2b0ab3838e
Ioannis Canellos <iocanel@apache.org>
2013-03-14 16:02:42 +0200

    [FABRIC-407] Fix kill script so that it can wait on processes that are not children of the current shell.

--- fabric/fabric-core/src/main/resources/org/fusesource/fabric/internal/karaf_kill.sh
+++ fabric/fabric-core/src/main/resources/org/fusesource/fabric/internal/karaf_kill.sh
@@ -3,5 +3,13 @@ function karaf_kill() {
    INSTANCES_FILE=$KARAF_HOME/instances/instance.properties
    PID=`cat $INSTANCES_FILE | grep "item.0.pid" | awk -F "=" '{print $2}'`
    kill $PID
-   wait $PID
+      for i in {1..5};
+        do
+            if ps -p $PID > /dev/null; then
+                echo "Fabric has been successfully stopped"
+                break
+            else
+                sleep 3
+            fi
+        done
 }
