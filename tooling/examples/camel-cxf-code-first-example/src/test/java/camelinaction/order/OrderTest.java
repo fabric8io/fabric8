aec301beafbae3a8e8922355a3a9b346a5e2cd1e
Willem Jiang <willem.jiang@gmail.com>
2013-03-21 22:14:05 +0800

    ENTESB-652 fixed the issue of cxf-codefirst archetype

--- tooling/examples/camel-cxf-code-first-example/src/test/java/camelinaction/order/OrderTest.java
+++ tooling/examples/camel-cxf-code-first-example/src/test/java/camelinaction/order/OrderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package camelinaction;
+package camelinaction.order;
 
 import java.util.ArrayList;
 import java.util.List;
