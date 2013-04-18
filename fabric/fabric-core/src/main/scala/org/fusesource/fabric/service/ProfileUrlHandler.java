add725273af08612f54a98819e5cac390619b38d
Guillaume Nodet <gnodet@gmail.com>
2013-04-18 11:17:40 +0200

    Use the new profile url handler for the jetty.xml

--- fabric/fabric-core/src/main/scala/org/fusesource/fabric/service/ProfileUrlHandler.java
+++ fabric/fabric-core/src/main/scala/org/fusesource/fabric/service/ProfileUrlHandler.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.fusesource.fabric.service;
 
 import org.fusesource.fabric.api.FabricService;
@@ -10,11 +27,12 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 public class ProfileUrlHandler extends AbstractURLStreamHandlerService {
 
     private final FabricService fabricService;
-    private static final String SYNTAX = "profile:<profile name>/<resource name>";
+    private static final String SYNTAX = "profile:<resource name>";
 
     public ProfileUrlHandler(FabricService fabricService) {
         this.fabricService = fabricService;
@@ -49,8 +67,9 @@ public class ProfileUrlHandler extends AbstractURLStreamHandlerService {
             String path = url.getPath();
             Profile profile = fabricService.getCurrentContainer().getOverlayProfile();
 
-            if (profile.getFileConfigurations().containsKey(path)) {
-                byte[] b = profile.getFileConfigurations().get(path);
+            Map<String, byte[]> configs = profile.getFileConfigurations();
+            if (configs.containsKey(path)) {
+                byte[] b = configs.get(path);
                 return new ByteArrayInputStream(b);
             } else {
                 throw new IllegalArgumentException("Resource " + path + " does not exist in the profile overlay.");
