4dad2f390627b09e50d4642aa8ec32d3ecb61127
Guillaume Nodet <gnodet@gmail.com>
2013-06-07 16:35:17 +0200

    Fix wrong http url registered for containers

--- fabric/fabric-core/src/main/scala/org/fusesource/fabric/service/KarafContainerRegistration.java
+++ fabric/fabric-core/src/main/scala/org/fusesource/fabric/service/KarafContainerRegistration.java
@@ -499,7 +499,7 @@ public class
                 if (event.getPid().equals(HTTP_PID) && event.getType() == ConfigurationEvent.CM_UPDATED) {
                     Configuration config = configurationAdmin.getConfiguration(HTTP_PID);
                     int httpPort = Integer.parseInt((String) config.getProperties().get(HTTP_KEY));
-                    String httpUrl = getSshUrl(name, httpPort);
+                    String httpUrl = getHttpUrl(name, httpPort);
                     setData(curator, CONTAINER_HTTP.getPath(name), httpUrl);
                     if (fabricService.getPortService().lookupPort(current, HTTP_PID, HTTP_KEY) != httpPort) {
                         fabricService.getPortService().unRegisterPort(current, HTTP_PID);
