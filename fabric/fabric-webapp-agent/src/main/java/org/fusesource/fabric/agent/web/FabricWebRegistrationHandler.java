7bdffd6ea64077beb247df818dc12a7c062c1ec2
James Strachan <james.strachan@gmail.com>
2013-06-25 07:06:15 +0100

    first working spike of a fabric webapp agent that registers tomcat based web apps into the ZK registry

--- fabric/fabric-webapp-agent/src/main/java/org/fusesource/fabric/agent/web/FabricWebRegistrationHandler.java
+++ fabric/fabric-webapp-agent/src/main/java/org/fusesource/fabric/agent/web/FabricWebRegistrationHandler.java
@@ -16,12 +16,28 @@
  */
 package org.fusesource.fabric.agent.web;
 
+import java.lang.management.ManagementFactory;
+import java.util.HashSet;
+import java.util.Set;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerDelegate;
+import javax.management.MalformedObjectNameException;
+import javax.management.Notification;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.QueryExp;
+
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.zookeeper.CreateMode;
 import org.fusesource.fabric.api.FabricService;
+import org.fusesource.fabric.zookeeper.ZkPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.fusesource.fabric.zookeeper.utils.ZooKeeperUtils.setData;
 
 public class FabricWebRegistrationHandler implements ConnectionStateListener {
 
@@ -29,105 +45,166 @@ public class FabricWebRegistrationHandler implements ConnectionStateListener {
 
     private FabricService fabricService;
     private CuratorFramework curator;
+    private NotificationListener listener;
+    private NotificationFilter filter;
+    private MBeanServer mBeanServer;
+    private int port = -1;
+
+    // TODO load from system properties
+    String containerId = "tomcat1";
+    String host = "localhost";
 
 
-    public void init() {
-        LOGGER.info("Initialising " + this + " with fabricService: " + fabricService + " and curator: " + curator);
+    public void init() throws Exception {
+        LOGGER.info("Initialising " + this + " with fabricService: " + fabricService + " and curator: "
+                + curator);
+
+        if (mBeanServer == null) {
+            mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        }
+
+        if (mBeanServer != null) {
+            Object handback = null;
+            listener = getNotificationListener();
+            filter = getNotificationFilter();
+
+            mBeanServer
+                    .addNotificationListener(MBeanServerDelegate.DELEGATE_NAME, listener, filter, handback);
+        }
+        findWebAppMBeans();
     }
 
-    public void destroy() {
+    public void destroy() throws Exception {
+        if (mBeanServer != null) {
+            if (listener != null) {
+                mBeanServer.removeNotificationListener(MBeanServerDelegate.DELEGATE_NAME, listener);
+            }
+        }
     }
+
+    /**
+     * Lets query JMX for any MBeans to define web apps in containers like Tomcat, Jetty, JBoss
+     */
+    protected void findWebAppMBeans() {
+        try {
+            if (port < 0) {
+                // lets try find the port
+                Set<ObjectName> objectNames = findObjectNames(null, "Catalina:type=Connector,*",
+                        "Tomcat:type=Connector,*");
+
+                for (ObjectName objectName : objectNames) {
+                    Object protocol = mBeanServer.getAttribute(objectName, "protocol");
+                    if (protocol != null && protocol instanceof String && protocol.toString().toLowerCase()
+                            .startsWith("http/")) {
+                        Object portValue = mBeanServer.getAttribute(objectName, "port");
+                        if (portValue instanceof Number) {
+                            port = ((Number)portValue).intValue();
+                            if (port > 0) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (port > 0 && curator != null) {
+                Set<ObjectName> objectNames = findObjectNames(null, "Catalina:j2eeType=WebModule,*", "Tomcat:j2eeType=WebModule,*");
+                for (ObjectName objectName : objectNames) {
+                    Object pathValue = mBeanServer.getAttribute(objectName, "path");
+                    if (pathValue != null && pathValue instanceof String) {
+                        String path = pathValue.toString();
+
+                        String url = "http://" + host + ":" + port + path;
+                        String name = path;
+                        while (name.startsWith("/")) {
+                            name = name.substring(1);
+                        }
+
+                        // lets try figure out the group / version...
+                        String version = "unknown";
+                        String[] versionSplit = name.split("-\\d\\.\\d");
+                        if (versionSplit.length < 2) {
+                            versionSplit = name.split("-SNAPSHOT");
+                        }
+                        if (versionSplit.length > 1) {
+                            String justName =versionSplit[0];
+                            version = name.substring(justName.length() + 1);
+                            name = justName;
+                        }
+                        LOGGER.info("Found name " + name + " version " + version + " url " + url + " from path " + path);
+                        if (name.length() > 0) {
+                            registerWebapp(name, version, url);
+                        }
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            LOGGER.warn("Failed to poll for web app mbeans " + e, e);
+        }
+    }
+
+    protected Set<ObjectName> findObjectNames(QueryExp queryExp, String... objectNames)
+            throws MalformedObjectNameException {
+        Set<ObjectName> answer = new HashSet<ObjectName>();
+        for (String objectName : objectNames) {
+            Set<ObjectName> set = mBeanServer.queryNames(new ObjectName(objectName), queryExp);
+            if (set != null) {
+                answer.addAll(set);
+            }
+        }
+        return answer;
+    }
+
+    protected NotificationListener getNotificationListener() {
+        return new NotificationListener() {
+            @Override
+            public void handleNotification(Notification notification, Object handback) {
+                findWebAppMBeans();
+            }
+        };
+    }
+
+    protected NotificationFilter getNotificationFilter() {
+        return new NotificationFilter() {
+            @Override
+            public boolean isNotificationEnabled(Notification notification) {
+                return true;
+            }
+        };
+    }
+
 
     /**
      * Registers a webapp to the registry.
      */
-    void registerWebapp() {
-        String id = "";
-        String url = "";
-
-
-        // Catalina:type=Connector,port=8080
-
-        // TODO lets query the HTTP port...
-
-
-
-/*
-        String id = container.getId();
-        String url = container.getHttpUrl() + webEvent.getContextPath();
-
-        if (!url.startsWith("http")) {
-            url = "http://" + url;
-        }
-
-        String name = webEvent.getBundle().getSymbolicName();
-
-        if (name.equals("org.jolokia")) {
-            container.setJolokiaUrl(url);
-            System.setProperty("jolokia.agent", url);
-        }
-*/
-
-/*
-        String json = "{\"id\":\"" + id + "\", \"services\":[\"" + url + "\"],\"container\":\"" + id + "\"}";
+    protected void registerWebapp(String name, String version, String url) {
+        String json = "{\"containerId\":\"" + containerId + "\", \"services\":[\"" + url
+                + "\"],\"container\":\"" + containerId + "\"}";
         try {
-            setData(curator, ZkPath.WEBAPPS_CONTAINER.getPath(name,
-                    webEvent.getBundle().getVersion().toString(), id), json, CreateMode.EPHEMERAL);
+            String zkPath = ZkPath.WEBAPPS_CONTAINER.getPath(name, version, containerId);
+            setData(curator, zkPath, json, CreateMode.EPHEMERAL);
         } catch (Exception e) {
-            LOGGER.error("Failed to register webapp {}.", webEvent.getContextPath(), e);
+            LOGGER.error("Failed to register webapp {}.", json, e);
         }
-*/
     }
 
-
-    /**
-     * Unregister a webapp from the registry.
-     */
-    void unRegisterWebapp() {
-/*
-        try {
-            String name = webEvent.getBundle().getSymbolicName();
-            if (name.equals("org.jolokia")) {
-                container.setJolokiaUrl(null);
-                System.clearProperty("jolokia.agent");
-            }
-
-            delete(curator, ZkPath.WEBAPPS_CONTAINER.getPath(name,
-                    webEvent.getBundle().getVersion().toString(), container.getId()));
-        } catch (KeeperException.NoNodeException e) {
-            // If the node does not exists, ignore the exception
-        } catch (Exception e) {
-            LOGGER.error("Failed to unregister webapp {}.", webEvent.getContextPath(), e);
-        }
-*/
-    }
 
     @Override
     public void stateChanged(CuratorFramework client, ConnectionState newState) {
         switch (newState) {
-            case CONNECTED:
-            case RECONNECTED:
-                this.curator = client;
-                onConnected();
-                break;
-            default:
-                onDisconnected();
-                this.curator = null;
-                break;
+        case CONNECTED:
+        case RECONNECTED:
+            this.curator = client;
+            onConnected();
+            break;
+        default:
+            onDisconnected();
+            this.curator = null;
+            break;
         }
     }
 
     public void onConnected() {
-/*
-        for (WebEvent event : webEvents.values()) {
-            webEvent(event);
-        }
-        for (Map<String, ServletEvent> map : servletEvents.values()) {
-            for (ServletEvent event : map.values()) {
-                servletEvent(event);
-            }
-        }
-*/
     }
 
     public void onDisconnected() {
