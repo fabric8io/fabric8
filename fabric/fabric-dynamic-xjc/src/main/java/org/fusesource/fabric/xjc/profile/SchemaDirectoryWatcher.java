73312d1268aeff26162177286a4ef80e02889c2f
James Strachan <james.strachan@gmail.com>
2013-07-23 15:49:27 +0100

    almost working example-dozer profile for #38; just the class loader pieces are not quite working yet so we can't load in the jaxb context (due to jaxb being missing it seems)

--- fabric/fabric-dynamic-xjc/src/main/java/org/fusesource/fabric/xjc/profile/SchemaDirectoryWatcher.java
+++ fabric/fabric-dynamic-xjc/src/main/java/org/fusesource/fabric/xjc/profile/SchemaDirectoryWatcher.java
@@ -17,19 +17,35 @@
 package org.fusesource.fabric.xjc.profile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.google.common.base.Throwables;
 import com.google.common.io.Closeables;
 
+import org.apache.aries.util.AriesFrameworkUtil;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.PathChildrenCache;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
-import org.fusesource.fabric.zookeeper.utils.ZooKeeperUtils;
+import org.fusesource.fabric.api.Container;
+import org.fusesource.fabric.api.FabricService;
+import org.fusesource.fabric.api.Profile;
+import org.fusesource.fabric.xjc.CompileResults;
+import org.fusesource.fabric.xjc.DynamicXJC;
+import org.fusesource.fabric.zookeeper.ZkPath;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,62 +54,170 @@ import org.slf4j.LoggerFactory;
 public class SchemaDirectoryWatcher implements PathChildrenCacheListener {
     private static final transient Logger LOG = LoggerFactory.getLogger(SchemaDirectoryWatcher.class);
 
-    private final CuratorFramework curator;
-    private final PathChildrenCache pathCache;
-    private final String schemaPath;
-    private final ExecutorService executorService = Executors.newFixedThreadPool(2);
+    private CuratorFramework curator;
+    private String schemaPath;
+    private BundleContext bundleContext;
+    private FabricService fabricService;
+    private Timer timer = new Timer();
+    private AtomicBoolean startedFlag = new AtomicBoolean(false);
 
-    public SchemaDirectoryWatcher(ProfileDynamicXJc xjc, String schemaPath) {
-        this.schemaPath = schemaPath;
-        curator = xjc.getCurator();
-        this.pathCache = new PathChildrenCache(curator, schemaPath, true, false, executorService);
+    private ExecutorService executorService = Executors.newFixedThreadPool(2);
+    private final ConcurrentMap<String, PathChildrenCache>
+            pathCacheMap = new ConcurrentHashMap<String, PathChildrenCache>();
+    private long timerDelay = 1000;
+
+    public SchemaDirectoryWatcher() {
     }
 
     public void start() {
-         try {
-             ZooKeeperUtils.createDefault(curator, schemaPath, null);
-             pathCache.getListenable().addListener(this);
-             pathCache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
-             pathCache.rebuild();
-         } catch (Exception e) {
-             throw Throwables.propagate(e);
-         }
-     }
+        try {
+            watchSchemaFolders();
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
 
-     public void stop() throws IOException {
-         Closeables.close(pathCache, true);
-         executorService.shutdown();
-     }
+    public void stop() throws IOException {
+        for (Map.Entry<String, PathChildrenCache> entry : pathCacheMap.entrySet()) {
+            PathChildrenCache pathCache = entry.getValue();
+            Closeables.close(pathCache, true);
+        }
+        pathCacheMap.clear();
+        executorService.shutdown();
+        timer.cancel();
+    }
 
 
     public void childEvent(CuratorFramework curator, PathChildrenCacheEvent event)
             throws Exception {
         switch (event.getType()) {
-            case INITIALIZED:
-            case CHILD_ADDED:
-            case CHILD_REMOVED:
-                executorService.submit(new RecompileTask());
-                break;
+        case INITIALIZED:
+        case CHILD_ADDED:
+        case CHILD_REMOVED:
+            asyncRecompile();
+            break;
         }
 
     }
 
 
-    private class RecompileTask implements Runnable {
+    // Properties
+    //-------------------------------------------------------------------------
+
+    public FabricService getFabricService() {
+        return fabricService;
+    }
+
+    public void setFabricService(FabricService fabricService) {
+        this.fabricService = fabricService;
+    }
+
+    public CuratorFramework getCurator() {
+        return curator;
+    }
+
+    public void setCurator(CuratorFramework curator) {
+        this.curator = curator;
+    }
+
+    public BundleContext getBundleContext() {
+        return bundleContext;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    public String getSchemaPath() {
+        return schemaPath;
+    }
+
+    public void setSchemaPath(String schemaPath) {
+        this.schemaPath = schemaPath;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public long getTimerDelay() {
+        return timerDelay;
+    }
+
+    public void setTimerDelay(long timerDelay) {
+        this.timerDelay = timerDelay;
+    }
+
+
+    // Implementation
+    //-------------------------------------------------------------------------
+
+    protected void asyncRecompile() {
+        if (startedFlag.compareAndSet(false, true)) {
+            timer.schedule(new RecompileTask(), timerDelay);
+        }
+    }
+
+
+    /**
+     * Lets try resolve the current {@link #getSchemaPath()} based on the profile paths
+     */
+    protected void watchSchemaFolders() throws Exception {
+        Container container = fabricService.getCurrentContainer();
+        String version = container.getVersion().getId();
+        Profile[] profiles = container.getProfiles();
+        for (Profile profile : profiles) {
+            String profileId = profile.getId();
+            String profilePath = ZkPath.getProfilePath(version, profileId);
+            String path = profilePath + "/" + schemaPath;
+            if (curator.checkExists().forPath(path) != null) {
+                LOG.info("Starting SchemaDirectoryWatcher on path " + path);
+                PathChildrenCache pathCache = new PathChildrenCache(curator, path, true, false,
+                        executorService);
+                pathCacheMap.put(path, pathCache);
+                pathCache.getListenable().addListener(this);
+                pathCache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+                pathCache.rebuild();
+            }
+        }
+    }
+
+    private class RecompileTask extends TimerTask {
         @Override
         public void run() {
             recompile();
         }
     }
 
-    protected  void recompile() {
-        System.out.println("Recompiling the XSDs!");
+    protected void recompile() {
+        LOG.debug("Looking for XSDs to recompile");
 
-        List<ChildData> childData = pathCache.getCurrentData();
-        int totalItems = childData.size();
-        for (ChildData data : childData) {
-            String path = data.getPath();
-            System.out.println("Has path: " + path);
+        Set<String> urls = new TreeSet<String>();
+        List<PathChildrenCache> pathCaches = new ArrayList<PathChildrenCache>(pathCacheMap.values());
+        for (PathChildrenCache pathCache : pathCaches) {
+            List<ChildData> childData = pathCache.getCurrentData();
+            int totalItems = childData.size();
+            for (ChildData data : childData) {
+                String path = data.getPath();
+                urls.add("zk:" + path);
+            }
         }
+        LOG.info("Recompiling XSDs at URLs: " + urls);
+        startedFlag.set(false);
+
+        ClassLoader classLoader = AriesFrameworkUtil.getClassLoader(bundleContext.getBundle());
+        if (classLoader == null) {
+            System.out.println("No class loader!");
+            classLoader = getClass().getClassLoader();
+        }
+        System.out.println("Class loader : " + classLoader);
+        DynamicXJC xjc = new DynamicXJC(classLoader);
+        xjc.setSchemaUrls(new ArrayList<String>(urls));
+        CompileResults compileResults = xjc.compileSchemas();
+        System.out.println("got results: " + compileResults.getJAXBContext());
     }
 }
