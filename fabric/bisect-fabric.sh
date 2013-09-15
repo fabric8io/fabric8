5fec53a84ffaffcc006c5c1958daff62f1ebb283
Thomas Diesler <thomas.diesler@jboss.com>
2013-09-15 14:02:03 +0200

    Add git bisect support
    Usage
    
     cp fabric/bisect-fabric.sh bisect-fabric.sh
     git bisect start [badrev] [goodrev]
     git bisect run ./bisect-fabric.sh

---
+++ fabric/bisect-fabric.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# 
+# Usage
+#
+# cp fabric/bisect-fabric.sh bisect-fabric.sh
+# git bisect start [badrev] [goodrev]
+# git bisect run ./bisect-fabric.sh
+#
+# @author thomas.diesler@jboss.com
+# @since 15-Sep-2013
+
+mvn -pl fabric/fabric-aggregator -am -ff clean install | tee mvn.out
+
+killall java
+
+MVN_RESULT=`cat mvn.out | grep -o "BUILD SUCCESS\|BUILD FAILURE"`
+echo $MVN_RESULT
+
+if [ "$MVN_RESULT" = "BUILD SUCCESS" ]; then
+   exit 0
+else
+   exit 1
+fi
