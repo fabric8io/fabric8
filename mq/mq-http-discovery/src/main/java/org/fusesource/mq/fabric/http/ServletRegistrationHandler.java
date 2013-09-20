1bfea0eb1ee7ac3ec1e58e82b36bf06e9d4ddabc
Thomas Diesler <thomas.diesler@jboss.com>
2013-09-20 17:31:14 +0200

    @ThreadSafe ServletRegistrationHandler

--- mq/mq-http-discovery/src/main/java/org/fusesource/mq/fabric/http/ServletRegistrationHandler.java
+++ mq/mq-http-discovery/src/main/java/org/fusesource/mq/fabric/http/ServletRegistrationHandler.java
@@ -9,6 +9,7 @@ import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
+import org.fusesource.fabric.api.jcip.ThreadSafe;
 import org.fusesource.fabric.api.scr.AbstractComponent;
 import org.fusesource.fabric.api.scr.ValidatingReference;
 import org.osgi.service.component.ComponentContext;
@@ -17,10 +18,9 @@ import org.osgi.service.http.HttpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- */
-@Component(name = "org.fusesource.mq.fabric.http", description = "Fabric Discovery Servlet", immediate = true)
-public class ServletRegistrationHandler extends AbstractComponent {
+@ThreadSafe
+@Component(name = "org.fusesource.mq.fabric.http", description = "Fabric Discovery Servlet", immediate = true) // Done
+public final class ServletRegistrationHandler extends AbstractComponent {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ServletRegistrationHandler.class);
 
@@ -30,34 +30,30 @@ public class ServletRegistrationHandler extends AbstractComponent {
     private final ValidatingReference<CuratorFramework> curator = new ValidatingReference<CuratorFramework>();
 
     @Activate
-    synchronized void activate(ComponentContext context, Map<String, String> properties) {
-        activateComponent();
+    void activate(ComponentContext context, Map<String, String> properties) {
         try {
             FabricDiscoveryServlet discoveryServlet = new FabricDiscoveryServlet();
             discoveryServlet.setCurator(curator.get());
             HttpContext base = httpService.get().createDefaultHttpContext();
             httpService.get().registerServlet("/mq-discovery", discoveryServlet, createParams("mq-discovery"), base);
         } catch (Throwable t) {
-            deactivateComponent();
             LOGGER.warn("Failed to register fabric maven proxy servlets, due to:" + t.getMessage());
         }
+        activateComponent();
     }
 
     @Deactivate
-    synchronized void deactivate() {
+    void deactivate() {
+        deactivateComponent();
         try {
-            try {
-                httpService.get().unregister("/mq-discovery");
-            } catch (Exception ex) {
-                LOGGER.warn("Http service returned error on servlet unregister. Possibly the service has already been stopped");
-            }
-        } finally {
-            deactivateComponent();
+            httpService.get().unregister("/mq-discovery");
+        } catch (Exception ex) {
+            LOGGER.warn("Http service returned error on servlet unregister.");
         }
     }
 
-    private Dictionary createParams(String name) {
-        Dictionary d = new Hashtable();
+    private Dictionary<String, String> createParams(String name) {
+        Dictionary<String, String> d = new Hashtable<String, String>();
         d.put("servlet-name", name);
         return d;
     }
