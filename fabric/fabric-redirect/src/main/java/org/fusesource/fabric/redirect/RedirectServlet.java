81461cdd06cf8e081d5c64180a45f19381fe8a3c
James Strachan <james.strachan@gmail.com>
2013-10-07 15:31:33 +0100

    fixes #123

--- fabric/fabric-redirect/src/main/java/org/fusesource/fabric/redirect/RedirectServlet.java
+++ fabric/fabric-redirect/src/main/java/org/fusesource/fabric/redirect/RedirectServlet.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
