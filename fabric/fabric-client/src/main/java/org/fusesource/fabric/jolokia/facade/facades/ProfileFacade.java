05a31e600faa44b89cae05cc25dbb30be516e5f5
Lars Heinemann <lhein@apache.org>
2013-10-09 13:13:38 +0200

    fixed some method calls to include the version id

--- fabric/fabric-client/src/main/java/org/fusesource/fabric/jolokia/facade/facades/ProfileFacade.java
+++ fabric/fabric-client/src/main/java/org/fusesource/fabric/jolokia/facade/facades/ProfileFacade.java
@@ -163,27 +163,27 @@ public class ProfileFacade implements Profile, HasId {
 
     @Override
     public void setBundles(List<String> strings) {
-        Void v = Helpers.exec(j4p, "setProfileBundles(java.lang.String, java.lang.String, java.util.List)", id, strings);
+        Void v = Helpers.exec(j4p, "setProfileBundles(java.lang.String, java.lang.String, java.util.List)", versionId, id, strings);
     }
 
     @Override
     public void setFabs(List<String> strings) {
-        Void v = Helpers.exec(j4p, "setProfileFabs(java.lang.String, java.lang.String, java.util.List)", id, strings);
+        Void v = Helpers.exec(j4p, "setProfileFabs(java.lang.String, java.lang.String, java.util.List)", versionId, id, strings);
     }
 
     @Override
     public void setFeatures(List<String> strings) {
-        Void v = Helpers.exec(j4p, "setProfileFeatures(java.lang.String, java.lang.String, java.util.List)", id, strings);
+        Void v = Helpers.exec(j4p, "setProfileFeatures(java.lang.String, java.lang.String, java.util.List)", versionId, id, strings);
     }
 
     @Override
     public void setRepositories(List<String> strings) {
-        Void v = Helpers.exec(j4p, "setProfileRepositories(java.lang.String, java.lang.String, java.util.List)", id, strings);
+        Void v = Helpers.exec(j4p, "setProfileRepositories(java.lang.String, java.lang.String, java.util.List)", versionId, id, strings);
     }
 
     @Override
     public void setOverrides(List<String> strings) {
-        Void v = Helpers.exec(j4p, "setProfileOverrides(java.lang.String, java.lang.String, java.util.List)", id, strings);
+        Void v = Helpers.exec(j4p, "setProfileOverrides(java.lang.String, java.lang.String, java.util.List)", versionId, id, strings);
     }
 
     @Override
