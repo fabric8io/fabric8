2b84502556cdbc7cd5b0331649459f779c36b799
James Strachan <james.strachan@gmail.com>
2013-10-15 20:18:29 +0100

    initial spike of #137 though not using it yet in the profile metadata due to issue with child containers

---
+++ fabric/fabric-core/src/main/java/org/fusesource/fabric/service/VersionPropertyPointerResolver.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fusesource.fabric.service;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.fusesource.fabric.api.DataStore;
+import org.fusesource.fabric.api.FabricService;
+import org.fusesource.fabric.api.PlaceholderResolver;
+import org.fusesource.fabric.api.Profile;
+import org.fusesource.fabric.api.jcip.ThreadSafe;
+import org.fusesource.fabric.api.scr.AbstractComponent;
+import org.fusesource.fabric.api.scr.ValidatingReference;
+import org.fusesource.fabric.internal.ProfileOverlayImpl;
+import org.fusesource.fabric.utils.Strings;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+@ThreadSafe
+@Component(name = "org.fusesource.fabric.placholder.resolver.versionprop", description = "Fabric Version Property Placeholder Resolver", immediate = true)
+@Service(PlaceholderResolver.class)
+public final class VersionPropertyPointerResolver extends AbstractComponent implements PlaceholderResolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VersionPropertyPointerResolver.class);
+    private static final String SCHEME = "version";
+
+    private static final String EMPTY = "";
+
+    @Reference(referenceInterface = FabricService.class)
+    private final ValidatingReference<FabricService> fabricService = new ValidatingReference<FabricService>();
+    @Reference(referenceInterface = DataStore.class)
+    private final ValidatingReference<DataStore> dataStore = new ValidatingReference<DataStore>();
+    private String targetPid = "org.fusesource.fabric.version";
+
+    @Activate
+    void activate(ComponentContext context) {
+        activateComponent();
+    }
+
+    @Deactivate
+    void deactivate() {
+        deactivateComponent();
+    }
+
+    @Override
+    public String getScheme() {
+        return SCHEME;
+    }
+
+    /**
+     * Resolves the placeholder found inside the value, for the specific key of the pid.
+     */
+    @Override
+    public String resolve(String pid, String key, String value) {
+        assertValid();
+        try {
+            if (Strings.isNotBlank(value)) {
+                Profile profile = fabricService.get().getCurrentContainer().getOverlayProfile();
+                String pidPey = value.substring(SCHEME.length() + 1);
+                ProfileOverlayImpl profileOverlay = new ProfileOverlayImpl(profile, false, dataStore.get());
+                String answer = substituteFromProfile(profileOverlay, targetPid, pidPey);
+                if (answer != null) {
+                    answer = replaceVersions(answer, profileOverlay);
+                }
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("Replaced value " + value + " with answer: " + answer);
+                }
+                return answer;
+            }
+        } catch (Exception e) {
+            LOGGER.debug("Could not load property value: {} in version pid. Returning empty String.", value, e);
+        }
+        return EMPTY;
+    }
+
+    /**
+     * Lets replace any other instances of ${version:key} with the value
+     */
+    protected String replaceVersions(String value, ProfileOverlayImpl profileOverlay) {
+        // TODO we should really support other completions here too other than ${version:
+        String prefix = "${version:";
+        String postfix = "}";
+        boolean replaced;
+        do {
+            replaced = false;
+            int startIdx = value.indexOf(prefix);
+            if (startIdx >= 0) {
+                int keyIdx = startIdx + prefix.length();
+                int endIdx = value.indexOf(postfix, keyIdx);
+                if (endIdx > 0) {
+                    String newKey = value.substring(keyIdx, endIdx);
+                    String newValue = substituteFromProfile(profileOverlay, targetPid, newKey);
+                    if (newValue != null) {
+                        value = value.substring(0, startIdx) + newValue + value.substring(endIdx + 1);
+                    }
+                    replaced = true;
+                }
+            }
+        } while (replaced);
+        return value;
+    }
+
+    private String substituteFromProfile(Profile profile, String pid, String key) {
+        Map<String, String> configuration = profile.getConfiguration(pid);
+        if (configuration.containsKey(key)) {
+            return configuration.get(key);
+        } else return EMPTY;
+    }
+
+    void bindFabricService(FabricService fabricService) {
+        this.fabricService.bind(fabricService);
+    }
+
+    void unbindFabricService(FabricService fabricService) {
+        this.fabricService.unbind(fabricService);
+    }
+
+    void bindDataStore(DataStore dataStore) {
+        this.dataStore.bind(dataStore);
+    }
+
+    void unbindDataStore(DataStore dataStore) {
+        this.dataStore.unbind(dataStore);
+    }
+}
