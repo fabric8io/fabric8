f7bd5dbf7fd6237e762fbde4acd6639f507f2d29
Thomas Diesler <thomas.diesler@jboss.com>
2013-10-22 20:18:11 +0200

    Revert "FABRIC-590 - fix MQProfileTest"
    This reverts commit 2ae3af72b77d606f1148cc9fdca0a2a9a7e510b4.

--- fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/mq/MQProfileTest.java
+++ fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/mq/MQProfileTest.java
@@ -25,6 +25,7 @@ import static org.junit.Assert.assertFalse;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+@Ignore("[FABRIC-590] Fix fabric/fabric-itests/fabric-itests-basic")
 public class MQProfileTest extends FabricTestSupport {
 
     ArrayList<Container> containers = new ArrayList<Container>();
@@ -61,7 +62,7 @@ public class MQProfileTest extends FabricTestSupport {
 
         containers.addAll(ContainerBuilder.create().withName("example").withProfiles("example-mq").assertProvisioningResult().build());
         // give it a bit time
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         assertEquals("Producer not present", 1, bean.getTotalProducerCount());
         assertEquals("Consumer not present", 1, bean.getTotalConsumerCount());
     }
@@ -90,7 +91,7 @@ public class MQProfileTest extends FabricTestSupport {
 
         containers.addAll(ContainerBuilder.create().withName("example").withProfiles("example-mq").assertProvisioningResult().build());
         // give it a bit time
-        Thread.sleep(10000);
+        Thread.sleep(5000);
 
         installAndCheckFeature("activemq");
 
@@ -115,7 +116,6 @@ public class MQProfileTest extends FabricTestSupport {
         waitForProvisionSuccess(container2, PROVISION_TIMEOUT, TimeUnit.MILLISECONDS);
 
         installAndCheckFeature("activemq");
-        Thread.sleep(10000);
 
         BrokerViewMBean broker1 = (BrokerViewMBean)getMBean(container1, new ObjectName("org.apache.activemq:type=Broker,brokerName=ms-broker"), BrokerViewMBean.class);
 
@@ -149,8 +149,7 @@ public class MQProfileTest extends FabricTestSupport {
         containers.add(container2);
         waitForProvisionSuccess(container2, PROVISION_TIMEOUT, TimeUnit.MILLISECONDS);
 
-        containers.addAll(ContainerBuilder.create().withName("example-producer").withProfiles("example-mq-producer").withProfiles("mq-client-us-east").assertProvisioningResult().build());
-        containers.addAll(ContainerBuilder.create().withName("example-consumer").withProfiles("example-mq-consumer").withProfiles("mq-client-us-west").assertProvisioningResult().build());
+        containers.addAll(ContainerBuilder.create().withName("example").withProfiles("example-mq-cluster").assertProvisioningResult().build());
         // give it a bit time
         Thread.sleep(10000);
 
