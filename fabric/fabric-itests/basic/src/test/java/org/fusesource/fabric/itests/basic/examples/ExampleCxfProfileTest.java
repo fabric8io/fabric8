95d96d41da1b78a69895cdad020c565a4d038d1e
Willem Jiang <willem.jiang@gmail.com>
2013-11-28 20:45:50 +0800

    FABRIC-672 Fixed fabric basic ExampleCxfProfileTest

--- fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/examples/ExampleCxfProfileTest.java
+++ fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/examples/ExampleCxfProfileTest.java
@@ -16,56 +16,73 @@
  */
 package org.fusesource.fabric.itests.basic.examples;
 
-import org.fusesource.fabric.demo.cxf.client.Client;
+
+import java.util.Set;
+import org.fusesource.fabric.api.Container;
+import org.fusesource.fabric.demo.cxf.Hello;
+import org.fusesource.fabric.itests.paxexam.support.ContainerBuilder;
 import org.fusesource.fabric.itests.paxexam.support.FabricTestSupport;
+import static org.fusesource.tooling.testing.pax.exam.karaf.ServiceLocator.getOsgiService;
 import org.junit.After;
-import org.junit.Ignore;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
-
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-
 import org.ops4j.pax.exam.Option;
-
 import static org.ops4j.pax.exam.OptionUtils.combine;
-
+import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.junit.ProbeBuilder;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.osgi.framework.Constants;
+
 
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-@Ignore("[FABRIC-672] Fix fabric basic ExampleCxfProfileTest")
+// This test need to take sometime to download the cxf bundle
 public class ExampleCxfProfileTest extends FabricTestSupport {
     @After
     public void tearDown() throws InterruptedException {
-        destroyChildContainer("cxf-server");
+        ContainerBuilder.destroy();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.err.println(executeCommand("fabric:create -n"));
+        System.err.println(executeCommand("features:install fabric-cxf", 600000, false));
+        String projectVersion = System.getProperty("fabricitest.version");
+        // install bundle of CXF demo client
+        System.err.println(executeCommand("osgi:install -s mvn:org.fusesource.examples/fabric-cxf-demo-client/" + projectVersion));
+        System.err.println(executeCommand("osgi:list"));
+        System.err.println(executeCommand("packages:imports 141"));
+
     }
 
     @Test
     public void testExample() throws Exception {
-        // make sure the cxf related bundle is installed
-        System.err.println(executeCommand("features:install cxf"));
-        Thread.sleep(10000);
-        System.err.println(executeCommand("features:install fabric-cxf"));
-        Thread.sleep(5000);
-        System.err.println(executeCommand("fabric:create -n"));
-        createAndAssertChildContainer("cxf-server", "root", "example-cxf");
+
+
+        System.err.println("creating the cxf-server container.");
+        Set<Container> containers = ContainerBuilder.create().withName("child").withProfiles("example-cxf").assertProvisioningResult().build();
+        assertTrue("We should create the cxf-server container.", containers.size() ==1);
+        System.err.println("created the cxf-server container.");
+        // install bundle of CXF
+        Thread.sleep(2000);
         System.err.println(executeCommand("fabric:cluster-list"));
         // install bundle of CXF
-        Thread.sleep(5000);
-        String projectVersion = System.getProperty("fabricitest.version");
-        // install bundle of CXF demo client
-        System.err.println(executeCommand("osgi:install -s mvn:org.fusesource.examples/fabric-cxf-demo-client/" + projectVersion));
+        Thread.sleep(2000);
         // calling the client here
-        Client client = new Client();
-        String result1 = client.getProxy().sayHello();
-        String result2 = client.getProxy().sayHello();
+        Hello proxy = getOsgiService(Hello.class);
+        assertNotNull(proxy);
+        String result1 = proxy.sayHello();
+        String result2 = proxy.sayHello();
         assertNotSame("We should get the two different result", result1, result2);
     }
 
@@ -73,10 +90,16 @@ public class ExampleCxfProfileTest extends FabricTestSupport {
     public Option[] config() {
         return combine(
                 fabricDistributionConfiguration(),
+                mavenBundle("org.fusesource.examples", "fabric-cxf-demo-common"),
                 // Passing the system property to the test container
                 systemProperty("fabricitest.version").value(System.getProperty("fabricitest.version"))
         );
+    }
 
-
+    @ProbeBuilder
+    public TestProbeBuilder probeConfiguration(TestProbeBuilder probe) {
+        probe.setHeader(Constants.DYNAMICIMPORT_PACKAGE, "*,org.apache.felix.service.*;status=provisional");
+        probe.setHeader(Constants.IMPORT_PACKAGE, "org.fusesource.fabric.demo.cxf,org.fusesource.tooling.testing.pax.exam.karaf,*");
+        return probe;
     }
 }
