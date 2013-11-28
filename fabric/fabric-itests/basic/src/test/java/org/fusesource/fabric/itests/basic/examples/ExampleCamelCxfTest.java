651bce7a2e311d90cc3dca6075d9b4876365ce7c
Ioannis Canellos <iocanel@gmail.com>
2013-11-28 18:06:45 +0200

    Add utility method that waits for a condition to be meet. Use that method to ensure that ExampleCamelCxfTest that it waits for the routes to start.

--- fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/examples/ExampleCamelCxfTest.java
+++ fabric/fabric-itests/basic/src/test/java/org/fusesource/fabric/itests/basic/examples/ExampleCamelCxfTest.java
@@ -19,6 +19,7 @@ package org.fusesource.fabric.itests.basic.examples;
 import org.fusesource.fabric.api.Container;
 import org.fusesource.fabric.itests.paxexam.support.ContainerBuilder;
 import org.fusesource.fabric.itests.paxexam.support.FabricTestSupport;
+import org.fusesource.fabric.itests.paxexam.support.Provision;
 import org.fusesource.jansi.AnsiString;
 import org.junit.After;
 import org.junit.Assert;
@@ -35,6 +36,7 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 
 @RunWith(JUnit4TestRunner.class)
@@ -52,11 +54,17 @@ public class ExampleCamelCxfTest extends FabricTestSupport {
         Set<Container> containers = ContainerBuilder.create().withName("child").withProfiles("example-camel-cxf").assertProvisioningResult().build();
 		System.err.println(executeCommand("fabric:container-list"));
 
-        for (Container container : containers) {
+        for (final Container container : containers) {
+            Provision.waitForCondition(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    String response = new AnsiString(executeCommand("fabric:container-connect -u admin -p admin "+container.getId()+" camel:route-list | grep fabric-camel-cxf")).getPlain().toString();
+                    return response.contains("fabric-camel-cxf");
+                }
+            }, 10000L);
+
             System.err.println(executeCommand("fabric:container-connect -u admin -p admin "+container.getId()+" osgi:list"));
             System.err.println(executeCommand("fabric:container-connect -u admin -p admin "+container.getId()+" camel:route-list"));
-            String response = new AnsiString(executeCommand("fabric:container-connect -u admin -p admin "+container.getId()+" camel:route-list | grep fabric-camel-cxf")).getPlain().toString();
-            Assert.assertTrue(response.contains("fabric-camel-cxf"));
         }
     }
 
