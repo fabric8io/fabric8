3ef206f4ad0005cfcd6a3237ec9e604ad109ff39
Stan Lewis <slewis@fusesource.com>
2014-01-16 13:58:13 -0500

    Remove @Ignore and links to jira from tests, these are development-only

--- fabric/fabric-client/src/test/java/io/fabric8/jolokia/facade/FabricMBeanFacadeTest.java
+++ fabric/fabric-client/src/test/java/io/fabric8/jolokia/facade/FabricMBeanFacadeTest.java
@@ -26,8 +26,18 @@ import java.util.Collection;
 /**
  * Author: lhein
  */
-@Ignore("[FABRIC-791] Fix jolokia FabricMBeanFacadeTest")
 public class FabricMBeanFacadeTest {
+    /*
+        These tests are only meant to be run by a developer against an existing fabric
+        as part of developing the fabric-client module.  To run the tests, start up a
+        fabric8 distro, create a fabric and build fabric-client via:
+
+        mvn clean install -DhasFabric=true
+
+        You can pass -Dfabric.user and -Dfabric.password if a different username/password
+        was used to create the fabric.
+    */
+
     String userName = System.getProperty("fabric.user", "admin");
     String password = System.getProperty("fabric.password", "admin");
 
