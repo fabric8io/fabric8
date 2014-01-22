4fa2022891dd8c862e232836084bffab3c8f25f4
Ioannis Canellos <iocanel@gmail.com>
2014-01-22 21:39:28 +0200

    [FABRIC-667] Un-ignore CamelProfileTest.

--- fabric/fabric-itests/basic/src/test/java/io/fabric8/itests/basic/camel/CamelProfileTest.java
+++ fabric/fabric-itests/basic/src/test/java/io/fabric8/itests/basic/camel/CamelProfileTest.java
@@ -19,7 +19,6 @@ import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-@Ignore("[FABRIC-667] Fix fabric basic CamelProfileTest")
 public class CamelProfileTest extends FabricFeaturesTest {
 
     @After
@@ -32,7 +31,7 @@ public class CamelProfileTest extends FabricFeaturesTest {
         System.err.println(executeCommand("fabric:create -n"));
         Set<Container> containers = ContainerBuilder.create().withName("feature-camel").withProfiles("feature-camel").assertProvisioningResult().build();
         assertProvisionedFeature(containers, "camel-http", "feature-camel", "camel-http");
-        //assertProvisionedFeature(containers, "camel-jetty", "feature-camel", "camel-jetty");
+        assertProvisionedFeature(containers, "camel-jetty", "feature-camel", "camel-jetty");
         assertProvisionedFeature(containers, "camel-jms", "feature-camel", "camel-jms");
         assertProvisionedFeature(containers, "camel-ftp", "feature-camel", "camel-ftp");
         assertProvisionedFeature(containers, "camel-quartz", "feature-camel", "camel-quartz");
