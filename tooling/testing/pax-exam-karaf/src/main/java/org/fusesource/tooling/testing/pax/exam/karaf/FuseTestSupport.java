0c78a8a32b2c21042f8b1c656a96d502459cbe9c
Thomas Diesler <thomas.diesler@jboss.com>
2014-01-29 11:47:00 +0100

    [FABRIC-814] PaxExam may run test before support bundles are installed

--- tooling/testing/pax-exam-karaf/src/main/java/org/fusesource/tooling/testing/pax/exam/karaf/FuseTestSupport.java
+++ tooling/testing/pax-exam-karaf/src/main/java/org/fusesource/tooling/testing/pax/exam/karaf/FuseTestSupport.java
@@ -17,9 +17,21 @@
 
 package org.fusesource.tooling.testing.pax.exam.karaf;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
 import org.apache.felix.service.command.CommandProcessor;
 import org.apache.felix.service.command.CommandSession;
 import org.apache.karaf.features.FeaturesService;
+import org.apache.karaf.tooling.exam.options.KarafDistributionOption;
 import org.junit.Assert;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
@@ -29,20 +41,6 @@ import org.ops4j.pax.exam.options.MavenArtifactProvisionOption;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
-
-import javax.inject.Inject;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.fusesource.tooling.testing.pax.exam.karaf.ServiceLocator.getOsgiService;
-import static org.apache.karaf.tooling.exam.options.KarafDistributionOption.editConfigurationFilePut;
 
 public class FuseTestSupport {
 
@@ -57,13 +55,12 @@ public class FuseTestSupport {
     @Inject
     protected BundleContext bundleContext;
 
-      protected Bundle installBundle(String groupId, String artifactId) throws Exception {
+    public Bundle installBundle(String groupId, String artifactId) throws Exception {
         MavenArtifactProvisionOption mvnUrl = mavenBundle(groupId, artifactId);
         return bundleContext.installBundle(mvnUrl.getURL());
     }
 
-
-    protected Bundle getInstalledBundle(String symbolicName) {
+    public Bundle getInstalledBundle(String symbolicName) {
         for (Bundle b : bundleContext.getBundles()) {
             if (b.getSymbolicName().equals(symbolicName)) {
                 return b;
@@ -75,16 +72,13 @@ public class FuseTestSupport {
         throw new RuntimeException("Bundle " + symbolicName + " does not exist");
     }
 
-
     /**
      * Make available system properties that are configured for the test, to the test container.
      * <p>Note:</p> If not obvious the container runs in in forked mode and thus system properties passed
      * form command line or surefire plugin are not available to the container without an approach like this.
-     * @param propertyName
-     * @return
      */
     public static Option copySystemProperty(String propertyName) {
-        return editConfigurationFilePut("etc/system.properties", propertyName, System.getProperty(propertyName) != null ? System.getProperty(propertyName) : "");
+        return KarafDistributionOption.editConfigurationFilePut("etc/system.properties", propertyName, System.getProperty(propertyName) != null ? System.getProperty(propertyName) : "");
     }
 
     /**
@@ -96,7 +90,7 @@ public class FuseTestSupport {
      * @param artifactId the artifactId of the maven bundle
      * @return the provisioning option for the given bundle
      */
-    protected static MavenArtifactProvisionOption mavenBundle(String groupId, String artifactId) {
+    public static MavenArtifactProvisionOption mavenBundle(String groupId, String artifactId) {
         return CoreOptions.mavenBundle(groupId, artifactId).versionAsInProject();
     }
 
@@ -110,7 +104,7 @@ public class FuseTestSupport {
      * @param version    the version of the maven bundle
      * @return the provisioning option for the given bundle
      */
-    protected static MavenArtifactProvisionOption mavenBundle(String groupId, String artifactId, String version) {
+    public MavenArtifactProvisionOption mavenBundle(String groupId, String artifactId, String version) {
         return CoreOptions.mavenBundle(groupId, artifactId).version(version);
     }
 
@@ -120,8 +114,8 @@ public class FuseTestSupport {
      * @param command
      * @return
      */
-    protected String executeCommand(final String command) {
-       return executeCommands(COMMAND_TIMEOUT, false, command);
+    public String executeCommand(final String command) {
+        return executeCommands(COMMAND_TIMEOUT, false, command);
     }
 
     /**
@@ -131,7 +125,7 @@ public class FuseTestSupport {
      * @param commands
      * @return
      */
-    protected String executeCommands(final String... commands) {
+    public String executeCommands(final String... commands) {
         return executeCommands(COMMAND_TIMEOUT, false, commands);
     }
 
@@ -144,56 +138,55 @@ public class FuseTestSupport {
 
      * @return
      */
-    protected String executeCommand(final String command, final long timeout, final boolean silent) {
+    public String executeCommand(final String command, final long timeout, final boolean silent) {
         return executeCommands(timeout, silent, command);
     }
 
-     /**
-     * Executes a shell command and returns output as a String.
-     * Commands have a default timeout of 10 seconds.
-     * @param timeout The amount of time in millis to wait for the command to execute.
-     * @param silent  Specifies if the command should be displayed in the screen.
-      * @param commands The command to execute.
-     * @return
-     */
-    protected String executeCommands(final long timeout, final boolean silent, final String... commands) {
+    /**
+    * Executes a shell command and returns output as a String.
+    * Commands have a default timeout of 10 seconds.
+    * @param timeout The amount of time in millis to wait for the command to execute.
+    * @param silent  Specifies if the command should be displayed in the screen.
+     * @param commands The command to execute.
+    * @return
+    */
+    public String executeCommands(final long timeout, final boolean silent, final String... commands) {
         String response = null;
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         final PrintStream printStream = new PrintStream(byteArrayOutputStream);
-        final CommandProcessor commandProcessor = getOsgiService(CommandProcessor.class);
+        final CommandProcessor commandProcessor = ServiceLocator.getOsgiService(CommandProcessor.class);
         final CommandSession commandSession = commandProcessor.createSession(System.in, printStream, printStream);
         commandSession.put("APPLICATION", System.getProperty("karaf.name", "root"));
         commandSession.put("USER", "karaf");
-        FutureTask<String> commandFuture = new FutureTask<String>(
-                new Callable<String>() {
-                    public String call() throws Exception {
-                        for (String command : commands) {
-                            boolean keepRunning = true;
+        FutureTask<String> commandFuture = new FutureTask<String>(new Callable<String>() {
+            public String call() throws Exception {
+                for (String command : commands) {
+                    boolean keepRunning = true;
 
-                            if (!silent) {
-                                System.out.println(command);
-                                System.out.flush();
-                            }
-
-                            while (!Thread.currentThread().isInterrupted() && keepRunning) {
-                                try {
-                                    commandSession.execute(command);
-                                    keepRunning = false;
-                                } catch (Exception e) {
-                                    if (retryException(e)) {
-                                        keepRunning = true;
-                                        sleep(1000);
-                                    } else {
-                                        throw new CommandExecutionException(e);
-                                    }
-                                }
-                            }
-                        }
-                        printStream.flush();
-                        return byteArrayOutputStream.toString();
+                    if (!silent) {
+                        System.out.println(command);
+                        System.out.flush();
                     }
 
-                });
+                    while (!Thread.currentThread().isInterrupted() && keepRunning) {
+                        try {
+                            commandSession.execute(command);
+                            keepRunning = false;
+                        } catch (Exception e) {
+                            if (retryException(e)) {
+                                keepRunning = true;
+                                sleep(1000);
+                            } else {
+                                throw new CommandExecutionException(e);
+                            }
+                        }
+                    }
+                }
+                printStream.flush();
+                return byteArrayOutputStream.toString();
+            }
+
+        });
 
         try {
             executor.submit(commandFuture);
@@ -206,7 +199,6 @@ public class FuseTestSupport {
 
         return response;
     }
-
 
     private static boolean retryException(Exception e) {
         //The gogo runtime package is not exported, so we are just checking against the class name.
@@ -229,7 +221,7 @@ public class FuseTestSupport {
      */
     public void installAndCheckFeature(String feature) throws Exception {
         System.err.println(executeCommand("features:install " + feature));
-        FeaturesService featuresService = getOsgiService(FeaturesService.class);
+        FeaturesService featuresService = ServiceLocator.getOsgiService(FeaturesService.class);
         System.err.println(executeCommand("osgi:list -t 0"));
         Assert.assertTrue("Expected " + feature + " feature to be installed.", featuresService.isInstalled(featuresService.getFeature(feature)));
     }
@@ -242,11 +234,10 @@ public class FuseTestSupport {
      */
     public void unInstallAndCheckFeature(String feature) throws Exception {
         System.err.println(executeCommand("features:uninstall " + feature));
-        FeaturesService featuresService = getOsgiService(FeaturesService.class);
+        FeaturesService featuresService = ServiceLocator.getOsgiService(FeaturesService.class);
         System.err.println(executeCommand("osgi:list -t 0"));
         Assert.assertFalse("Expected " + feature + " feature to be installed.", featuresService.isInstalled(featuresService.getFeature(feature)));
     }
-
 
     /**
      * This is used to customize the Probe that will contain the test.
