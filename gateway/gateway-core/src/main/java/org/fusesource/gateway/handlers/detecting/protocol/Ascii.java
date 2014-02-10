e14b1ded19c737d235be69880aca06cdbdc11776
Hiram Chirino <hiram@hiramchirino.com>
2014-02-10 09:21:40 -0500

    Adding the start of a protocol detecting gateway.

---
+++ gateway/gateway-core/src/main/java/org/fusesource/gateway/handlers/detecting/protocol/Ascii.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fusesource.gateway.handlers.detecting.protocol;
+
+import org.vertx.java.core.buffer.Buffer;
+
+/**
+ * Holds a string and it's ascii encoded value as a buffer.
+ */
+public class Ascii {
+
+    private final Buffer buffer;
+    private final String string;
+    private int hashCode;
+
+    public Ascii(Buffer buffer) {
+        this.buffer = buffer;
+        this.string = decode(buffer);
+    }
+
+    public Ascii(String string) {
+        this.string = string;
+        this.buffer = encode(string);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // Overrides
+    ///////////////////////////////////////////////////////////////////
+
+    public Buffer toBuffer() {
+        return buffer;
+    }
+
+    public String toString() {
+        return string;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+
+        Class clazz = obj.getClass();
+        if( clazz==Ascii.class ) {
+            return obj.toString().equals(toString());
+        }
+        if( clazz==String.class ) {
+            return obj.equals(toString());
+        }
+        if( clazz==Buffer.class ) {
+            return obj.equals(toBuffer());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (hashCode == 0) {
+            hashCode = toString().hashCode();
+        }
+        return hashCode;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // Statics
+    ///////////////////////////////////////////////////////////////////
+
+    public static Ascii ascii(String value) {
+        if (value == null) {
+            return null;
+        }
+        return new Ascii(value);
+    }
+
+    public static Ascii ascii(Buffer buffer) {
+        if (buffer == null) {
+            return null;
+        }
+        return new Ascii(buffer);
+    }
+
+    static public Buffer encode(String value) {
+        int size = value.length();
+        Buffer rc = new Buffer(size);
+        for (int i = 0; i < size; i++) {
+            rc.appendByte((byte) (value.charAt(i) & 0xFF));
+        }
+        return rc;
+    }
+
+    static public String decode(Buffer value) {
+        int size = value.length();
+        char rc[] = new char[size];
+        for (int i = 0; i < size; i++) {
+            rc[i] = (char) (value.getByte(i) & 0xFF);
+        }
+        return new String(rc);
+    }
+
+
+}
---
+++ gateway/gateway-core/src/test/java/org/fusesource/gateway/handlers/detecting/DetectingGatewayTest.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fusesource.gateway.handlers.detecting;
+
+
+import org.apache.activemq.apollo.broker.Broker;
+import org.apache.activemq.apollo.dto.AcceptingConnectorDTO;
+import org.apache.activemq.apollo.dto.BrokerDTO;
+import org.apache.activemq.apollo.dto.VirtualHostDTO;
+import org.apache.activemq.apollo.util.ServiceControl;
+import org.fusesource.gateway.ServiceDTO;
+import org.fusesource.gateway.ServiceDetails;
+import org.fusesource.gateway.ServiceMap;
+import org.fusesource.gateway.handlers.detecting.protocol.stomp.StompProtocol;
+import org.fusesource.gateway.loadbalancer.LoadBalancer;
+import org.fusesource.gateway.loadbalancer.LoadBalancers;
+import org.fusesource.stomp.client.BlockingConnection;
+import org.fusesource.stomp.client.Stomp;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ */
+public class DetectingGatewayTest {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(DetectingGatewayTest.class);
+    ServiceMap serviceMap = new ServiceMap();
+
+    // Setup Vertx
+    protected Vertx vertx;
+    @Before
+    public void startVertx() {
+        if( vertx == null ) {
+            vertx = VertxFactory.newVertx();
+        }
+    }
+    @After
+    public void stopVertx(){
+        if( vertx!=null ) {
+            vertx.stop();
+            vertx = null;
+        }
+    }
+
+    // Setup some brokers.
+    final protected ArrayList<Broker> brokers = new ArrayList<Broker>();
+
+    @Before
+    public void startBrokers() {
+        for(int i=0; i < 2; i++) {
+
+            // create a broker..
+            String name = "broker" + i;
+            Broker broker = createBroker(name);
+            ServiceControl.start(broker);
+            brokers.add(broker);
+
+            // Add a service map entry for the broker.
+            ServiceDTO details = new ServiceDTO();
+            details.setId(name);
+            details.setVersion("1.0");
+            details.setContainer("testing");
+            details.setBundleName("none");
+            details.setBundleVersion("1.0");
+            List<String> services = Arrays.asList("stomp://localhost:" + portOfBroker(i));
+            details.setServices(services);
+            serviceMap.serviceUpdated(name, details);
+
+            println(String.format("Broker %s is exposing: %s", name, services));
+        }
+    }
+
+    @After
+    public void stopBrokers() {
+        for (Broker broker : brokers) {
+            ServiceControl.stop(broker);
+        }
+        brokers.clear();
+    }
+
+    int portOfBroker(int broker) {
+        return ((InetSocketAddress)brokers.get(broker).get_socket_address()).getPort();
+    }
+
+    public Broker createBroker(String hostname) {
+        Broker broker = new Broker();
+        BrokerDTO config = broker.config();
+
+        // Configure the virtual host..
+        VirtualHostDTO virtualHost = new VirtualHostDTO();
+        virtualHost.id = hostname;
+        virtualHost.host_names.add(hostname);
+        config.virtual_hosts.add(virtualHost);
+
+        // Configure the connectors
+        AcceptingConnectorDTO connector = new AcceptingConnectorDTO();
+        connector.connection_limit = 100;
+        connector.bind = "tcp://0.0.0.0:0";
+        config.connectors.clear();
+        config.connectors.add(connector);
+
+        return broker;
+    }
+
+    protected void println(Object msg) {
+        LOG.info(msg.toString());
+    }
+
+    @Test
+    public void canDetectTheStompProtocol() throws Exception {
+        DetectingGateway gateway = createGateway();
+        gateway.init();
+
+        // Lets establish a connection....
+        Stomp stomp = new Stomp("localhost", gateway.getBoundPort());
+        stomp.setHost("broker0"); // lets connect to the broker0 virtual host..
+        BlockingConnection connection = stomp.connectBlocking();
+
+        assertEquals(1, getConnectionsOnBroker(0));
+        for( int i = 1; i < brokers.size(); i++) {
+            assertEquals(0, getConnectionsOnBroker(i));
+        }
+
+        connection.close();
+    }
+
+    private int getConnectionsOnBroker(int brokerIdx) {
+        return brokers.get(brokerIdx).connections().size();
+    }
+
+    public DetectingGateway createGateway() {
+        String loadBalancerType = LoadBalancers.STICKY_LOAD_BALANCER;
+        int stickyLoadBalancerCacheSize = LoadBalancers.STICKY_LOAD_BALANCER_DEFAULT_CACHE_SIZE;
+
+        LoadBalancer<ServiceDetails> serviceLoadBalancer = LoadBalancers.createLoadBalancer(loadBalancerType, stickyLoadBalancerCacheSize);
+
+        ArrayList<Protocol> protocols = new ArrayList<Protocol>();
+        protocols.add(new StompProtocol());
+        DetectingGatewayProtocolHandler handler = new DetectingGatewayProtocolHandler(vertx, serviceMap, protocols, serviceLoadBalancer);
+        return new DetectingGateway(vertx, 0, handler);
+    }
+}
