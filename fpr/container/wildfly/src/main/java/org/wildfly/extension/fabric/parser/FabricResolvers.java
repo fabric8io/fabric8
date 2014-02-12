5f7d563c26348796726e5650f2d2fcdfe3a5ff0a
Thomas Diesler <thomas.diesler@jboss.com>
2014-02-12 14:27:00 +0100

    [FABRIC-630] Initial Fabric bootstrap on WildFly

---
+++ fpr/container/wildfly/src/main/java/org/wildfly/extension/fabric/parser/FabricResolvers.java
@@ -0,0 +1,40 @@
+/*
+ * #%L
+ * Wildfly Gravia Subsystem
+ * %%
+ * Copyright (C) 2010 - 2013 JBoss by Red Hat
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+package org.wildfly.extension.fabric.parser;
+
+import org.jboss.as.controller.descriptions.ResourceDescriptionResolver;
+import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
+
+/**
+ * @author Thomas.Diesler@jboss.com
+ * @since 13-Nov-2013
+ */
+final class FabricResolvers {
+
+    static final String RESOURCE_NAME = FabricResolvers.class.getPackage().getName() + ".LocalDescriptions";
+
+    static ResourceDescriptionResolver getResolver(String keyPrefix) {
+        return new StandardResourceDescriptionResolver(keyPrefix, RESOURCE_NAME, SecurityActions.getClassLoader(FabricResolvers.class), true, true);
+    }
+
+}
---
+++ fpr/container/wildfly/src/main/java/org/wildfly/extension/fabric/parser/Namespace10.java
@@ -0,0 +1,115 @@
+/*
+ * #%L
+ * Wildfly Gravia Subsystem
+ * %%
+ * Copyright (C) 2010 - 2013 JBoss by Red Hat
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+
+package org.wildfly.extension.fabric.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Constants related to namespace {@link Namespace#VERSION_1_0}.
+ *
+ * @author Thomas.Diesler@jboss.com
+ * @since 13-Nov-2013
+ */
+interface Namespace10 {
+
+    enum Attribute {
+        UNKNOWN(null),
+        ID("id"),
+        ;
+        private final String name;
+
+        Attribute(final String name) {
+            this.name = name;
+        }
+
+        /**
+         * Get the local name of this attribute.
+         *
+         * @return the local name
+         */
+        public String getLocalName() {
+            return name;
+        }
+
+        private static final Map<String, Attribute> MAP;
+
+        static {
+            final Map<String, Attribute> map = new HashMap<String, Attribute>();
+            for (Attribute element : values()) {
+                final String name = element.getLocalName();
+                if (name != null) map.put(name, element);
+            }
+            MAP = map;
+        }
+
+        public static Attribute forName(String localName) {
+            final Attribute element = MAP.get(localName);
+            return element == null ? UNKNOWN : element;
+        }
+
+        @Override
+        public String toString() {
+            return getLocalName();
+        }
+    }
+
+    enum Element {
+        // must be first
+        UNKNOWN(null),
+        ;
+
+        private final String name;
+
+        Element(final String name) {
+            this.name = name;
+        }
+
+        /**
+         * Get the local name of this element.
+         *
+         * @return the local name
+         */
+        public String getLocalName() {
+            return name;
+        }
+
+        private static final Map<String, Element> MAP;
+
+        static {
+            final Map<String, Element> map = new HashMap<String, Element>();
+            for (Element element : values()) {
+                final String name = element.getLocalName();
+                if (name != null) map.put(name, element);
+            }
+            MAP = map;
+        }
+
+        public static Element forName(String localName) {
+            final Element element = MAP.get(localName);
+            return element == null ? UNKNOWN : element;
+        }
+    }
+}
