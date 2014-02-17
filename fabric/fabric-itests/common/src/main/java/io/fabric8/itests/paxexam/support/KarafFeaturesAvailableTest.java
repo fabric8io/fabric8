5e3ccbb71e9eb77484d8a7d9ca30953301d71404
Grzegorz Grzybek <ggrzybek@redhat.com>
2014-02-17 14:44:48 +0100

    FABRIC-730 product integration tests for validating feature definitions
    The tests run the container and iterate through all available, not
    installed features. Each feature is being installed and all the problems
    are reported to STDERR.
    
    TODO: investigate the problem why consecutive uninstallation usually
    causes problems (both with next installations and with the PAX test
    system itself - "no such object in table" exceptions).

---
+++ fabric/fabric-itests/common/src/main/java/io/fabric8/itests/paxexam/support/KarafFeaturesAvailableTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.itests.paxexam.support;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.options.DefaultCompositeOption;
+import org.ops4j.pax.exam.options.extra.VMOption;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class KarafFeaturesAvailableTest extends FabricTestSupport {
+
+    @Test
+    public void testFeatures() throws Exception {
+        checkAllNotInstalledFeatures();
+    }
+
+    @Configuration
+    public Option[] config() {
+        return new Option[]{
+            new DefaultCompositeOption(fabricDistributionConfiguration()),
+            new VMOption("-XX:MaxPermSize=512M"),
+        };
+    }
+
+}
