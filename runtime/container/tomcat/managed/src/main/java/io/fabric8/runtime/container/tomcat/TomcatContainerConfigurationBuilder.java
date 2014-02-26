45e75ea9a6feee6c79d48444c232d08a39bcdee6
Thomas Diesler <thomas.diesler@jboss.com>
2014-02-26 17:01:49 +0100

    [FABRIC-925] Create disconnected root containers

---
+++ runtime/container/tomcat/managed/src/main/java/io/fabric8/runtime/container/tomcat/TomcatContainerConfigurationBuilder.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.runtime.container.tomcat;
+
+import io.fabric8.runtime.container.ContainerConfigurationBuilder;
+
+
+/**
+ * The managed container configuration builder
+ *
+ * @author thomas.diesler@jboss.com
+ * @since 26-Feb-2014
+ */
+public class TomcatContainerConfigurationBuilder extends ContainerConfigurationBuilder {
+
+    private final TomcatContainerConfiguration configuration = new TomcatContainerConfiguration();
+
+    @Override
+    protected TomcatContainerConfiguration internalConfiguration() {
+        return configuration;
+    }
+
+}
