75508dce4c67fada304789787a50f5fa429a8d8e
Thomas Diesler <thomas.diesler@jboss.com>
2014-02-26 17:01:49 +0100

    [FABRIC-925] Create disconnected root containers

---
+++ runtime/container/common/src/main/java/io/fabric8/runtime/container/LifecycleException.java
@@ -0,0 +1,23 @@
+package io.fabric8.runtime.container;
+
+/**
+ * LifecycleException
+ *
+ * @author thomas.diesler@jboss.com
+ * @since 26-Feb-2014
+ */
+public class LifecycleException extends Exception
+{
+   private static final long serialVersionUID = 1L;
+
+   public LifecycleException(String message)
+   {
+      super(message);
+   }
+
+   public LifecycleException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+}
---
+++ runtime/container/common/src/main/java/io/fabric8/runtime/container/ManagedContainer.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.runtime.container;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import org.jboss.gravia.runtime.RuntimeType;
+
+
+/**
+ * The managed root container
+ *
+ * @author thomas.diesler@jboss.com
+ * @since 26-Feb-2014
+ */
+public interface ManagedContainer<T extends ContainerConfiguration> {
+
+    enum State {
+        CREATED,
+        STARTED,
+        STOPPED,
+        DESTROYED
+    }
+
+    RuntimeType getRuntimeType();
+
+    File getContainerHome();
+
+    State getState();
+
+    void create(T configuration) throws LifecycleException;
+
+    void start() throws LifecycleException;
+
+    void stop() throws LifecycleException;
+
+    void destroy() throws LifecycleException;
+
+    public static class Factory<T extends ContainerConfiguration> {
+
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        public static <T extends ContainerConfiguration> ManagedContainer<T> create(T configuration) throws LifecycleException {
+            RuntimeType type = configuration.getRuntimeType();
+            ServiceLoader<ManagedContainer> loader = ServiceLoader.load(ManagedContainer.class);
+            Iterator<ManagedContainer> iterator = loader.iterator();
+            while(iterator.hasNext()) {
+                ManagedContainer<T> service = iterator.next();
+                if (service.getRuntimeType() == type) {
+                    service.create(configuration);
+                    return service;
+                }
+            }
+            throw new IllegalStateException("Cannot obtain managed container service for: " + type);
+        }
+    }
+}
