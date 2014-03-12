c239992d183a476d9f23b2d0b2273a0e4fea0c6f
Ioannis Canellos <iocanel@gmail.com>
2014-03-12 19:23:52 +0200

    #883: Make ProfileWatcher scr managed to prevent grace period issues.

---
+++ fabric/fabric-agent-commands/src/main/java/io/fabric8/agent/commands/support/Utils.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) FuseSource, Inc.
+ * http://fusesource.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.agent.commands.support;
+
+import io.fabric8.agent.mvn.Parser;
+import io.fabric8.api.Container;
+import io.fabric8.api.Profile;
+import io.fabric8.utils.ChecksumUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Utils.class);
+
+    public static boolean isSnapshot(Parser parser) {
+        String version = parser.getVersion();
+        return version != null && version.contains("SNAPSHOT");
+    }
+
+
+    public static Long getFileChecksum(File file) {
+        try {
+            return ChecksumUtils.checksum(new FileInputStream(file));
+        } catch (IOException e) {
+            LOG.warn("Failed to get checksum of file: " + file.getAbsolutePath() + ". " + e, e);
+            return null;
+        }
+    }
+
+    public static Properties findProfileChecksums(Profile profile) {
+        Properties checksums = null;
+        Container[] containers = profile.getAssociatedContainers();
+        if (containers != null) {
+            for (Container container : containers) {
+                checksums = container.getProvisionChecksums();
+                if (checksums != null) {
+                    break;
+                }
+            }
+        }
+        return checksums;
+    }
+}
