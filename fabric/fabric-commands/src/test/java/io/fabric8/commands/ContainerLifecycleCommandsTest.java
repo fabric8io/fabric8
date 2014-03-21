406101e52e5bb65ff3621080b19fdcf027f720ba
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-03-21 11:29:37 +0100

    [FABRIC-962] Reenabling the test after changing mock expectations

--- fabric/fabric-commands/src/test/java/io/fabric8/commands/ContainerLifecycleCommandsTest.java
+++ fabric/fabric-commands/src/test/java/io/fabric8/commands/ContainerLifecycleCommandsTest.java
@@ -25,7 +25,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import io.fabric8.api.Container;
-import io.fabric8.api.FabricException;
 import io.fabric8.api.FabricService;
 import io.fabric8.api.scr.support.ReflectionHelper;
 import io.fabric8.internal.ContainerImpl;
@@ -34,18 +33,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 
+import org.apache.curator.framework.CuratorFramework;
 import org.apache.felix.service.command.CommandSession;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore("[FABRIC-962] Fix ContainerLifecycleCommandsTest for SCR commands")
 public class ContainerLifecycleCommandsTest {
 
     private ContainerStopAction stop;
     private FabricService fabricService;
     private CommandSession commandSession;
     private ByteArrayOutputStream result;
+    private CuratorFramework curatorFramework;
 
     @Before
     public void init() throws Exception {
@@ -55,9 +54,7 @@ public class ContainerLifecycleCommandsTest {
         this.result = new ByteArrayOutputStream();
         expect(this.commandSession.getConsole()).andReturn(new PrintStream(result)).anyTimes();
 
-        // Attempt to avoid the call to curator
-        //Field forceField = ContainerLifecycleAction.class.getDeclaredField("force");
-        //ReflectionHelper.setField(forceField, stop, true);
+        this.curatorFramework = createMock(CuratorFramework.class);
     }
 
     @Test
@@ -65,7 +62,7 @@ public class ContainerLifecycleCommandsTest {
         containers("c1");
 
         ContainerImpl c1 = newContainer("c1");
-        expect(this.fabricService.getContainer("c1")).andReturn(c1);
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework).anyTimes();
         expect(this.fabricService.getContainers()).andReturn(new Container[] { c1 });
         this.fabricService.stopContainer(c1, false);
 
@@ -78,14 +75,14 @@ public class ContainerLifecycleCommandsTest {
     public void testStopNonExistingContainer() throws Exception {
         containers("cNon1");
 
-        expect(this.fabricService.getContainer("cNon1")).andThrow(
-            new FabricException("Container 'cNon1' does not exist"));
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework).anyTimes();
+        expect(this.fabricService.getContainers()).andReturn(new Container[0]);
 
         replay(this.fabricService, this.commandSession);
         try {
             this.stop.execute(this.commandSession);
             fail("Should throw FabricException");
-        } catch (FabricException e) {
+        } catch (IllegalArgumentException e) {
         }
         verify(this.fabricService);
     }
@@ -123,13 +120,13 @@ public class ContainerLifecycleCommandsTest {
         ContainerImpl c1 = newContainer("c1");
         ContainerImpl c2 = newContainer("c2");
         ContainerImpl c3 = newContainer("c3");
-        expect(this.fabricService.getContainer("c1")).andReturn(c1);
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework).anyTimes();
         expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2 });
         this.fabricService.stopContainer(c1, false);
-        expect(this.fabricService.getContainer("c3")).andReturn(c3);
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework).anyTimes();
         expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2 });
         this.fabricService.stopContainer(c3, false);
-        expect(this.fabricService.getContainer("c2")).andReturn(c2);
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework).anyTimes();
         expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2 });
         this.fabricService.stopContainer(c2, false);
 
@@ -148,15 +145,15 @@ public class ContainerLifecycleCommandsTest {
         ContainerImpl c2 = newContainer("c2");
         ContainerImpl c3 = newContainer("c3");
         ContainerImpl d1 = newContainer("d1");
-        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 });
-        expect(this.fabricService.getContainer("c1")).andReturn(c1);
-        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 });
+        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 }).once();
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
+        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 }).once();
         this.fabricService.stopContainer(c1, false);
-        expect(this.fabricService.getContainer("c3")).andReturn(c3);
-        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 });
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
+        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 }).once();
         this.fabricService.stopContainer(c3, false);
-        expect(this.fabricService.getContainer("c2")).andReturn(c2);
-        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 });
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
+        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1, c3, c2, d1 }).once();
         this.fabricService.stopContainer(c2, false);
 
         replay(this.fabricService, this.commandSession);
@@ -173,13 +170,14 @@ public class ContainerLifecycleCommandsTest {
     public void testStopNoMatchingContainers() throws Exception {
         containers("c1", "c2");
 
-        expect(this.fabricService.getContainer("c1")).andThrow(new FabricException("Container 'c1' does not exist"));
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
+        expect(this.fabricService.getContainers()).andReturn(new Container[0]).once();
 
         replay(this.fabricService, this.commandSession);
         try {
             this.stop.execute(this.commandSession);
             fail("Should throw FabricException");
-        } catch (FabricException e) {
+        } catch (IllegalArgumentException e) {
         }
         verify(this.fabricService);
     }
@@ -189,17 +187,18 @@ public class ContainerLifecycleCommandsTest {
         containers("c1", "c2");
 
         ContainerImpl c1 = newContainer("c1");
-        expect(this.fabricService.getContainer("c1")).andReturn(c1);
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
         expect(this.fabricService.getContainers()).andReturn(new Container[] { c1 }).once();
         this.fabricService.stopContainer(c1, false);
-        expect(this.fabricService.getContainer("c2")).andThrow(new FabricException("Container 'c2' does not exist"));
+        expect(this.fabricService.adapt(CuratorFramework.class)).andReturn(this.curatorFramework);
+        expect(this.fabricService.getContainers()).andReturn(new Container[] { c1 }).once();
 
         replay(this.fabricService, this.commandSession);
         try {
             this.stop.execute(this.commandSession);
             fail("Should throw FabricException");
-        } catch (FabricException e) {
-            assertThat(e.getMessage().contains("'c2'"), is(true));
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage().contains(" c2 "), is(true));
         }
         verify(this.fabricService);
     }
