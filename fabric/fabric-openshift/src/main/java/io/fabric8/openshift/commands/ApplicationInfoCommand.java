50aec273956e30b2a2a06e78d4a240ffc74be86e
Claus Ibsen <claus.ibsen@gmail.com>
2014-04-21 11:38:52 +0200

    #1165: Added openshift restart command and also show ssh url in app info

--- fabric/fabric-openshift/src/main/java/io/fabric8/openshift/commands/ApplicationInfoCommand.java
+++ fabric/fabric-openshift/src/main/java/io/fabric8/openshift/commands/ApplicationInfoCommand.java
@@ -1,14 +1,16 @@
 package io.fabric8.openshift.commands;
 
+import java.util.List;
+
 import com.openshift.client.IApplication;
 import com.openshift.client.IDomain;
+import com.openshift.client.IGear;
+import com.openshift.client.IGearGroup;
 import com.openshift.client.IOpenShiftConnection;
 import com.openshift.client.cartridge.IEmbeddedCartridge;
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.felix.gogo.commands.Option;
-
-import java.util.List;
 
 @Command(name = "application-list", scope = "openshift", description = "Lists available openshift application")
 public class ApplicationInfoCommand extends OpenshiftCommandSupport {
@@ -31,10 +33,22 @@ public class ApplicationInfoCommand extends OpenshiftCommandSupport {
                 System.out.println(String.format(FORMAT, "UUID:", application.getUUID()));
                 System.out.println(String.format(FORMAT, "Application URL:", application.getApplicationUrl()));
                 System.out.println(String.format(FORMAT, "Git URL:", application.getGitUrl()));
+                System.out.println(String.format(FORMAT, "SSH URL:", application.getSshUrl()));
                 System.out.println(String.format(FORMAT, "Cartridge:", application.getCartridge().getName()));
                 System.out.println(String.format(FORMAT, "Embedded Cartridges:", cartridgesToString(application.getEmbeddedCartridges())));
                 System.out.println(String.format(FORMAT, "Scale:", application.getApplicationScale().getValue()));
                 System.out.println(String.format(FORMAT, "Gear Profile:", application.getGearProfile().getName()));
+
+                // find the gear state
+                String state = null;
+                for (IGearGroup group : application.getGearGroups()) {
+                    for (IGear gear : group.getGears()) {
+                        state = gear.getState().name().toLowerCase();
+                    }
+                }
+                if (state != null) {
+                    System.out.println(String.format(FORMAT, "Gear State:", state));
+                }
             }
         }
         return null;
