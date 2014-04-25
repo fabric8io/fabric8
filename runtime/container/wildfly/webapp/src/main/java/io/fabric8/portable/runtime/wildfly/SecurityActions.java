f48674bf04c6ac610d623c53212bde535c6a2e21
Claus Ibsen <claus.ibsen@gmail.com>
2014-04-25 18:10:29 +0200

    Migrate license headers in runtime module

--- runtime/container/wildfly/webapp/src/main/java/io/fabric8/portable/runtime/wildfly/SecurityActions.java
+++ runtime/container/wildfly/webapp/src/main/java/io/fabric8/portable/runtime/wildfly/SecurityActions.java
@@ -1,23 +1,17 @@
-/*
- * #%L
- * JBossOSGi Framework
- * %%
- * Copyright (C) 2010 - 2012 JBoss by Red Hat
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 2.1 of the
- * License, or (at your option) any later version.
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
  *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-2.1.html>.
- * #L%
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 package io.fabric8.portable.runtime.wildfly;
 
