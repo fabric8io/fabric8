8e4d39383ae3b166a51bf901c70e4a3ccb38bfc1
Claus Ibsen <claus.ibsen@gmail.com>
2014-05-01 12:34:59 +0200

    #1171: Migrated fabric-agent-commands from blueprint to scr

--- fabric/fabric-agent-commands/src/main/java/io/fabric8/agent/commands/Watch.java
+++ fabric/fabric-agent-commands/src/main/java/io/fabric8/agent/commands/Watch.java
@@ -1,136 +1,70 @@
 /**
- *  Copyright 2005-2014 Red Hat, Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Red Hat licenses this file to you under the Apache License, version
- *  2.0 (the "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied.  See the License for the specific language governing
- *  permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.fabric8.agent.commands;
 
-import io.fabric8.agent.commands.support.ProfileVersionKey;
-import io.fabric8.agent.mvn.Parser;
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
-import org.apache.felix.gogo.commands.Option;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
+import io.fabric8.api.scr.ValidatingReference;
+import io.fabric8.boot.commands.support.AbstractCommandComponent;
+import org.apache.felix.gogo.commands.Action;
+import org.apache.felix.gogo.commands.basic.AbstractCommand;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.service.command.Function;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+@Component(immediate = true)
+@Service({Function.class, AbstractCommand.class})
+@org.apache.felix.scr.annotations.Properties({
+        @Property(name = "osgi.command.scope", value = Watch.SCOPE_VALUE),
+        @Property(name = "osgi.command.function", value = Watch.FUNCTION_VALUE)
+})
+public final class Watch extends AbstractCommandComponent {
 
-import static io.fabric8.agent.commands.support.Utils.*;
+    public static final String SCOPE_VALUE = "fabric";
+    public static final String FUNCTION_VALUE = "watch";
+    public static final String DESCRIPTION = "Watches and updates bundles.";
 
-@Command(name = "watch", scope = "fabric", description = "Watches and updates bundles.", detailedDescription = "classpath:watch.txt")
-public class Watch extends OsgiCommandSupport {
+    @Reference(referenceInterface = ProfileWatcher.class)
+    private final ValidatingReference<ProfileWatcher> profileWatcher = new ValidatingReference<ProfileWatcher>();
 
-    @Argument(index = 0, name = "urls", description = "The bundle URLs", required = false, multiValued = true)
-    List<String> urls;
+    @Activate
+    void activate() {
+        activateComponent();
+    }
 
-    @Option(name = "-i", aliases = {}, description = "Watch interval", required = false, multiValued = false)
-    private long interval;
-
-    @Option(name = "--start", description = "Starts watching the selected bundles", required = false, multiValued = false)
-    protected boolean start;
-
-    @Option(name = "--stop", description = "Stops watching all bundles", required = false, multiValued = false)
-    protected boolean stop;
-
-    @Option(name = "--remove", description = "Removes bundles from the watch list", required = false, multiValued = false)
-    protected boolean remove;
-
-    @Option(name = "--list", description = "Displays the watch list", required = false, multiValued = false)
-    protected boolean list;
-
-    @Option(name = "--no-upload", description = "If specified then updated bundles are not uploaded to the Fabric's maven proxy repository", required = false, multiValued = false)
-    protected boolean noUpload;
-
-    private ProfileWatcher watcher;
+    @Deactivate
+    void deactivate() {
+        deactivateComponent();
+    }
 
     @Override
-    protected Object doExecute() throws Exception {
-
-        if (start && stop) {
-            System.err.println("Please use only one of --start and --stop options!");
-            return null;
-        }
-
-        if (interval > 0) {
-            System.out.println("Setting watch interval to " + interval + " ms");
-            watcher.setInterval(interval);
-        }
-        if (stop) {
-            System.out.println("Stopping watch");
-            watcher.stop();
-        }
-        watcher.setUpload(!noUpload);
-        if (urls != null) {
-            if (remove) {
-                for (String url : urls) {
-                    watcher.remove(url);
-                }
-            } else {
-                for (String url : urls) {
-                    watcher.add(url);
-                }
-            }
-        }
-        if (start) {
-            System.out.println("Starting watch");
-            watcher.start();
-        }
-
-        if (list) { //List the watched bundles.
-            String format = "%-40s %-30s %-8s %-80s";
-            System.out.println(String.format(format, "URL", "Profile", "Version", "Bundle"));
-            for (String url : watcher.getWatchURLs()) {
-
-                Map<ProfileVersionKey, Map<String, Parser>> profileArtifacts = watcher.getProfileArtifacts();
-                if (profileArtifacts.size() > 0) {
-                    Set<Map.Entry<ProfileVersionKey, Map<String, Parser>>> entries = profileArtifacts.entrySet();
-                    for (Map.Entry<ProfileVersionKey, Map<String, Parser>> entry : entries) {
-                        ProfileVersionKey key = entry.getKey();
-                        Map<String, Parser> artifactMap = entry.getValue();
-                        Set<Map.Entry<String, Parser>> artifactMapEntries = artifactMap.entrySet();
-                        for (Map.Entry<String, Parser> artifactMapEntry : artifactMapEntries) {
-                            String location = artifactMapEntry.getKey();
-                            Parser parser = artifactMapEntry.getValue();
-                            if (isSnapshot(parser) || watcher.wildCardMatch(location, url)) {
-                                System.out.println(String.format(format, url, key.getProfileId(), key.getVersion(), location));
-                            }
-                        }
-                    }
-                } else {
-                    System.out.println(String.format(format, url, "", "", ""));
-                }
-            }
-        } else {
-            List<String> urls = watcher.getWatchURLs();
-            if (urls != null && urls.size() > 0) {
-                System.out.println("Watched URLs: ");
-                for (String url : watcher.getWatchURLs()) {
-                    System.out.println(url);
-                }
-            } else {
-                System.out.println("No watched URLs");
-            }
-        }
-
-        return null;
+    public Action createNewAction() {
+        assertValid();
+        return new WatchAction(profileWatcher.get());
     }
 
-    public ProfileWatcher getWatcher() {
-        return watcher;
+    void bindProfileWatcher(ProfileWatcher profileWatcher) {
+        this.profileWatcher.bind(profileWatcher);
     }
 
-    public void setWatcher(ProfileWatcher watcher) {
-        this.watcher = watcher;
+    void unbindProfileWatcher(ProfileWatcher profileWatcher) {
+        this.profileWatcher.unbind(profileWatcher);
     }
+
 }
