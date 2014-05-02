3c0eaafe78990b7fc14049aaf52d9188f92bf01a
Henryk Konsek <hekonsek@gmail.com>
2014-05-02 14:24:01 +0200

    [Spring Boot] ActiveMQ now connects to the VM broker in the test environment.

--- process/process-spring-boot/process-spring-boot-starter-activemq/src/main/java/io/fabric8/process/spring/boot/starter/activemq/ActiveMQAutoConfiguration.java
+++ process/process-spring-boot/process-spring-boot-starter-activemq/src/main/java/io/fabric8/process/spring/boot/starter/activemq/ActiveMQAutoConfiguration.java
@@ -1,8 +1,26 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
 package io.fabric8.process.spring.boot.starter.activemq;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +29,23 @@ import javax.jms.ConnectionFactory;
 @Configuration
 public class ActiveMQAutoConfiguration {
 
-    @Value("${io.fabric8.process.spring.boot.starter.activemq.broker.url:vm://testBroker?broker.persistent=false}")
+    @Value("${io.fabric8.process.spring.boot.starter.activemq.broker.url:}")
     String brokerUrl;
+
+    @Autowired
+    BrokerUrlResolver brokerUrlResolver;
+
+    @ConditionalOnMissingClass(org.junit.runner.Runner.class)
+    @Bean
+    BrokerUrlResolver defaultBrokerUrlResolver() {
+        return new DefaultBrokerUrlResolver();
+    }
+
+    @ConditionalOnClass(org.junit.runner.Runner.class)
+    @Bean
+    BrokerUrlResolver testBrokerUrlResolver() {
+        return new TestBrokerUrlResolver();
+    }
 
     @Value("${io.fabric8.process.spring.boot.starter.activemq.broker.username:}")
     String brokerUsername;
@@ -22,7 +55,14 @@ public class ActiveMQAutoConfiguration {
 
     @Bean
     ConnectionFactory pooledConnectionFactory() {
-        ActiveMQConnectionFactory amqConnectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        String resolvedBrokerUrl;
+        if(!brokerUrl.isEmpty()) {
+            resolvedBrokerUrl = brokerUrl;
+        } else {
+            resolvedBrokerUrl = brokerUrlResolver.brokerUrl();
+        }
+
+        ActiveMQConnectionFactory amqConnectionFactory = new ActiveMQConnectionFactory(resolvedBrokerUrl);
         if(!brokerUsername.isEmpty()) {
             amqConnectionFactory.setUserName(brokerUsername);
         }
