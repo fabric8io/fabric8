a90f47cee64addd5159fe6ce0ea50abbef5126ca
Henryk Konsek <hekonsek@gmail.com>
2014-05-13 14:11:11 +0200

    Created basic AbstractProcessmanagerTest API.

---
+++ process/process-spring-boot/process-spring-boot-itests/process-spring-boot-itests-invoicing/src/test/java/io/fabric8/process/spring/boot/itests/invoicing/AbstractProcessManagerTest.java
@@ -0,0 +1,62 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.process.spring.boot.itests.invoicing;
+
+import io.fabric8.process.manager.service.ProcessManagerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.ops4j.pax.exam.MavenUtils;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.web.client.ResourceAccessException;
+
+import javax.management.MalformedObjectNameException;
+import java.io.File;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+import static com.jayway.awaitility.Awaitility.waitAtMost;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public abstract class AbstractProcessManagerTest extends Assert {
+
+    protected final String projectVersion = MavenUtils.asInProject().getVersion("io.fabric8", "process-spring-boot-itests-service-invoicing");
+
+    protected ProcessManagerService processManagerService;
+
+    protected final TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Before
+    public void setUp() throws MalformedObjectNameException {
+        System.setProperty("java.protocol.handler.pkgs", "org.ops4j.pax.url");
+        processManagerService = new ProcessManagerService(new File("target", UUID.randomUUID().toString()));
+    }
+
+    protected void waitForRestResource(final String uri) {
+        waitAtMost(1, MINUTES).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                try {
+                    restTemplate.getForObject(uri, String.class);
+                } catch (ResourceAccessException e) {
+                    System.out.println(e.getMessage());
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+
+}
