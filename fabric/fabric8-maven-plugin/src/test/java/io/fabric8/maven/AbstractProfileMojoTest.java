e27d18ba50cd413c0bc267592671b1c85ee8337a
Henryk Konsek <hekonsek@gmail.com>
2014-06-01 21:21:19 +0200

    [Maven plugin] Added basic unit tests.

---
+++ fabric/fabric8-maven-plugin/src/test/java/io/fabric8/maven/AbstractProfileMojoTest.java
@@ -0,0 +1,55 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.maven;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+
+import static io.fabric8.api.FabricConstants.FABRIC_VERSION;
+
+public class AbstractProfileMojoTest extends Assert {
+
+    AbstractProfileMojo mojo = new AbstractProfileMojo() {
+        @Override
+        public void execute() throws MojoExecutionException, MojoFailureException {
+
+        }
+    };
+
+    @Test
+    public void shouldResolveProfileForSpringBootMainClass() throws MalformedURLException {
+        // Given
+        System.setProperty("java.protocol.handler.pkgs", "org.ops4j.pax.url");
+        Map<String, String> profilesMapping = mojo.getDefaultClassToProfileMap();
+        URLClassLoader jarClassLoader = new URLClassLoader(new URL[]{new URL("mvn:io.fabric8/process-spring-boot-itests-service-invoicing/" + FABRIC_VERSION + "/jar")});
+
+        // When
+        for (String mainClass : profilesMapping.keySet()) {
+            if (mojo.hasClass(jarClassLoader, mainClass)) {
+                return;
+            }
+        }
+        fail("Profile for Spring Boot main class not found.");
+    }
+
+}
