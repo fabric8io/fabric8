929172e136d334513734bbb783ef518387442ee3
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-06-16 16:42:51 +0200

    [FABRIC-1067] Use existing resolveOptionalImports to resolve bundles with dependency="true" when installing features through profiles

--- fabric/fabric-agent/src/main/java/io/fabric8/agent/Activator.java
+++ fabric/fabric-agent/src/main/java/io/fabric8/agent/Activator.java
@@ -18,22 +18,14 @@ package io.fabric8.agent;
 import io.fabric8.api.Constants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.cm.ManagedService;
 import org.slf4j.LoggerFactory;
 
-import java.util.Dictionary;
 import java.util.Hashtable;
-import java.util.concurrent.TimeUnit;
 
 public class Activator implements BundleActivator {
 
-    private static final String OBR_RESOLVE_OPTIONAL_IMPORTS = "obr.resolve.optional.imports";
-    private static final String RESOLVE_OPTIONAL_IMPORTS = "resolve.optional.imports";
-    private static final String URL_HANDLERS_TIMEOUT = "url.handlers.timeout";
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(Activator.class);
 
     private DeploymentAgent agent;
@@ -41,9 +33,6 @@ public class Activator implements BundleActivator {
 
     public void start(BundleContext context) throws Exception {
         agent = new DeploymentAgent(context);
-        Dictionary<String, Object> config = getConfig(context);
-        agent.setResolveOptionalImports(getResolveOptionalImports(config));
-        agent.setUrlHandlersTimeout(getUrlHandlersTimeout(config));
         agent.start();
         Hashtable<String, String> props = new Hashtable<String, String>();
         props.put(org.osgi.framework.Constants.SERVICE_PID, Constants.AGENT_PID);
@@ -53,47 +42,6 @@ public class Activator implements BundleActivator {
     public void stop(BundleContext context) throws Exception {
         registration.unregister();
         agent.stop();
-    }
-
-    private boolean getResolveOptionalImports(Dictionary<String, Object> config) {
-        if (config != null) {
-            String str = (String) config.get(OBR_RESOLVE_OPTIONAL_IMPORTS);
-            if (str == null) {
-                str = (String) config.get(RESOLVE_OPTIONAL_IMPORTS);
-            }
-            if (str != null) {
-                return Boolean.parseBoolean(str);
-            }
-        }
-        return false;
-    }
-
-    private long getUrlHandlersTimeout(Dictionary<String, Object> config) {
-        if (config != null) {
-            Object timeout = config.get(URL_HANDLERS_TIMEOUT);
-            if (timeout instanceof Number) {
-                return ((Number) timeout).longValue();
-            } else if (timeout instanceof String) {
-                return Long.parseLong((String) timeout);
-            }
-        }
-        return TimeUnit.MILLISECONDS.convert(5, TimeUnit.MINUTES);
-    }
-
-    private Dictionary<String, Object> getConfig(BundleContext bundleContext) {
-        try {
-            ServiceReference configAdminServiceReference = bundleContext.getServiceReference(ConfigurationAdmin.class.getName());
-            if (configAdminServiceReference != null) {
-                ConfigurationAdmin configAdmin = (ConfigurationAdmin) bundleContext.getService(configAdminServiceReference);
-                if (configAdmin != null) {
-                    Configuration[] configuration = configAdmin.listConfigurations("(service.pid=" + Constants.AGENT_PID + ")");
-                    return (configuration != null && configuration.length > 0) ? configuration[0].getProperties() : null;
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.warn("Unable to retrieve agent configuration", e);
-        }
-        return null;
     }
 
 }
