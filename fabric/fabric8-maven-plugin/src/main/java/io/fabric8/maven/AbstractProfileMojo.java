8469ad382dbba1015dc01690b9c7a20420a76ed0
Claus Ibsen <claus.ibsen@gmail.com>
2014-06-17 23:27:04 +0200

    #1779: fabri8:zip should include type in bundle url for itself, so we war projects works.

--- fabric/fabric8-maven-plugin/src/main/java/io/fabric8/maven/AbstractProfileMojo.java
+++ fabric/fabric8-maven-plugin/src/main/java/io/fabric8/maven/AbstractProfileMojo.java
@@ -438,7 +438,8 @@ public abstract class AbstractProfileMojo extends AbstractMojo {
     protected void addProjectArtifactBundle(ProjectRequirements requirements) {
         DependencyDTO rootDependency = requirements.getRootDependency();
         if (rootDependency != null) {
-            String url = rootDependency.toBundleUrl();
+            // we need url with type, so when we deploy war files the mvn url is correct
+            String url = rootDependency.toBundleUrlWithType();
             if (!requirements.getBundles().contains(url)) {
                 requirements.getBundles().add(url);
             }
