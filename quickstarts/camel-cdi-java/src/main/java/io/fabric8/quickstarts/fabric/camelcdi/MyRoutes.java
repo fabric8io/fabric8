2a0001f4114cf193ed9227cd18db066962e97433
cpansegrouw <christiaan@thumbzup.com>
2014-06-18 10:22:51 +0200

    fix import path

--- quickstarts/camel-cdi-java/src/main/java/io/fabric8/quickstarts/fabric/camelcdi/MyRoutes.java
+++ quickstarts/camel-cdi-java/src/main/java/io/fabric8/quickstarts/fabric/camelcdi/MyRoutes.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.fabric8.quickstarts.camelcdi;
+package io.fabric8.quickstarts.fabric.camelcdi;
 
 import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
