16db8ff936545db628495906b93dd47748f5ceab
Ioannis Canellos <iocanel@gmail.com>
2014-06-24 10:51:11 +0300

    Add runtime properties to all distributions.

--- runtime/container/tomcat/webapp/src/main/java/io/fabric8/portable/runtime/tomcat/FabricPropertiesProvider.java
+++ runtime/container/tomcat/webapp/src/main/java/io/fabric8/portable/runtime/tomcat/FabricPropertiesProvider.java
@@ -15,51 +15,42 @@
  */
 package io.fabric8.portable.runtime.tomcat;
 
-import io.fabric8.api.CreateEnsembleOptions;
-import io.fabric8.utils.SystemProperties;
-
-import java.io.File;
-import java.util.Properties;
+import io.fabric8.api.RuntimeService;
+import org.jboss.gravia.container.tomcat.support.TomcatPropertiesProvider;
+import org.jboss.gravia.runtime.spi.CompositePropertiesProvider;
+import org.jboss.gravia.runtime.spi.EnvPropertiesProvider;
+import org.jboss.gravia.runtime.spi.PropertiesProvider;
+import org.jboss.gravia.runtime.spi.SubstitutionPropertiesProvider;
+import org.jboss.gravia.runtime.spi.SystemPropertiesProvider;
 
 import javax.servlet.ServletContext;
-
-import org.jboss.gravia.container.tomcat.support.TomcatPropertiesProvider;
-import org.jboss.gravia.runtime.spi.PropertiesProvider;
 
 /**
  * The Fabric {@link PropertiesProvider}
  */
-public class FabricPropertiesProvider extends TomcatPropertiesProvider {
+public class FabricPropertiesProvider  implements PropertiesProvider {
+
+
+    private final PropertiesProvider delegate;
 
     public FabricPropertiesProvider(ServletContext servletContext) {
-        super(servletContext);
+        delegate = new SubstitutionPropertiesProvider(
+                new CompositePropertiesProvider(
+                        new TomcatPropertiesProvider(servletContext),
+                        new SystemPropertiesProvider(),
+                        new EnvPropertiesProvider(RuntimeService.DEFAULT_ENV_PREFIX)
+                )
+        );
     }
 
     @Override
-    protected Properties initialProperties(ServletContext servletContext) {
-        Properties properties = super.initialProperties(servletContext);
+    public Object getProperty(String key) {
+        return delegate.getProperty(key);
+    }
 
-        // Setup the karaf.home directory
-        File karafBase = new File(getCatalinaWork().getPath() + File.separator + "karaf-base");
-        File karafData = new File(karafBase.getPath() + File.separator + "data");
-        File karafEtc = new File(karafBase.getPath() + File.separator + "etc");
-        File profilesImport = new File(karafBase.getPath() + File.separator + CreateEnsembleOptions.DEFAULT_IMPORT_PATH);
-
-        // Fabric integration properties
-        properties.setProperty(CreateEnsembleOptions.PROFILES_AUTOIMPORT_PATH, profilesImport.getAbsolutePath());
-
-        // [TODO] Derive port from tomcat config
-        // https://issues.jboss.org/browse/FABRIC-761
-        properties.setProperty("org.osgi.service.http.port", "8080");
-
-        // Karaf integration properties
-        properties.setProperty(SystemProperties.KARAF_HOME, karafBase.getAbsolutePath());
-        properties.setProperty(SystemProperties.KARAF_BASE, karafBase.getAbsolutePath());
-        properties.setProperty(SystemProperties.KARAF_DATA, karafData.getAbsolutePath());
-        properties.setProperty(SystemProperties.KARAF_ETC, karafEtc.getAbsolutePath());
-        properties.setProperty(SystemProperties.KARAF_NAME, "root");
-
-        return properties;
+    @Override
+    public Object getProperty(String key, Object defaultValue) {
+        return delegate.getProperty(key, defaultValue);
     }
 
 }
