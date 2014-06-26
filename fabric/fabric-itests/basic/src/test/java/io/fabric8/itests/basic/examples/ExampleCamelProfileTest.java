d031c3794f3bdf4577f945f08eb5579d1d7df760
Claus Ibsen <claus.ibsen@gmail.com>
2014-06-26 08:45:46 +0200

    Use the log quickstart as the example for the CI test.

--- fabric/fabric-itests/basic/src/test/java/io/fabric8/itests/basic/examples/ExampleCamelProfileTest.java
+++ fabric/fabric-itests/basic/src/test/java/io/fabric8/itests/basic/examples/ExampleCamelProfileTest.java
@@ -15,8 +15,8 @@
  */
 package io.fabric8.itests.basic.examples;
 
+import java.util.Set;
 
-import static io.fabric8.zookeeper.utils.ZooKeeperUtils.setData;
 import io.fabric8.api.Container;
 import io.fabric8.api.FabricService;
 import io.fabric8.api.ServiceProxy;
@@ -24,13 +24,6 @@ import io.fabric8.itests.paxexam.support.ContainerBuilder;
 import io.fabric8.itests.paxexam.support.ContainerCondition;
 import io.fabric8.itests.paxexam.support.FabricTestSupport;
 import io.fabric8.itests.paxexam.support.Provision;
-import io.fabric8.zookeeper.ZkPath;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Set;
-
-import org.apache.curator.framework.CuratorFramework;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -52,31 +45,15 @@ public class ExampleCamelProfileTest extends FabricTestSupport {
 
         ServiceProxy<FabricService> fabricProxy = ServiceProxy.createServiceProxy(bundleContext, FabricService.class);
         try {
-            FabricService fabricService = fabricProxy.getService();
-            CuratorFramework curator = fabricService.adapt(CuratorFramework.class);
-
-            Set<Container> containers = ContainerBuilder.create(fabricProxy, 2).withName("cnt").withProfiles("default").assertProvisioningResult().build();
+            Set<Container> containers = ContainerBuilder.create(fabricProxy, 1).withName("cnt").withProfiles("quickstarts-karaf-beginner-camel.log").assertProvisioningResult().build();
+            Assert.assertEquals("Should be 1 container", 1, containers.size());
             try {
-                LinkedList<Container> containerList = new LinkedList<Container>(containers);
-                Container broker = containerList.removeLast();
-
-                setData(curator, ZkPath.CONTAINER_PROVISION_RESULT.getPath(broker.getId()), "changing");
-                System.out.println(executeCommand("fabric:container-change-profile " + broker.getId() + " mq-default"));
-                Provision.provisioningSuccess(Arrays.asList(new Container[]{broker}), PROVISION_TIMEOUT);
-                System.out.println(executeCommand("fabric:cluster-list"));
-
-                for(Container c : containerList) {
-                    setData(curator, ZkPath.CONTAINER_PROVISION_RESULT.getPath(c.getId()), "changing");
-                    System.out.println(executeCommand("fabric:container-change-profile " + c.getId() + " example-camel-mq"));
-                }
-                Provision.provisioningSuccess(containerList, PROVISION_TIMEOUT);
-
-                Assert.assertTrue(Provision.waitForCondition(containerList, new ContainerCondition() {
+                Assert.assertTrue(Provision.waitForCondition(containers, new ContainerCondition() {
                     @Override
                     public Boolean checkConditionOnContainer(final Container c) {
                         System.out.println(executeCommand("fabric:container-connect -u admin -p admin " + c.getId() + " osgi:list"));
                         System.out.println(executeCommand("fabric:container-connect -u admin -p admin " + c.getId() + " camel:route-list"));
-                        String completed = executeCommand("fabric:container-connect -u admin -p admin " + c.getId() + " camel:route-info route2 | grep \"Exchanges Completed\"");
+                        String completed = executeCommand("fabric:container-connect -u admin -p admin " + c.getId() + " camel:route-info log-route | grep \"Exchanges Completed\"");
                         System.out.println(completed);
                         if (completed.contains("Exchanges Completed") && !completed.contains("Exchanges Completed: 0")) {
                             return true;
