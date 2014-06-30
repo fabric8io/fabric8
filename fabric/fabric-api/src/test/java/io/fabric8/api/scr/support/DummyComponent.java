35e3bd720ef62ec1f58691dff646ced0529a41c9
Thomas Diesler <thomas.diesler@jboss.com>
2014-06-30 13:46:22 +0200

    Move fabric-api-tests to fabric-api

--- fabric/fabric-api/src/test/java/io/fabric8/api/scr/support/DummyComponent.java
+++ fabric/fabric-api/src/test/java/io/fabric8/api/scr/support/DummyComponent.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package io.fabric8.api.test;
+package io.fabric8.api.scr.support;
 
 import io.fabric8.api.scr.AbstractFieldInjectionComponent;
 import org.apache.felix.scr.annotations.Component;
