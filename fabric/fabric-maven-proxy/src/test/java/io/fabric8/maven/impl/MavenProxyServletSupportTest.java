1a32c9981415237a689ce3cd6d9fc102c99928fd
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-07-01 14:41:25 +0200

    [FABRIC-171] Fix test for authenticated http proxy

--- fabric/fabric-maven-proxy/src/test/java/io/fabric8/maven/impl/MavenProxyServletSupportTest.java
+++ fabric/fabric-maven-proxy/src/test/java/io/fabric8/maven/impl/MavenProxyServletSupportTest.java
@@ -182,6 +182,7 @@ public class MavenProxyServletSupportTest {
                 String proxyAuth = request.getHeader("Proxy-Authorization");
                 if (proxyAuth == null || proxyAuth.trim().equals("")) {
                     response.setStatus(HttpServletResponse.SC_PROXY_AUTHENTICATION_REQUIRED);
+                    response.addHeader("Proxy-Authenticate", "Basic realm=\"Proxy Server\"");
                     baseRequest.setHandled(true);
                 } else {
                     response.setStatus(HttpServletResponse.SC_OK);
@@ -211,7 +212,7 @@ public class MavenProxyServletSupportTest {
         System.setProperty("karaf.data", new File("target").getCanonicalPath());
         FileUtils.deleteDirectory(new File("target/tmp"));
 
-        Server server = new Server(3000);
+        Server server = new Server(0);
         server.setHandler(serverHandler);
         server.start();
 
