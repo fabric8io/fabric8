baca9927871c902b703dba58a9bb11da0894ecb6
Henryk Konsek <hekonsek@gmail.com>
2014-07-07 16:18:40 +0200

    [Spring Boot process registry] Created basic Process Registry API.

---
+++ process/process-spring-boot/process-spring-boot-registry/src/test/java/io/fabric8/process/spring/boot/registry/ProcessRegistryAutoConfigurationTest.java
@@ -0,0 +1,57 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.process.spring.boot.registry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProcessRegistryAutoConfigurationTest.class)
+@EnableAutoConfiguration
+public class ProcessRegistryAutoConfigurationTest extends Assert {
+
+    @Autowired
+    ProcessRegistry registry;
+
+    @Bean
+    RegistryProperties processRegistryProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("foo", "bar");
+        return new RegistryProperties(properties);
+    }
+
+    @Test
+    public void shouldUseRegisteredMap() {
+        assertEquals("bar", registry.readProperty("foo"));
+    }
+
+}
