ec1d447003832909c1da0b20c48de6604ecdee35
James Strachan <james.strachan@gmail.com>
2014-07-08 11:44:55 +0100

    enable the metrics by default in the test app

--- apm/src/test/java/io/fabric8/testApp/TestApp.java
+++ apm/src/test/java/io/fabric8/testApp/TestApp.java
@@ -13,11 +13,36 @@
 
 package io.fabric8.testApp;
 
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class TestApp {
+    public static ObjectName AGENT_MBEAN_NAME;
+    protected static AtomicBoolean enabledAgent = new AtomicBoolean(false);
+
+    static {
+        try {
+            AGENT_MBEAN_NAME = new ObjectName("io.fabric8.apmagent", "type", "apmAgent");
+        } catch (MalformedObjectNameException e) {
+            System.out.println("Failed to create object name: " + e);
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public static void main(String args[]) {
         try {
             System.err.println("RUNNING ...");
+
+            // lets see if the agent has started up yet:
+            checkEnabledMetrics();
             final int COUNT = 2;
             for (int i = 0; i < COUNT; i++) {
                 TestLoad testLoad = new TestLoad("TestLoad:" + i);
@@ -27,6 +52,24 @@ public class TestApp {
             testLoad.doStart();
         } catch (Throwable e) {
             e.printStackTrace();
+        }
+    }
+
+    protected static void checkEnabledMetrics() {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        if (mBeanServer != null) {
+            if (mBeanServer.isRegistered(AGENT_MBEAN_NAME)) {
+                if (enabledAgent.compareAndSet(false, true)) {
+                    System.out.println("==== HAS AGENT!");
+                    try {
+                        mBeanServer.invoke(AGENT_MBEAN_NAME, "startMetrics", new Object[0], new String[0]);
+                        System.out.println("Enabled agent " + AGENT_MBEAN_NAME);
+                    } catch (Exception e) {
+                        System.out.println("Failed to invoke the mbean: " + AGENT_MBEAN_NAME);
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
