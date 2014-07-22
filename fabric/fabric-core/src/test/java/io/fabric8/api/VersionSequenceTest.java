32de0d06ad16ef15328b8be0062d9e8d8b42b0d7
Thomas Diesler <thomas.diesler@jboss.com>
2014-07-22 22:35:36 +0200

    Migrate getVersions(), hasVersion(), deleteVersion()

--- fabric/fabric-core/src/test/java/io/fabric8/api/VersionSequenceTest.java
+++ fabric/fabric-core/src/test/java/io/fabric8/api/VersionSequenceTest.java
@@ -15,25 +15,28 @@
  */
 package io.fabric8.api;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
-
-/**
- */
-public class VersionSequenceTest {
+public final class VersionSequenceTest {
 
     @Test
     public void testSort() throws Exception {
         List<VersionSequence> list = versions("1.15", "1.5", "1.0.10", "1.0", "1.0.5");
         Collections.sort(list);
-        assertEquals(versions("1.0", "1.0.5", "1.0.10", "1.5", "1.15"), list);
+        Assert.assertEquals(versions("1.0", "1.0.5", "1.0.10", "1.5", "1.15"), list);
+    }
+
+    @Test
+    public void testComparator() throws Exception {
+        List<String> list = Arrays.asList("1.15", "1.5", "1.0.10", "1.0", "1.0.5");
+        Collections.sort(list, VersionSequence.getComparator());
+        Assert.assertEquals(Arrays.asList("1.0", "1.0.5", "1.0.10", "1.5", "1.15"), list);
     }
 
     @Test
@@ -44,7 +47,7 @@ public class VersionSequenceTest {
         assertNext("1.2.3.9", "1.2.3.10");
     }
 
-    protected List<VersionSequence> versions(String... names) {
+    private List<VersionSequence> versions(String... names) {
         List<VersionSequence> answer = new ArrayList<VersionSequence>();
         for (String name : names) {
             answer.add(new VersionSequence(name));
@@ -52,18 +55,18 @@ public class VersionSequenceTest {
         return answer;
     }
 
-    protected void assertNext(String versionName, String expectedVersionName) {
+    private void assertNext(String versionName, String expectedVersionName) {
         VersionSequence vs = new VersionSequence(versionName);
         VersionSequence next = vs.next();
-        assertNotNull(next);
+        Assert.assertNotNull(next);
         String name = next.getName();
-        assertEquals("Next version number is not correct", expectedVersionName, name);
+        Assert.assertEquals("Next version number is not correct", expectedVersionName, name);
 
         int less = vs.compareTo(next);
-        assertTrue("Old value should be less than new one: " + less, less < 0);
+        Assert.assertTrue("Old value should be less than new one: " + less, less < 0);
 
         int greater = next.compareTo(vs);
-        assertTrue("New value should be greater than old one: " + greater, greater > 0);
+        Assert.assertTrue("New value should be greater than old one: " + greater, greater > 0);
     }
 
 
