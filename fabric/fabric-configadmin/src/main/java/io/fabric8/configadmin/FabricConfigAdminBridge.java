b895d745cec89170c5eff7530f5e2e8ae234dab8
Guillaume Nodet <gnodet@gmail.com>
2014-07-24 23:02:43 +0200

    Force the use of scr 1.6.2 (at the cost of loosing the scr commands for now)

--- fabric/fabric-configadmin/src/main/java/io/fabric8/configadmin/FabricConfigAdminBridge.java
+++ fabric/fabric-configadmin/src/main/java/io/fabric8/configadmin/FabricConfigAdminBridge.java
@@ -70,7 +70,7 @@ public final class FabricConfigAdminBridge extends AbstractComponent implements 
     private final ValidatingReference<FabricService> fabricService = new ValidatingReference<FabricService>();
     @Reference(referenceInterface = ContainerRegistration.class)
     private final ValidatingReference<ContainerRegistration> containerRegistration = new ValidatingReference<ContainerRegistration>();
-    @Reference(referenceInterface = URLStreamHandlerService.class, target = "url.handler.protocol=profile")
+    @Reference(referenceInterface = URLStreamHandlerService.class, target = "(url.handler.protocol=profile)")
     private final ValidatingReference<URLStreamHandlerService> urlHandler = new ValidatingReference<URLStreamHandlerService>();
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor(new NamedThreadFactory("fabric-configadmin"));
