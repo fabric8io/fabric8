7a0591b7b65c8da65354d2da6cbb1662217783bf
Guillaume Nodet <gnodet@gmail.com>
2014-07-25 10:02:57 +0200

    Log executed commands in tests

--- tooling/testing/pax-exam-karaf/src/main/java/io/fabric8/tooling/testing/pax/exam/karaf/FabricKarafTestSupport.java
+++ tooling/testing/pax-exam-karaf/src/main/java/io/fabric8/tooling/testing/pax/exam/karaf/FabricKarafTestSupport.java
@@ -44,6 +44,8 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FabricKarafTestSupport {
 
@@ -54,6 +56,8 @@ public class FabricKarafTestSupport {
     public static final Long COMMAND_TIMEOUT = 70000L;
 
     static final ExecutorService executor = Executors.newCachedThreadPool();
+
+    static final Logger LOGGER = LoggerFactory.getLogger(FabricKarafTestSupport.class);
 
     @Inject
     protected BundleContext bundleContext;
@@ -182,6 +186,7 @@ public class FabricKarafTestSupport {
                                 System.out.println(command);
                                 System.out.flush();
                             }
+                            LOGGER.info("Executing command: " + command);
 
                             while (!Thread.currentThread().isInterrupted() && keepRunning) {
                                 try {
