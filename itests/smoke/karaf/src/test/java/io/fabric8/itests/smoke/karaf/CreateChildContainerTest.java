757b4321eccc8623dc03b681897e16757f842409
Thomas Diesler <thomas.diesler@jboss.com>
2014-07-30 14:24:17 +0200

    Move karaf smoke tests to their proper package

--- itests/smoke/karaf/src/test/java/io/fabric8/itests/smoke/karaf/CreateChildContainerTest.java
+++ itests/smoke/karaf/src/test/java/io/fabric8/itests/smoke/karaf/CreateChildContainerTest.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package io.fabric8.runtime.itests.karaf;
+package io.fabric8.itests.smoke.karaf;
 
 import io.fabric8.api.Container;
 import io.fabric8.api.FabricService;
