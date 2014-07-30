c024079bded85c9fb4d8761aa349b610b2dc5bd8
Thomas Diesler <thomas.diesler@jboss.com>
2014-07-30 14:24:17 +0200

    Move karaf smoke tests to their proper package

--- itests/smoke/karaf/src/test/java/io/fabric8/itests/smoke/karaf/EnsembleTest.java
+++ itests/smoke/karaf/src/test/java/io/fabric8/itests/smoke/karaf/EnsembleTest.java
@@ -13,21 +13,26 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package io.fabric8.runtime.itests.karaf;
+package io.fabric8.itests.smoke.karaf;
 
 import io.fabric8.api.Container;
 import io.fabric8.api.FabricService;
+import io.fabric8.api.ZooKeeperClusterService;
 import io.fabric8.runtime.itests.support.CommandSupport;
+import io.fabric8.runtime.itests.support.ContainerBuilder;
 import io.fabric8.runtime.itests.support.FabricEnsembleSupport;
 import io.fabric8.runtime.itests.support.Provision;
 import io.fabric8.runtime.itests.support.ServiceProxy;
 
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.felix.gogo.commands.Action;
 import org.apache.felix.gogo.commands.basic.AbstractCommand;
-import org.apache.karaf.admin.AdminService;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.osgi.StartLevelAware;
@@ -42,19 +47,20 @@ import org.jboss.gravia.runtime.ServiceLocator;
 import org.jboss.osgi.metadata.OSGiManifestBuilder;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.asset.Asset;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osgi.service.cm.ConfigurationAdmin;
 
 @RunWith(Arquillian.class)
-public class JoinTest {
-
-    private static final String WAIT_FOR_JOIN_SERVICE = "wait-for-service io.fabric8.boot.commands.service.JoinAvailable";
+@Ignore("[FABRIC-1127] Fix EnsembleTest")
+public class EnsembleTest {
 
     @Deployment
     @StartLevelAware(autostart = true)
     public static Archive<?> deployment() {
-        final ArchiveBuilder archive = new ArchiveBuilder("join-test");
+        final ArchiveBuilder archive = new ArchiveBuilder("ensemble-test");
         archive.addClasses(RuntimeType.TOMCAT, AnnotatedContextListener.class);
         archive.addPackage(CommandSupport.class.getPackage());
         archive.setManifest(new Asset() {
@@ -69,7 +75,7 @@ public class JoinTest {
                     builder.addImportPackages(RuntimeLocator.class, FabricService.class);
                     builder.addImportPackages(AbstractCommand.class, Action.class);
                     builder.addImportPackage("org.apache.felix.service.command;status=provisional");
-                    builder.addImportPackages(ConfigurationAdmin.class, AdminService.class);
+                    builder.addImportPackages(ConfigurationAdmin.class);
                     return builder.openStream();
                 } else {
                     ManifestBuilder builder = new ManifestBuilder();
@@ -83,39 +89,57 @@ public class JoinTest {
     }
 
     @Test
-	public void testJoin() throws Exception {
+    public void testAddAndRemove() throws Exception {
         System.err.println(CommandSupport.executeCommand("fabric:create --force --clean -n"));
-        ModuleContext moduleContext = RuntimeLocator.getRequiredRuntime().getModuleContext();;
+        ModuleContext moduleContext = RuntimeLocator.getRequiredRuntime().getModuleContext();
         ServiceProxy<FabricService> fabricProxy = ServiceProxy.createServiceProxy(moduleContext, FabricService.class);
         try {
             FabricService fabricService = fabricProxy.getService();
-
-            AdminService adminService = ServiceLocator.awaitService(moduleContext, AdminService.class);
-            String version = System.getProperty("fabric.version");
-            System.err.println(CommandSupport.executeCommand("admin:create --featureURL mvn:io.fabric8/fabric8-karaf/" + version + "/xml/features --feature fabric-git --feature fabric-agent --feature fabric-boot-commands child1"));
-
+            Set<Container> containers = ContainerBuilder.create(2).withName("ens").assertProvisioningResult().build();
             try {
-                System.err.println(CommandSupport.executeCommand("admin:start child1"));
-                Provision.instanceStarted(Arrays.asList("child1"), FabricEnsembleSupport.PROVISION_TIMEOUT);
-                System.err.println(CommandSupport.executeCommand("admin:list"));
-                String joinCommand = "fabric:join -f --zookeeper-password "+ fabricService.getZookeeperPassword() +" " + fabricService.getZookeeperUrl();
-                String response = "";
-                for (int i = 0; i < 10 && !response.contains("true"); i++) {
-                    response = CommandSupport.executeCommand("ssh:ssh -l karaf -P karaf -p " + adminService.getInstance("child1").getSshPort() + " localhost " + WAIT_FOR_JOIN_SERVICE);
-                    Thread.sleep(1000);
+                Deque<Container> containerQueue = new LinkedList<Container>(containers);
+                Deque<Container> addedContainers = new LinkedList<Container>();
+
+                for (int e = 0; e < 3 && containerQueue.size() >= 2 && containerQueue.size() % 2 == 0; e++) {
+                    Container cnt1 = containerQueue.removeFirst();
+                    Container cnt2 = containerQueue.removeFirst();
+                    addedContainers.add(cnt1);
+                    addedContainers.add(cnt2);
+                    FabricEnsembleSupport.addToEnsemble(fabricService, cnt1, cnt2);
+                    System.err.println(CommandSupport.executeCommand("config:proplist --pid io.fabric8.zookeeper"));
+
+                    System.err.println(CommandSupport.executeCommand("fabric:container-list"));
+                    System.err.println(CommandSupport.executeCommand("fabric:ensemble-list"));
+                    ZooKeeperClusterService zooKeeperClusterService = ServiceLocator.awaitService(moduleContext, ZooKeeperClusterService.class);
+                    Assert.assertNotNull(zooKeeperClusterService);
+                    List<String> ensembleContainersResult = zooKeeperClusterService.getEnsembleContainers();
+                    Assert.assertTrue(ensembleContainersResult.contains(cnt1.getId()));
+                    Assert.assertTrue(ensembleContainersResult.contains(cnt2.getId()));
+                    Provision.provisioningSuccess(Arrays.asList(fabricService.getContainers()), FabricEnsembleSupport.PROVISION_TIMEOUT);
                 }
 
-                System.err.println(CommandSupport.executeCommand("ssh:ssh -l karaf -P karaf -p " + adminService.getInstance("child1").getSshPort() + " localhost " + joinCommand));
-                Provision.containersExist(Arrays.asList("child1"), FabricEnsembleSupport.PROVISION_TIMEOUT);
-                Container child1 = fabricService.getContainer("child1");
-                System.err.println(CommandSupport.executeCommand("fabric:container-list"));
-                Provision.containersStatus(Arrays.asList(child1), "success", FabricEnsembleSupport.PROVISION_TIMEOUT);
-                System.err.println(CommandSupport.executeCommand("fabric:container-list"));
+                for (int e = 0; e < 3 && addedContainers.size() >= 2 && addedContainers.size() % 2 == 0; e++) {
+                    Container cnt1 = addedContainers.removeFirst();
+                    Container cnt2 = addedContainers.removeFirst();
+                    containerQueue.add(cnt1);
+                    containerQueue.add(cnt2);
+                    FabricEnsembleSupport.removeFromEnsemble(fabricService, cnt1, cnt2);
+                    System.err.println(CommandSupport.executeCommand("config:proplist --pid io.fabric8.zookeeper"));
+
+                    System.err.println(CommandSupport.executeCommand("fabric:container-list"));
+                    System.err.println(CommandSupport.executeCommand("fabric:ensemble-list"));
+                    ZooKeeperClusterService zooKeeperClusterService = ServiceLocator.awaitService(moduleContext, ZooKeeperClusterService.class);
+                    Assert.assertNotNull(zooKeeperClusterService);
+                    List<String> ensembleContainersResult = zooKeeperClusterService.getEnsembleContainers();
+                    Assert.assertFalse(ensembleContainersResult.contains(cnt1.getId()));
+                    Assert.assertFalse(ensembleContainersResult.contains(cnt2.getId()));
+                    Provision.provisioningSuccess(Arrays.asList(fabricService.getContainers()), FabricEnsembleSupport.PROVISION_TIMEOUT);
+                }
             } finally {
-                System.err.println(CommandSupport.executeCommand("admin:stop child1"));
+                ContainerBuilder.stop(containers);
             }
         } finally {
             fabricProxy.close();
         }
-	}
+    }
 }
