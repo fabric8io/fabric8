f05f800c5521e3cd04450b3ed6a4f4ddf3e5461c
James Strachan <james.strachan@gmail.com>
2014-08-04 21:15:43 +0100

    #2189 use the fabric8:copy-metatype plugin to copy the metatype metadata into the distro

--- fabric8-maven-plugin/src/main/java/io/fabric8/maven/CopyMetaTypeFilesMojo.java
+++ fabric8-maven-plugin/src/main/java/io/fabric8/maven/CopyMetaTypeFilesMojo.java
@@ -1,22 +1,23 @@
 package io.fabric8.maven;
 
-import io.fabric8.api.jmx.MetaTypeObjectSummaryDTO;
 import io.fabric8.common.util.Files;
 import org.apache.felix.metatype.MetaData;
 import org.apache.felix.metatype.MetaDataReader;
-import org.apache.felix.metatype.OCD;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -30,19 +31,50 @@ import java.util.zip.ZipEntry;
 @Mojo(name = "copy-metatype")
 public class CopyMetaTypeFilesMojo extends AbstractMojo {
 
-    @Parameter(property = "inputDir", defaultValue = "${project.build.directory}/${project.artifactId}-${project.version}/system")
+    @Parameter(property = "inputDir", defaultValue = "${project.build.directory}/features-repo")
     private File inputDir = new File("target/system");
 
-    @Parameter(property = "outputDir", defaultValue = "${project.build.directory}/${project.artifactId}-${project.version}/metatype")
+    @Parameter(property = "outputDir", defaultValue = "${project.build.directory}/metatype")
     private File outputDir = new File("target/metatype");
+
+    @Component
+    protected MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        if (!inputDir.exists() || !inputDir.isDirectory()) {
-            throw new MojoExecutionException("No input folder " + inputDir.getPath());
-        }
         outputDir.mkdirs();
-        processFolder(inputDir);
+        if (inputDir.exists() && inputDir.isDirectory()) {
+            processFolder(inputDir);
+        } else {
+            getLog().warn("inputDir " + inputDir + " is not an existing directory");
+        }
+
+        // now lets process the classpath elements in case there's any extra
+        processClassPath();
+    }
+
+    protected void processClassPath() throws MojoExecutionException {
+        try {
+            Set<String> elements = new HashSet<>();
+            elements.addAll(project.getCompileClasspathElements());
+            elements.addAll(project.getRuntimeClasspathElements());
+            elements.addAll(project.getSystemClasspathElements());
+            for (Object object : elements) {
+                if (object != null) {
+                    String path = object.toString();
+                    File file = new File(path);
+                    if (file.isFile() && isJar(file)) {
+                        processJar(file);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new MojoExecutionException("Failed to resolve classpath: " + e, e);
+        }
+    }
+
+    protected static boolean isJar(File file) {
+        return file.getName().endsWith(".jar");
     }
 
     protected void processFolder(File input) throws MojoExecutionException {
@@ -50,12 +82,8 @@ public class CopyMetaTypeFilesMojo extends AbstractMojo {
             File[] files = input.listFiles();
             if (files != null) {
                 for (File file : files) {
-                    if (file.isFile() && file.getName().endsWith(".jar")) {
-                        try {
-                            processJar(file);
-                        } catch (IOException e) {
-                            throw new MojoExecutionException("Failed to process jar " + file + ". " + e, e);
-                        }
+                    if (file.isFile() && isJar(file)) {
+                        processJar(file);
                     } else if (file.isDirectory()) {
                         processFolder(file);
                     }
@@ -64,45 +92,50 @@ public class CopyMetaTypeFilesMojo extends AbstractMojo {
         }
     }
 
-    protected void processJar(File file) throws IOException {
-        JarFile jarFile = new JarFile(file);
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String name = entry.getName();
-            if (name.startsWith("OSGI-INF/metatype/")) {
-                if (name.endsWith(".xml")) {
-                    MetaDataReader reader = new MetaDataReader();
-                    InputStream in = jarFile.getInputStream(entry);
-                    if (in != null) {
-                        MetaData metadata = reader.parse(in);
+    protected void processJar(File file) throws MojoExecutionException {
+        try {
+            JarFile jarFile = new JarFile(file);
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+                if (name.startsWith("OSGI-INF/metatype/")) {
+                    if (name.endsWith(".xml")) {
+                        MetaDataReader reader = new MetaDataReader();
+                        InputStream in = jarFile.getInputStream(entry);
+                        if (in != null) {
+                            MetaData metadata = reader.parse(in);
 
-                        // lets try get the i18n properties
-                        Properties properties = new Properties();
-                        String propertiesFile = name.substring(0, name.length() - 3) + "properties";
-                        ZipEntry propertiesEntry = jarFile.getEntry(propertiesFile);
-                        if (propertiesEntry != null) {
-                            InputStream propertiesIn = jarFile.getInputStream(entry);
-                            if (propertiesIn != null) {
-                                properties.load(propertiesIn);
+                            // lets try get the i18n properties
+                            Properties properties = new Properties();
+                            String propertiesFile = name.substring(0, name.length() - 3) + "properties";
+                            ZipEntry propertiesEntry = jarFile.getEntry(propertiesFile);
+                            if (propertiesEntry != null) {
+                                InputStream propertiesIn = jarFile.getInputStream(entry);
+                                if (propertiesIn != null) {
+                                    properties.load(propertiesIn);
+                                }
                             }
+                            writeMetaTypeObjects(metadata, properties, jarFile, entry);
                         }
-                        writeMetaTypeObjects(metadata, properties, jarFile, entry);
                     }
                 }
             }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to process jar " + file + ". " + e, e);
         }
-
     }
 
     protected void writeMetaTypeObjects(MetaData metadata, Properties properties, JarFile jarFile, JarEntry jarEntry) throws IOException {
-        Map<String,Object> objectClassDefinitions = metadata.getObjectClassDefinitions();
-        Set<Map.Entry<String, Object>> entries = objectClassDefinitions.entrySet();
+        Map<String, Object> map = metadata.getDesignates();
+        Set<Map.Entry<String, Object>> entries = map.entrySet();
         for (Map.Entry<String, Object> entry : entries) {
             String pid = entry.getKey();
-            Object value = entry.getValue();
-
             File pidOutDir = new File(outputDir, pid);
+            if (pidOutDir.exists()) {
+                System.out.println("PID " + pid + " already exists at " + pidOutDir);
+                return;
+            }
             pidOutDir.mkdirs();
             File xmlFile = new File(pidOutDir, "metatype.xml");
             InputStream in = jarFile.getInputStream(jarEntry);
