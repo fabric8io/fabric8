8e2f26907c9c9ffa50a41704c20e71e597e48f0c
Frank Wilson <frank@zenlambda.com>
2014-08-14 08:38:14 +0100

    Tidy the mule-helloworld example
        * Mule helloworld example was moved to regular
        quickstarts project. Fixes #2280.
    
        * This was achieved by adding bundle type and
        classifier parameters to the fabric8 maven plugin
        (profile zip goal). The classifier parameter was not
        necessary for the mule example, but it seemed logical
        to also offer this feature.
    
        * All enhancements to the maven plugin are covered
        by tests that use the maven test harness.
    
        * An archetype was also created for the mule example
    
        * The archetype permits running the
        autoscale/remote/archetype tests on the mule example,
        which appear to work.

---
+++ fabric8-maven-plugin/src/test/java/io/fabric8/maven/stubs/CreateProfileZipProjectStub.java
@@ -0,0 +1,76 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.maven.stubs;
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+import org.codehaus.plexus.util.ReaderFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateProfileZipProjectStub extends MavenProjectStub {
+
+    public CreateProfileZipProjectStub() {
+        MavenXpp3Reader pomReader = new MavenXpp3Reader();
+        Model model;
+        File pomFile = new File(getBasedir(), "pom.xml");
+        try
+        {
+
+            model = pomReader.read( ReaderFactory.newXmlReader(pomFile) );
+            setModel( model );
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+
+        setGroupId( model.getGroupId() );
+        setArtifactId( model.getArtifactId() );
+        setVersion( model.getVersion() );
+        setName( model.getName() );
+        setUrl( model.getUrl() );
+        setPackaging( model.getPackaging() );
+        setArtifact(new CreateProfileArtifactStub(getGroupId(), getArtifactId(), getVersion(), getPackaging()));
+        setFile(pomFile);
+
+        Build build = new Build();
+        build.setFinalName( model.getArtifactId() );
+        build.setDirectory( getBasedir() + "/target" );
+        build.setSourceDirectory( getBasedir() + "/src/main/java" );
+        build.setOutputDirectory( getBasedir() + "/target/classes" );
+        build.setTestSourceDirectory( getBasedir() + "/src/test/java" );
+        build.setTestOutputDirectory( getBasedir() + "/target/test-classes" );
+        setBuild( build );
+
+        List compileSourceRoots = new ArrayList();
+        compileSourceRoots.add( getBasedir() + "/src/main/java" );
+        setCompileSourceRoots( compileSourceRoots );
+
+        List testCompileSourceRoots = new ArrayList();
+        testCompileSourceRoots.add( getBasedir() + "/src/test/java" );
+        setTestCompileSourceRoots( testCompileSourceRoots );
+    }
+
+    public File getBasedir()
+    {
+        return new File( super.getBasedir() + "/src/test/resources/unit/zip-test/" );
+    }
+}
