cf2e19ed1a3fe69be131effb1efcf22450d81a16
Guillaume Nodet <gnodet@gmail.com>
2014-08-28 14:58:33 +0200

    Use a service tracker for ConfigurationAdmin in fab-osgi

--- fab/fab-osgi/src/main/java/io/fabric8/fab/osgi/internal/Activator.java
+++ fab/fab-osgi/src/main/java/io/fabric8/fab/osgi/internal/Activator.java
@@ -16,12 +16,16 @@
 package io.fabric8.fab.osgi.internal;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InterruptedIOException;
 
 import io.fabric8.fab.osgi.ServiceConstants;
 import org.ops4j.pax.url.commons.handler.HandlerActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * Activator for the fab protocol
@@ -30,6 +34,7 @@ public class Activator extends HandlerActivator<Configuration> {
     private static Activator instance;
 
     private BundleContext bundleContext;
+    private ConfigAdmin configAdmin;
     public static OsgiModuleRegistry registry = new OsgiModuleRegistry();
 
     public static Activator getInstance() {
@@ -52,20 +57,69 @@ public class Activator extends HandlerActivator<Configuration> {
     @Override
     public void start(BundleContext bundleContext) {
         this.bundleContext = bundleContext;
-
-        ServiceReference serviceReference = bundleContext.getServiceReference("org.osgi.service.cm.ConfigurationAdmin");
-        ConfigurationAdmin configurationAdmin = (ConfigurationAdmin) bundleContext.getService(serviceReference);
+        this.configAdmin = new ConfigAdmin();
+        this.configAdmin.open();
 
         File data = new File(System.getProperty("karaf.data", "."));
         registry.setDirectory(new File(data, "fab-module-registry"));
-        registry.setConfigurationAdmin(configurationAdmin);
+        registry.setConfigurationAdmin(new ConfigAdmin());
         registry.setPid("io.fabric8.fab.osgi.registry");
         registry.load();
 
         super.start(bundleContext);
     }
 
+    @Override
+    public void stop(BundleContext bundleContext) {
+        this.configAdmin.close();
+        super.stop(bundleContext);
+    }
+
     public BundleContext getBundleContext() {
         return bundleContext;
+    }
+
+    public class ConfigAdmin extends ServiceTracker<ConfigurationAdmin, ConfigurationAdmin> implements ConfigurationAdmin {
+
+        public ConfigAdmin() {
+            super(bundleContext, ConfigurationAdmin.class, null);
+        }
+
+        private ConfigurationAdmin getConfigAdmin() throws IOException {
+            try {
+                ConfigurationAdmin ca = waitForService(5000l);
+                if (ca != null) {
+                    return ca;
+                }
+                throw new IllegalStateException("ConfigurationAdmin not present");
+            } catch (InterruptedException e) {
+                throw (InterruptedIOException) new InterruptedIOException("ConfigurationAdmin not present").initCause(e);
+            }
+        }
+
+        @Override
+        public org.osgi.service.cm.Configuration createFactoryConfiguration(String factoryPid) throws IOException {
+            return getConfigAdmin().createFactoryConfiguration(factoryPid);
+        }
+
+        @Override
+        public org.osgi.service.cm.Configuration createFactoryConfiguration(String factoryPid, String location) throws IOException {
+            return getConfigAdmin().createFactoryConfiguration(factoryPid, location);
+        }
+
+        @Override
+        public org.osgi.service.cm.Configuration getConfiguration(String pid, String location) throws IOException {
+            return getConfigAdmin().getConfiguration(pid, location);
+        }
+
+        @Override
+        public org.osgi.service.cm.Configuration getConfiguration(String pid) throws IOException {
+            return getConfigAdmin().getConfiguration(pid);
+        }
+
+        @Override
+        public org.osgi.service.cm.Configuration[] listConfigurations(String filter) throws IOException, InvalidSyntaxException {
+            return getConfigAdmin().listConfigurations(filter);
+        }
     }
 }
