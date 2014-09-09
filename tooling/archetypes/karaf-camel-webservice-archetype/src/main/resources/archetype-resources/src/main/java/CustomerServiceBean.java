394e9417c11ad256e7a9931fe9e4ee3bc257f100
Claus Ibsen <claus.ibsen@gmail.com>
2014-09-09 09:49:16 +0200

    #1639: Removed not needed camel-webservice quickstart as we got better and simpler quickstarts instead.

--- tooling/archetypes/karaf-camel-webservice-archetype/src/main/resources/archetype-resources/src/main/java/CustomerServiceBean.java
+++ tooling/archetypes/karaf-camel-webservice-archetype/src/main/resources/archetype-resources/src/main/java/CustomerServiceBean.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package io.fabric8.example.camel.webservice;
+package ${package};
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
