8452a37dbd7e82f75c6e6992390890a94b956983
Claus Ibsen <claus.ibsen@gmail.com>
2014-09-10 11:13:45 +0200

    Fixed container info to show version correct after api changes

--- fabric/fabric-commands/src/main/java/io/fabric8/commands/ContainerInfoAction.java
+++ fabric/fabric-commands/src/main/java/io/fabric8/commands/ContainerInfoAction.java
@@ -59,7 +59,7 @@ public class ContainerInfoAction extends AbstractAction {
 		Container container = fabricService.getContainer(containerName);
 
 		System.out.println(String.format(FORMAT, "Name:", container.getId()));
-		System.out.println(String.format(FORMAT, "Version:", container.getVersion()));
+		System.out.println(String.format(FORMAT, "Version:", container.getVersionId()));
 		System.out.println(String.format(FORMAT, "Connected:", container.isAlive()));
         System.out.println(String.format(FORMAT, "Type:", emptyIfNull(container.getType())));
         Long processId = container.getProcessId();
