246e73fba5a44d3213403795d4e4c6a3db1c62e4
Frank Wilson <frank@zenlambda.com>
2014-10-07 13:47:10 +0100

    Log exception stack trace

--- fabric/fabric-maven-proxy/src/main/java/io/fabric8/maven/proxy/impl/MavenDownloadProxyServlet.java
+++ fabric/fabric-maven-proxy/src/main/java/io/fabric8/maven/proxy/impl/MavenDownloadProxyServlet.java
@@ -131,7 +131,7 @@ public class MavenDownloadProxyServlet extends MavenProxyServletSupport {
                 resp.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
                 LOGGER.warning("DownloadProxyServlet cannot process request as we are overloaded, returning HTTP Status: 503");
             } catch (Exception ex) {
-                LOGGER.warning("Error while downloading artifact:" + ex.getMessage());
+                LOGGER.log(Level.WARNING,"Error while downloading artifact: " + ex.getMessage(), ex);
             } finally {
                 Closeables.closeQuietly(is);
                 if (masterFuture != null && artifactFile != null) {
