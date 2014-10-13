bb20dd60a2dc0bd74dcc44f17abc146f34cb970b
James Strachan <james.strachan@gmail.com>
2014-10-13 15:25:25 +0100

    remove cruft

--- components/fabric-kubernetes-api/src/main/java/io/fabric8/kubernetes/api/Controller.java
+++ components/fabric-kubernetes-api/src/main/java/io/fabric8/kubernetes/api/Controller.java
@@ -26,7 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 import static io.fabric8.kubernetes.api.KubernetesHelper.getPodMap;
