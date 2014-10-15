8c489e96d5237ecb9d781c4df52e97d6cf675b8c
James Strachan <james.strachan@gmail.com>
2014-10-15 13:43:01 +0100

    added helper scripts to build and push images; plus added -Pdocker-build profile to build docker images as part of a build

---
+++ apps/build-and-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mvn clean install -Pdocker-build
+fabric8-mq/push.sh
+fabric8-mq-producer/push.sh
+fabric8-mq-consumer/push.sh
---
+++ apps/fabric8-mq-consumer/push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push $DOCKER_REGISTRY/fabric8/fabric8-mq-consumer:2.0.0-SNAPSHOT
