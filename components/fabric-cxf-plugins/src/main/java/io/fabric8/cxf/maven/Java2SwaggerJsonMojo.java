ad238b2082a0d14a4a9915ae1c64e8595ce317fe
James Strachan <james.strachan@gmail.com>
2014-10-15 16:12:51 +0100

    updated camel and cxf versions

--- components/fabric-cxf-plugins/src/main/java/io/fabric8/cxf/maven/Java2SwaggerJsonMojo.java
+++ components/fabric-cxf-plugins/src/main/java/io/fabric8/cxf/maven/Java2SwaggerJsonMojo.java
@@ -17,6 +17,14 @@
 package io.fabric8.cxf.maven;
 
 import io.fabric8.cxf.endpoint.SwaggerFeature;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.Feature;
+import org.apache.cxf.helpers.FileUtils;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
@@ -29,45 +37,33 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.feature.Feature;
-import org.apache.cxf.helpers.FileUtils;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectHelper;
-
 /**
  * @goal java2swagger
  * @description CXF Java To swagger json payload Tool
  * @requiresDependencyResolution test
  * @threadSafe
-*/
+ */
 public class Java2SwaggerJsonMojo extends AbstractMojo {
-    
-       
-        
+
+
     /**
      * @parameter
      */
     private String outputFile;
 
-   
-    
-    
+
     /**
      * Attach the generated swagger json file to the list of files to be deployed
      * on install. This means the swagger json file will be copied to the repository
      * with groupId, artifactId and version of the project and type "json".
-     *
+     * <p/>
      * With this option you can use the maven repository as a Service Repository.
      *
      * @parameter default-value="true"
      */
     private Boolean attachSwagger;
-    
-    
+
+
     /**
      * @parameter
      */
@@ -78,15 +74,15 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
      * @required
      */
     private List<String> classResourceNames;
-    
-        
+
+
     /**
      * @parameter expression="${project}"
      * @required
      */
     private MavenProject project;
-    
-    
+
+
     /**
      * Maven ProjectHelper.
      *
@@ -96,28 +92,27 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
     private MavenProjectHelper projectHelper;
 
 
-        
     /**
-     * @parameter 
+     * @parameter
      */
     private String outputFileName;
-    
+
     /**
      * @parameter default-value="json"
      */
     private String outputFileExtension;
-    
+
     /**
      * @parameter default-value="http://localhost:12333/cxf/swagger"
      */
     private String address;
-    
+
     private ClassLoader resourceClassLoader;
-    
-        
+
+
     public void execute() throws MojoExecutionException {
         List<Class<?>> resourceClasses = loadResourceClasses();
-        List <Object> resourceObjects = new ArrayList<Object>();
+        List<Object> resourceObjects = new ArrayList<Object>();
         for (Class<?> resourceClass : resourceClasses) {
             try {
                 resourceObjects.add(resourceClass.newInstance());
@@ -127,7 +122,7 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
                 throw new MojoExecutionException(e.getMessage(), e);
             }
         }
-        
+
         Thread.currentThread().setContextClassLoader(getClassLoader());
         List<Feature> features = new ArrayList<Feature>();
         features.add(new SwaggerFeature());
@@ -136,7 +131,7 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
         serverFacBean.setServiceBeans(resourceObjects);
         serverFacBean.setFeatures(features);
         Server server = serverFacBean.create();
-        
+
         InputStream in = null;
         try {
             String res = "";
@@ -157,16 +152,16 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
         } finally {
             server.stop();
         }
-        
+
 
     }
-    
-    
+
+
     private void generateJson(List<Class<?>> resourceClasses, String swagger) throws MojoExecutionException {
-     
+
         if (outputFile == null && project != null) {
             // Put the json in target/generated/json
-            
+
             String name = null;
             if (outputFileName != null) {
                 name = outputFileName;
@@ -175,10 +170,10 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
             } else {
                 name = "application";
             }
-            outputFile = (project.getBuild().getDirectory() + "/generated/json/" + name + "." 
-                + outputFileExtension).replace("/", File.separator);
+            outputFile = (project.getBuild().getDirectory() + "/generated/json/" + name + "."
+                    + outputFileExtension).replace("/", File.separator);
         }
-        
+
         BufferedWriter writer = null;
         try {
             FileUtils.mkDir(new File(outputFile).getParentFile());
@@ -206,29 +201,30 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
                 } else {
                     projectHelper.attachArtifact(project, "json", jsonFile);
                 }
-                
+
             }
         }
     }
 
-        
+
     private ClassLoader getClassLoader() throws MojoExecutionException {
         if (resourceClassLoader == null) {
             try {
                 List<?> runtimeClasspathElements = project.getRuntimeClasspathElements();
                 URL[] runtimeUrls = new URL[runtimeClasspathElements.size()];
                 for (int i = 0; i < runtimeClasspathElements.size(); i++) {
-                    String element = (String)runtimeClasspathElements.get(i);
+                    String element = (String) runtimeClasspathElements.get(i);
                     runtimeUrls[i] = new File(element).toURI().toURL();
                 }
                 resourceClassLoader = new URLClassLoader(runtimeUrls, Thread.currentThread()
-                    .getContextClassLoader());
+                        .getContextClassLoader());
             } catch (Exception e) {
                 throw new MojoExecutionException(e.getMessage(), e);
             }
         }
         return resourceClassLoader;
     }
+
     private List<Class<?>> loadResourceClasses() throws MojoExecutionException {
         List<Class<?>> resourceClasses = new ArrayList<Class<?>>(classResourceNames.size());
         for (String className : classResourceNames) {
@@ -236,13 +232,13 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
                 resourceClasses.add(getClassLoader().loadClass(className));
             } catch (Exception e) {
                 throw new MojoExecutionException(e.getMessage(), e);
-            } 
+            }
         }
-        
+
         return resourceClasses;
     }
-    
-    
+
+
     private static String getStringFromInputStream(InputStream in) throws Exception {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         int c = 0;
@@ -255,4 +251,3 @@ public class Java2SwaggerJsonMojo extends AbstractMojo {
     }
 
 }
-
