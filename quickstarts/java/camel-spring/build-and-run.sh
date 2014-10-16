8583c9315cef951669eee9a570d85434db88d751
Henryk Konsek <hekonsek@gmail.com>
2014-10-16 22:39:12 +0200

    Added fabric8:run to the build-and-run scripts.

--- quickstarts/java/camel-spring/build-and-run.sh
+++ quickstarts/java/camel-spring/build-and-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mvn clean install docker:build
-docker push $DOCKER_REGISTRY/quickstart/java-camel-spring:2.0.0-SNAPSHOT
+docker push $DOCKER_REGISTRY/quickstarts/java-camel-spring:2.0.0-SNAPSHOT
+mvn fabric8:deploy
 mvn fabric8:run
--- quickstarts/java/simple/build-and-run.sh
+++ quickstarts/java/simple/build-and-run.sh
@@ -2,3 +2,4 @@
 mvn clean install docker:build
 docker push $DOCKER_REGISTRY/quickstarts/java-simple:2.0.0-SNAPSHOT
 mvn fabric8:deploy
+mvn fabric8:run
