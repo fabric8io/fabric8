428742d068ad97051d703590d900c9b12950dbb1
Henryk Konsek <hekonsek@gmail.com>
2014-10-17 07:57:15 +0200

    Fixed build scripts.

--- quickstarts/java/camel-spring/build-and-run.sh
+++ quickstarts/java/camel-spring/build-and-run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mvn clean install docker:build
 docker push $DOCKER_REGISTRY/quickstart/java-camel-spring:2.0.0-SNAPSHOT
-mvn fabric8:deploy
 mvn fabric8:run
--- quickstarts/java/simple/build-and-deploy.sh
+++ quickstarts/java/simple/build-and-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean install docker:build
 docker push $DOCKER_REGISTRY/quickstart/java-simple:2.0.0-SNAPSHOT
-mvn fabric8:run
+mvn fabric8:deploy
