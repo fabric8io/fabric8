0ded050ef1ca3659a0e08dfd29d83db1c7764bdc
Jimmi Dyson <jimmidyson@gmail.com>
2014-10-19 21:49:10 +0100

    Working cluster with docker registry mirror & hawtio

--- bin/start-kube-cluster.sh
+++ bin/start-kube-cluster.sh
@@ -27,8 +27,21 @@ export KUBERNETES_PROVIDER=vagrant
 export KUBERNETES_MASTER=https://10.245.1.2
 ./cluster/kube-up.sh
 
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-controller.json create replicationControllers
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/hawtio-controller.json create replicationControllers
+MINION_1_IP=10.245.2.2
 
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-mirror-controller.json create replicationControllers
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-mirror-service.json create services
+
+while  ! curl -s -m 10  "http://${MINION_1_IP}:5001" > /dev/null;  do echo "DEBUG: Docker registry mirror not running yet." ; sleep 2s; done; echo "INFO: Docker registry mirror ready."
+
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-controller.json create replicationControllers
 ${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-service.json create services
+
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/hawtio-controller.json create replicationControllers
 ${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/hawtio-service.json create services
+
+while  ! curl -s -m 10  "http://${MINION_1_IP}:5000" > /dev/null;  do echo "DEBUG: Docker registry not running yet." ; sleep 2s; done; echo "INFO: Docker registry ready."
+
+while  ! curl -s -m 10  "http://${MINION_1_IP}:8080" > /dev/null;  do echo "DEBUG: Fabric8 Hawtio not running yet." ; sleep 2s; done; echo "INFO: Fabric8 Hawtio ready."
+
+echo "Hawtio is available at http://${MINION_1_IP}:8080/hawtio"
