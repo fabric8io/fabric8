7e02a71d299a6b2f0d72e3bad6554c93e1b06d37
Jimmi Dyson <jimmidyson@gmail.com>
2014-10-20 17:41:57 +0100

    Move kube-cluster about

--- bin/start-kube-cluster.sh
+++ bin/start-kube-cluster.sh
@@ -29,19 +29,19 @@ export KUBERNETES_MASTER=https://10.245.1.2
 
 MINION_1_IP=10.245.2.2
 
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-mirror-controller.json create replicationControllers
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-mirror-service.json create services
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/docker-registry-mirror-controller.json create replicationControllers
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/apps/docker-registry-mirror-service.json create services
 
-while  ! curl -s -m 10  "http://${MINION_1_IP}:5001" > /dev/null;  do echo "DEBUG: Docker registry mirror not running yet." ; sleep 2s; done; echo "INFO: Docker registry mirror ready."
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/docker-registry-controller.json create replicationControllers
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/docker-registry-service.json create services
 
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-controller.json create replicationControllers
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/docker-registry-service.json create services
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/hawtio-controller.json create replicationControllers
+${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${KUBE_DIR}/hawtio-service.json create services
 
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/hawtio-controller.json create replicationControllers
-${KUBE_DIR}/kubernetes/cluster/kubecfg.sh -c ${ROOTDIR}/apps/hawtio-service.json create services
+while  ! curl -s -m 10  "http://${MINION_1_IP}:5001" > /dev/null;  do echo "DEBUG: Docker registry mirror not running yet." ; sleep 10s; done; echo "INFO: Docker registry mirror ready."
 
-while  ! curl -s -m 10  "http://${MINION_1_IP}:5000" > /dev/null;  do echo "DEBUG: Docker registry not running yet." ; sleep 2s; done; echo "INFO: Docker registry ready."
+while  ! curl -s -m 10  "http://${MINION_1_IP}:5000" > /dev/null;  do echo "DEBUG: Docker registry not running yet." ; sleep 10s; done; echo "INFO: Docker registry ready."
 
-while  ! curl -s -m 10  "http://${MINION_1_IP}:8080" > /dev/null;  do echo "DEBUG: Fabric8 Hawtio not running yet." ; sleep 2s; done; echo "INFO: Fabric8 Hawtio ready."
+while  ! curl -s -m 10  "http://${MINION_1_IP}:8080" > /dev/null;  do echo "DEBUG: Fabric8 Hawtio not running yet." ; sleep 10s; done; echo "INFO: Fabric8 Hawtio ready."
 
 echo "Hawtio is available at http://${MINION_1_IP}:8080/hawtio"
