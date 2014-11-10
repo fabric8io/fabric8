324d7c1fb67c07398b697326ef1a59750a29e1c6
Jimmi Dyson <jimmidyson@gmail.com>
2014-11-10 13:57:49 +0000

    Tidy up old kube cluster stuff

--- kube-cluster/clean-kube-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./kubernetes/cluster/kubecfg.sh delete services/registry-mirror-service
-./kubernetes/cluster/kubecfg.sh delete services/registry-service
-./kubernetes/cluster/kubecfg.sh delete services/hawtio-service
-
-./kubernetes/cluster/kubecfg.sh delete replicationControllers/registryMirrorController
-./kubernetes/cluster/kubecfg.sh delete replicationControllers/registryController
-./kubernetes/cluster/kubecfg.sh delete replicationControllers/hawtioController
