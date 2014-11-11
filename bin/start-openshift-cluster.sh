e517c8a900188c0cdddddd2cdaf9f61c3db2c619
Jimmi Dyson <jimmidyson@gmail.com>
2014-11-11 14:01:29 +0000

    Use hostnames in /etc/hosts

--- bin/start-openshift-cluster.sh
+++ bin/start-openshift-cluster.sh
@@ -22,9 +22,9 @@ vagrant up
 vagrant ssh minion-1 -- sudo ip route add 10.244.2.0/24 via 10.245.2.3 dev enp0s8
 vagrant ssh minion-2 -- sudo ip route add 10.244.1.0/24 via 10.245.2.2 dev enp0s8
 
-grep master /etc/hosts || echo '10.245.1.2 master' | sudo tee -a /etc/hosts
-grep minion-1 /etc/hosts || echo '10.245.1.3 minion-1' |sudo tee -a /etc/hosts
-grep minion-2 /etc/hosts || echo '10.245.1.4 minion-2' | sudo tee -a /etc/hosts
+grep openshift-master /etc/hosts || echo '10.245.1.2 openshift-master' | sudo tee -a /etc/hosts
+grep openshift-minion-1 /etc/hosts || echo '10.245.1.3 openshift-minion-1' |sudo tee -a /etc/hosts
+grep openshift-minion-2 /etc/hosts || echo '10.245.1.4 openshift-minion-2' | sudo tee -a /etc/hosts
 
 cp ../build-latest-cadvisor.sh .
 
@@ -41,5 +41,5 @@ for m in minion-1 minion-2; do
     google/cadvisor:canary
 done
 
-export KUBERNETES_MASTER=http://master:8080
+export KUBERNETES_MASTER=http://openshift-master:8080
 ./_output/local/bin/linux/amd64/openshift kube apply -c ../influxdb.json
