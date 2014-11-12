629328e8d4b4dff48910d1853632bc18eeef40f1
James Strachan <james.strachan@gmail.com>
2014-11-12 06:16:53 +0000

    fixed start openshift docker script so it works on a mac and can reuse a pre-defined DOCKER_IP (e.g. if defined from boot2docker)

--- bin/start-openshift-docker.sh
+++ bin/start-openshift-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-export DOCKER_IP=127.0.0.1
+export DOCKER_IP=${DOCKER_IP:-127.0.0.1}
 export DOCKER_REGISTRY=$DOCKER_IP:5000
 export KUBERNETES_MASTER=http://$DOCKER_IP:8080
 export FABRIC8_CONSOLE=http://$DOCKER_IP:8484/hawtio
 
-docker run -d --name=openshift -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/manifests --privileged --net=host openshift/origin:latest start
+docker run -d --name=openshift -v /var/run/docker.sock:/var/run/docker.sock --privileged --net=host openshift/origin:latest start
 
 docker run -d --name=cadvisor -p 4194:8080 \
   --volume=/:/rootfs:ro \
@@ -14,7 +14,20 @@ docker run -d --name=cadvisor -p 4194:8080 \
   --volume=/var/lib/docker/:/var/lib/docker:ro \
   google/cadvisor:latest
 
-docker exec openshift openshift kube apply -c /manifests/registry.json
-docker exec openshift openshift kube apply -c /manifests/influxdb.json
-docker exec openshift openshift kube apply -c /manifests/fabric8.json
-docker exec openshift openshift kube apply -c /manifests/elasticsearch.json
+
+# using an env var but ideally we'd use an alias ;)
+KUBE="docker run -i -e KUBERNETES_MASTER=http://$DOCKER_IP:8080 openshift/origin:latest kube"
+
+#
+# Discover the APP_BASE from the location of this script.
+#
+if [ -z "$APP_BASE" ] ; then
+  DIRNAME=`dirname "$0"`
+  APP_BASE=`cd "$DIRNAME"; pwd`
+  export APP_BASE
+fi
+
+cat $APP_BASE/registry.json | $KUBE apply -c -
+cat $APP_BASE/influxdb.json | $KUBE apply -c -
+cat $APP_BASE/fabric8.json | $KUBE apply -c -
+cat $APP_BASE/elasticsearch.json | $KUBE apply -c -
