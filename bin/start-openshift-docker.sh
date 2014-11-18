32482956eaaae5291428f404191fd0954c44dc1d
James Rawlings <jamesrawlings@rely.eclpse.co.uk>
2014-11-18 19:54:02 +0000

    Adding fluentd logging, elastic search and Kibana
    The commit adds a number of containers to the start script so we can view logs from all docker containers using the Kibana console

--- bin/start-openshift-docker.sh
+++ bin/start-openshift-docker.sh
@@ -81,12 +81,16 @@ if [ -f "$APP_BASE/registry.json" ]; then
   cat $APP_BASE/registry.json | $KUBE apply -c -
   cat $APP_BASE/influxdb.json | $KUBE apply -c -
   cat $APP_BASE/elasticsearch.json | $KUBE apply -c -
+  cat $APP_BASE/fluentd.yml | $KUBE apply -c -
+  cat $APP_BASE/kibana.yml | $KUBE apply -c -
 else
   $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/kube-socat.json
   $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/fabric8.json
   $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/registry.json
   $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/influxdb.json
   $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/elasticsearch.json
+  $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/fluentd.yml
+  $KUBE apply -c https://raw.githubusercontent.com/fabric8io/fabric8/master/bin/kibana.yml
 fi
 
 K8S_SERVICES=$($KUBE list services)
@@ -100,10 +104,17 @@ getServiceIpAndPort()
   echo `echo "$1"|grep $2| sed 's/\s\+/ /g' | awk '{ print $3 ":" $4 }'`
 }
 
+getServiceIp()
+{
+  echo `echo "$1"|grep $2| sed 's/\s\+/ /g' | awk '{ print $3 }'`
+}
+
 FABRIC8_CONSOLE=http://$(getServiceIpAndPort "$K8S_SERVICES" hawtio-service)/hawtio/
 DOCKER_REGISTRY=http://$(getServiceIpAndPort "$K8S_SERVICES" registry)
 INFLUXDB=http://$(getServiceIpAndPort "$K8S_SERVICES" influx-master)
 ELASTICSEARCH=http://$(getServiceIpAndPort "$K8S_SERVICES" elasticsearch)
+ELASTICSEARCH_HOST=$(getServiceIp "$K8S_SERVICES" elasticsearch)
+KIBANA_CONSOLE=http://$(getServiceIpAndPort "$K8S_SERVICES" kibana-service)
 KUBERNETES=http://$DOCKER_IP:8080
 CADVISOR=http://$DOCKER_IP:4194
 
@@ -121,17 +132,25 @@ validateService "Influxdb" $INFLUXDB
 validateService "Elasticsearch" $ELASTICSEARCH
 validateService "Kubernetes master" $KUBERNETES
 validateService "cadvisor" $CADVISOR
+#validateService "Kibana console" $KIBANA_CONSOLE
+
+# temorary workaround to update the fluentd config with the Elastic Search service ip see https://github.com/GoogleCloudPlatform/kubernetes/blob/master/contrib/logging/fluentd-es-image/td-agent.conf#L8
+docker exec -ti $(docker ps | grep kubernetes/fluentd-elasticsearch | cut -c 1-12) sed -i 's/host/host '$ELASTICSEARCH_HOST'/g' /etc/td-agent/td-agent.conf > /dev/null 2>&1
+docker restart $(docker ps | grep kubernetes/fluentd-elasticsearch | cut -c 1-12) > /dev/null 2>&1
+# annoying hack finished
 
 echo
 echo "You're all up & running! Here are the available services:"
 echo
 SERVICE_TABLE="Service|URL\n-------|---"
 SERVICE_TABLE="$SERVICE_TABLE\nFabric8 console|$FABRIC8_CONSOLE"
+SERVICE_TABLE="$SERVICE_TABLE\nKibana console|$KIBANA_CONSOLE"
 SERVICE_TABLE="$SERVICE_TABLE\nDocker Registry|$DOCKER_REGISTRY"
 SERVICE_TABLE="$SERVICE_TABLE\nInfluxdb|$INFLUXDB"
 SERVICE_TABLE="$SERVICE_TABLE\nElasticsearch|$ELASTICSEARCH"
 SERVICE_TABLE="$SERVICE_TABLE\nKubernetes master|$KUBERNETES"
 SERVICE_TABLE="$SERVICE_TABLE\nCadvisor|$CADVISOR"
+
 printf "$SERVICE_TABLE" | column -t -s '|'
 
 if [[ $OSTYPE == darwin* ]]; then
