18cb30d56d2d1c36c70d141ba1ff28929601449f
James Strachan <james.strachan@gmail.com>
2014-11-26 12:31:35 +0000

    add a little helper script to pull down some docker image demos

---
+++ bin/docker-pull-demos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export FABRIC8_VERSION=2.0.9.1
+
+echo "pulling demo images...."
+
+docker pull fabric8/api-registry:$FABRIC8_VERSION
+docker pull fabric8/quickstart-java-cxf-cdi:$FABRIC8_VERSION
+docker pull fabric8/fabric8-mq-consumer:$FABRIC8_VERSION
+docker pull fabric8/fabric8-mq-producer:$FABRIC8_VERSION
+docker pull fabric8/fabric8-mq:$FABRIC8_VERSION
+
+echo "demos pulled!"
