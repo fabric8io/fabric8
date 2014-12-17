eecbec0ab32b52577e582ce6687e72a4430d0713
James Strachan <james.strachan@gmail.com>
2014-12-17 17:23:05 +0000

    adds an assertion library for kubernetes objects for easier testing. fixes #3271

---
+++ components/kubernetes-assertions/ReadMe.md
@@ -0,0 +1,13 @@
+## Kubernetes Assertions
+
+This library provides a bunch of helpful [assertj](http://joel-costigliola.github.io/assertj/) assertions for working with the [kubernetes-api](https://github.com/fabric8io/fabric8/tree/master/components/kubernetes-api).
+
+### Add it to your Maven pom.xml
+
+To be able to use the Java code in your [Apache Maven](http://maven.apache.org/) based project add this into your pom.xml
+
+            <dependency>
+                <groupId>io.fabric8</groupId>
+                <artifactId>kubernetes-assertions</artifactId>
+                <version>2.0.15</version>
+            </dependency>
---
+++ components/kubernetes-assertions/src/test/java/io/fabric8/kubernetes/assertions/ExampleTest.java
@@ -0,0 +1,71 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.kubernetes.assertions;
+
+import io.fabric8.kubernetes.api.model.PodSchema;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+
+/**
+ */
+public class ExampleTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        String expectedId = "abc";
+        Map<String,String> expectedLabels = new HashMap<>();
+        expectedLabels.put("foo", "bar");
+
+        PodSchema pod = new PodSchema();
+        pod.setId(expectedId);
+
+        pod.setLabels(expectedLabels);
+
+        assertThat(pod).hasId(expectedId).hasLabels(expectedLabels);
+
+
+
+        AssertionError error = null;
+        try {
+            assertThat(pod).hasId("cheese");
+        } catch (AssertionError e) {
+            error = e;
+        }
+        assertThat(error).isNotNull();
+        System.out.println("Caught expected " + error.getMessage());
+
+        error = null;
+        try {
+            Map<String,String> wrongLabels = new HashMap<>();
+            wrongLabels.put("bar", "whatnot");
+            assertThat(pod).hasLabels(wrongLabels);
+        } catch (AssertionError e) {
+            error = e;
+        }
+        assertThat(error).isNotNull();
+        System.out.println("Caught expected " + error.getMessage());
+    }
+
+}
