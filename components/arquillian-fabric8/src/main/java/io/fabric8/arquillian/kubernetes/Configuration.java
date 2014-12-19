2e5b34801e28da6d0d6cd71266bbc95648ecbcc3
Ioannis Canellos <iocanel@gmail.com>
2014-12-19 12:55:28 +0200

    Fix handling of timeout and poll interval.

--- components/arquillian-fabric8/src/main/java/io/fabric8/arquillian/kubernetes/Configuration.java
+++ components/arquillian-fabric8/src/main/java/io/fabric8/arquillian/kubernetes/Configuration.java
@@ -34,7 +34,7 @@ public class Configuration {
 
     private String masterUrl;
     private URL configUrl;
-    private long timeout = 5 * 60 * 10000;
+    private long timeout = 5 * 60 * 1000;
     private long pollInterval = 5 * 1000;
 
     public String getMasterUrl() {
@@ -78,7 +78,7 @@ public class Configuration {
             }
 
             if (map.containsKey(POLL_INTERVAL)) {
-                configuration.timeout = Long.parseLong(map.get(POLL_INTERVAL));
+                configuration.pollInterval = Long.parseLong(map.get(POLL_INTERVAL));
             }
 
         } catch (Throwable t) {
