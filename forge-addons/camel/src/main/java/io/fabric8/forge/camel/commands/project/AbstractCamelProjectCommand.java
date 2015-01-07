177bc92e6fbb11ccf315123eccca224836d52b2f
Claus Ibsen <claus.ibsen@gmail.com>
2015-01-07 13:56:22 +0100

    #2735: Working on forge commands for setup Camel project.

---
+++ forge-addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/AbstractCamelProjectCommand.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.forge.camel.commands.project;
+
+import java.util.List;
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.configuration.Configuration;
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.addon.projects.facets.DependencyFacet;
+import org.jboss.forge.addon.projects.ui.AbstractProjectCommand;
+
+public abstract class AbstractCamelProjectCommand extends AbstractProjectCommand {
+
+    public static String CATEGORY = "Camel";
+
+    @Inject
+    protected ProjectFactory projectFactory;
+
+    @Inject
+    protected Configuration configuration;
+
+    @Override
+    protected boolean isProjectRequired() {
+        return true;
+    }
+
+    @Override
+    protected ProjectFactory getProjectFactory() {
+        return projectFactory;
+    }
+
+    protected Dependency findCamelCoreDependency(Project project) {
+        List<Dependency> dependencies = project.getFacet(DependencyFacet.class).getEffectiveDependencies();
+        for (Dependency d : dependencies) {
+            if ("org.apache.camel".equals(d.getCoordinate().getGroupId()) && "camel-core".equals(d.getCoordinate().getArtifactId())) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+}
