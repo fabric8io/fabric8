3e9ec23ae14c50fe4fe19c34c3618b91084c68f2
James Strachan <james.strachan@gmail.com>
2015-02-04 10:45:14 +0000

    return nicer exception messages when kube REST calls barf

--- components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/ExceptionResponseMapper.java
+++ components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/ExceptionResponseMapper.java
@@ -1,25 +1,30 @@
 package io.fabric8.kubernetes.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.Status;
-import io.fabric8.kubernetes.api.model.StatusError;
-
-import java.io.InputStream;
-
-import javax.ws.rs.core.Response;
-
 import org.apache.cxf.jaxrs.client.ResponseExceptionMapper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
 
 public class ExceptionResponseMapper implements ResponseExceptionMapper<Exception> {
     @Override
     public Exception fromResponse(Response response) {
         try {
-
             ObjectMapper mapper = new ObjectMapper();
-            Status error = mapper.readValue((InputStream) response.getEntity(), Status.class);
-
-            return new KubernetesApiException(error.getMessage());
+            Object entity = response.getEntity();
+            String message = "No message";
+            if (entity instanceof InputStream) {
+                Status error = mapper.readValue((InputStream) entity, Status.class);
+                if (error != null) {
+                    message = error.getMessage();
+                }
+            } else if (entity != null) {
+                message = entity.toString();
+            }
+            message = "HTTP " + response.getStatus() + " " + message;
+            return new WebApplicationException(message, response);
         } catch (Exception ex) {
             return new Exception(
                     "Could not deserialize server side exception: " + ex.getMessage());
