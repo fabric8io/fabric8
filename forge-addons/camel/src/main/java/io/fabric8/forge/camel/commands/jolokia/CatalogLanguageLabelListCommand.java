9a0e6d3e97ab6241a75a9932f61a565a97c0277b
Claus Ibsen <claus.ibsen@gmail.com>
2015-02-19 13:42:01 +0100

    Camel forge commands should only be enabled if connected

--- forge-addons/camel/src/main/java/io/fabric8/forge/camel/commands/jolokia/CatalogLanguageLabelListCommand.java
+++ forge-addons/camel/src/main/java/io/fabric8/forge/camel/commands/jolokia/CatalogLanguageLabelListCommand.java
@@ -49,7 +49,7 @@ public class CatalogLanguageLabelListCommand extends AbstractJolokiaCommand {
     @Override
     public boolean isEnabled(UIContext context) {
         // we dont want this in GUI as it dont add value there
-        return !context.getProvider().isGUI();
+        return !context.getProvider().isGUI() && getJolokiaUrl() != null;
     }
 
     @Override
