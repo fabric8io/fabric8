1d3de15799f6a08e5d6c3e448600a030d4a97167
Ioannis Canellos <iocanel@gmail.com>
2015-03-10 00:32:29 +0200

    Session services ready policy should wait for a connection to the actual endpoint instead of the proxy.

--- components/arquillian-fabric8/src/main/java/io/fabric8/arquillian/kubernetes/await/SessionServicesAreReady.java
+++ components/arquillian-fabric8/src/main/java/io/fabric8/arquillian/kubernetes/await/SessionServicesAreReady.java
@@ -18,11 +18,8 @@ package io.fabric8.arquillian.kubernetes.await;
 
 import io.fabric8.arquillian.kubernetes.Configuration;
 import io.fabric8.arquillian.kubernetes.Session;
-import io.fabric8.arquillian.utils.Util;
 import io.fabric8.kubernetes.api.KubernetesClient;
-import io.fabric8.kubernetes.api.KubernetesHelper;
 import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.utils.Filter;
 
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -52,20 +49,39 @@ public class SessionServicesAreReady implements Callable<Boolean> {
             session.getLogger().warn("No services are available yet, waiting...");
         } else if (configuration.isWaitForServiceConnection()) {
             for (Service s : filterServices(services, configuration.getWaitForServices())) {
-                String serviceStatus = null;
-                try (Socket socket = new Socket()) {
-                    socket.connect(new InetSocketAddress(s.getPortalIP(), s.getPort()), configuration.getServiceConnectionTimeout());
-                    serviceStatus = "Service: " + s.getId() + " is ready";
-                } catch (Exception e) {
+                if (!isEndpointAvailable(s)) {
                     result = false;
-                    serviceStatus = "Service: " + s.getId() + " is not ready! Error: " + e.getMessage();
-                } finally {
-                    session.getLogger().warn(serviceStatus);
+                    break;
                 }
             }
         }
         return result;
     }
+
+    /**
+     * Checks if there is an endpoint for the service available.
+     * @param s The target service.
+     * @return  Returns true if a connection to at least one of the endpoints is possible.
+     */
+    private boolean isEndpointAvailable(Service s) {
+        String serviceStatus = null;
+        boolean result = false;
+        for (String endpoit : kubernetesClient.endpointsForService(s.getId(), s.getNamespace()).getEndpoints()) {
+            String addr = endpoit.substring(0, endpoit.indexOf(":"));
+            Integer port = Integer.parseInt(endpoit.substring(endpoit.indexOf(":") + 1));
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress(addr, port), configuration.getServiceConnectionTimeout());
+                serviceStatus = "Service: " + s.getId() + " is ready. Provider:"+ addr+".";
+                return true;
+            } catch (Exception e) {
+                serviceStatus = "Service: " + s.getId() + " is not ready! Error: " + e.getMessage();
+            } finally {
+                session.getLogger().warn(serviceStatus);
+            }
+        }
+        return result;
+    }
+
 
     private List<Service> filterServices(List<Service> services, List<String> selectedIds) {
         if (selectedIds != null && !selectedIds.isEmpty()) {
