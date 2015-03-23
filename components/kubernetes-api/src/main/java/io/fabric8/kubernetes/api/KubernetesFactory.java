150208e0152d09529dbc1b535720a308f42311ec
Jimmi Dyson <jimmidyson@gmail.com>
2015-03-23 12:07:52 +0000

    Configurable kubernetes certificate/key data via env vars

--- components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/KubernetesFactory.java
+++ components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/KubernetesFactory.java
@@ -20,6 +20,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.cfg.Annotations;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import io.fabric8.utils.Strings;
+import net.oauth.signature.pem.PEMReader;
+import net.oauth.signature.pem.PKCS1EncodedKeySpec;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
@@ -30,14 +32,17 @@ import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.*;
 import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.RSAPrivateCrtKeySpec;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,15 +57,21 @@ public class KubernetesFactory {
 
     public static final String KUBERNETES_TRUST_ALL_CERIFICATES = "KUBERNETES_TRUST_CERT";
 
-    public static final String KUBERNETES_USERNAME = "KUBERNETES_USERNAME";
-
-    public static final String KUBERNETES_PASSWORD = "KUBERNETES_PASSWORD";
     public static final String KUBERNETES_SERVICE_HOST_ENV_VAR = "KUBERNETES_SERVICE_HOST";
     public static final String KUBERNETES_SERVICE_PORT_ENV_VAR = "KUBERNETES_SERVICE_PORT";
     public static final String KUBERNETES_RO_SERVICE_HOST_ENV_VAR = "KUBERNETES_RO_SERVICE_HOST";
     public static final String KUBERNETES_RO_SERVICE_PORT_ENV_VAR = "KUBERNETES_RO_SERVICE_PORT";
     public static final String KUBERNETES_MASTER_ENV_VAR = "KUBERNETES_MASTER";
-    public static final String KUBERNETES_CA_CERTIFICATE_ENV_VAR = "KUBERNETES_CA_CERTIFICATE";
+    public static final String KUBERNETES_CA_CERTIFICATE_FILE_ENV_VAR = "KUBERNETES_CA_CERTIFICATE_FILE";
+    public static final String KUBERNETES_CLIENT_CERTIFICATE_FILE_ENV_VAR = "KUBERNETES_CLIENT_CERTIFICATE_FILE";
+    public static final String KUBERNETES_CLIENT_KEY_FILE_ENV_VAR = "KUBERNETES_CLIENT_KEY_FILE";
+    public static final String KUBERNETES_CLIENT_KEY_DATA_ENV_VAR = "KUBERNETES_CLIENT_KEY_DATA";
+    public static final String KUBERNETES_CA_CERTIFICATE_DATA_ENV_VAR = "KUBERNETES_CA_CERTIFICATE_DATA";
+    public static final String KUBERNETES_CLIENT_CERTIFICATE_DATA_ENV_VAR = "KUBERNETES_CLIENT_CERTIFICATE_DATA";
+    public static final String KUBERNETES_CLIENT_KEY_ALGO_ENV_VAR = "KUBERNETES_CLIENT_KEY_ALGO";
+    public static final String KUBERNETES_CLIENT_KEY_PASSWORD_ENV_VAR = "KUBERNETES_CLIENT_KEY_PASSWORD";
+    public static final String KUBERNETES_USERNAME_ENV_VAR = "KUBERNETES_USERNAME";
+    public static final String KUBERNETES_PASSWORD_ENV_VAR = "KUBERNETES_PASSWORD";
     public static final String KUBERNETES_MASTER_SYSTEM_PROPERTY = "kubernetes.master";
 
     private String address;
@@ -68,6 +79,13 @@ public class KubernetesFactory {
     private boolean trustAllCerts = false;
 
     private File caCertFile;
+    private File clientCertFile;
+    private File clientKeyFile;
+    private String caCertData;
+    private String clientCertData;
+    private String clientKeyData;
+    private String clientKeyAlgo = "RSA";
+    private char[] clientKeyPassword = new char[]{};
     private String username;
     private String password;
 
@@ -114,20 +132,58 @@ public class KubernetesFactory {
     private void init() {
         if (System.getenv(KUBERNETES_TRUST_ALL_CERIFICATES) != null) {
             this.trustAllCerts = Boolean.valueOf(System.getenv(KUBERNETES_TRUST_ALL_CERIFICATES));
-        } else if (System.getenv(KUBERNETES_CA_CERTIFICATE_ENV_VAR) != null) {
-            File possibleCaCertFile = new File(System.getenv(KUBERNETES_CA_CERTIFICATE_ENV_VAR));
-            if (possibleCaCertFile.exists() && possibleCaCertFile.canRead()) {
-                this.caCertFile = possibleCaCertFile;
+        } else if (System.getenv(KUBERNETES_CA_CERTIFICATE_FILE_ENV_VAR) != null) {
+            File candidateCaCertFile = new File(System.getenv(KUBERNETES_CA_CERTIFICATE_FILE_ENV_VAR));
+            if (candidateCaCertFile.exists() && candidateCaCertFile.canRead()) {
+                this.caCertFile = candidateCaCertFile;
             } else {
-                log.error("Specified CA certificate file {} does not exist or is not readable", possibleCaCertFile);
+                log.error("Specified CA certificate file {} does not exist or is not readable", candidateCaCertFile);
             }
         }
 
-        if (System.getenv(KUBERNETES_USERNAME) != null) {
-            this.username = System.getenv(KUBERNETES_USERNAME);
+        if (System.getenv(KUBERNETES_CA_CERTIFICATE_DATA_ENV_VAR) != null) {
+            this.caCertData = System.getenv(KUBERNETES_CA_CERTIFICATE_DATA_ENV_VAR);
         }
-        if (System.getenv(KUBERNETES_PASSWORD) != null) {
-            this.password = System.getenv(KUBERNETES_PASSWORD);
+
+        if (System.getenv(KUBERNETES_CLIENT_CERTIFICATE_FILE_ENV_VAR) != null) {
+            File candidateClientCertFile = new File(System.getenv(KUBERNETES_CLIENT_CERTIFICATE_FILE_ENV_VAR));
+            if (candidateClientCertFile.exists() && candidateClientCertFile.canRead()) {
+                this.clientCertFile = candidateClientCertFile;
+            } else {
+                log.error("Specified client certificate file {} does not exist or is not readable", candidateClientCertFile);
+            }
+        }
+
+        if (System.getenv(KUBERNETES_CLIENT_CERTIFICATE_DATA_ENV_VAR) != null) {
+            this.clientCertData = System.getenv(KUBERNETES_CLIENT_CERTIFICATE_DATA_ENV_VAR);
+        }
+
+        if (System.getenv(KUBERNETES_CLIENT_KEY_FILE_ENV_VAR) != null) {
+            File candidateClientKeyFile = new File(System.getenv(KUBERNETES_CLIENT_KEY_FILE_ENV_VAR));
+            if (candidateClientKeyFile.exists() && candidateClientKeyFile.canRead()) {
+                this.clientKeyFile = candidateClientKeyFile;
+            } else {
+                log.error("Specified client key file {} does not exist or is not readable", candidateClientKeyFile);
+            }
+        }
+
+        if (System.getenv(KUBERNETES_CLIENT_KEY_DATA_ENV_VAR) != null) {
+            this.clientKeyData = System.getenv(KUBERNETES_CLIENT_KEY_DATA_ENV_VAR);
+        }
+
+        if (System.getenv(KUBERNETES_CLIENT_KEY_ALGO_ENV_VAR) != null) {
+            this.clientKeyAlgo = System.getenv(KUBERNETES_CLIENT_KEY_ALGO_ENV_VAR);
+        }
+
+        if (System.getenv(KUBERNETES_CLIENT_KEY_PASSWORD_ENV_VAR) != null) {
+            this.clientKeyPassword = System.getenv(KUBERNETES_CLIENT_KEY_PASSWORD_ENV_VAR).toCharArray();
+        }
+
+        if (System.getenv(KUBERNETES_USERNAME_ENV_VAR) != null) {
+            this.username = System.getenv(KUBERNETES_USERNAME_ENV_VAR);
+        }
+        if (System.getenv(KUBERNETES_PASSWORD_ENV_VAR) != null) {
+            this.password = System.getenv(KUBERNETES_PASSWORD_ENV_VAR);
         }
 
     }
@@ -154,8 +210,11 @@ public class KubernetesFactory {
         configureAuthDetails(webClient);
         if (trustAllCerts) {
             disableSslChecks(webClient);
-        } else if (caCertFile != null) {
+        } else if (caCertFile != null || caCertData != null) {
             configureCaCert(webClient);
+        }
+        if ((clientCertFile != null || clientCertData != null) && (clientKeyFile != null || clientKeyData != null)) {
+            configureClientCert(webClient);
         }
         return JAXRSClientFactory.fromClient(webClient, clientType);
     }
@@ -224,7 +283,7 @@ public class KubernetesFactory {
         if (verifyAddress) {
             try {
                 URL url = new URL(this.address);
-                if (KubernetesHelper.isServiceSsl(url.getHost(), url.getPort(), trustAllCerts)) {
+                if (KubernetesHelper.isServiceSsl(url.getHost(), url.getPort(), true)) {
                     this.address = "https://" + url.getHost() + ":" + url.getPort();
                 } else {
                     this.address = "http://" + url.getHost() + ":" + url.getPort();
@@ -298,11 +357,69 @@ public class KubernetesFactory {
         }
     }
 
+    private InputStream getInputStreamFromDataOrFile(String data, File file) throws FileNotFoundException {
+        if (data != null) {
+            return new ByteArrayInputStream(data.getBytes());
+        }
+        if (file != null) {
+            return new FileInputStream(file);
+        }
+        return null;
+    }
+
+    private void configureClientCert(WebClient webClient) {
+        try {
+            InputStream certInputStream = getInputStreamFromDataOrFile(clientCertData, clientCertFile);
+            CertificateFactory certFactory = CertificateFactory.getInstance("X509");
+            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(certInputStream);
+
+            InputStream keyInputStream = getInputStreamFromDataOrFile(clientKeyData, clientKeyFile);
+            PEMReader reader = new PEMReader(keyInputStream);
+            RSAPrivateCrtKeySpec keySpec = new PKCS1EncodedKeySpec(reader.getDerBytes()).getKeySpec();
+            KeyFactory kf = KeyFactory.getInstance(clientKeyAlgo);
+            RSAPrivateKey privKey = (RSAPrivateKey) kf.generatePrivate(keySpec);
+
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(null);
+
+            String alias = cert.getSubjectX500Principal().getName();
+            keyStore.setKeyEntry(alias, privKey, clientKeyPassword, new Certificate[] {cert});
+
+            KeyManagerFactory keyManagerFactory =
+                    KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, clientKeyPassword);
+
+            HTTPConduit conduit = WebClient.getConfig(webClient)
+                    .getHttpConduit();
+
+            TLSClientParameters params = conduit.getTlsClientParameters();
+
+            if (params == null) {
+                params = new TLSClientParameters();
+                conduit.setTlsClientParameters(params);
+            }
+
+            KeyManager[] existingKeyManagers = params.getKeyManagers();
+            KeyManager[] keyManagers;
+
+            if (existingKeyManagers == null || ArrayUtils.isEmpty(existingKeyManagers)) {
+                keyManagers = keyManagerFactory.getKeyManagers();
+            } else {
+                keyManagers = (KeyManager[]) ArrayUtils.addAll(existingKeyManagers, keyManagerFactory.getKeyManagers());
+            }
+
+            params.setKeyManagers(keyManagers);
+
+        } catch (Exception e) {
+            log.error("Could not create key manager for " + clientCertFile + " (" + clientKeyFile + ")", e);
+        }
+    }
+
     private void configureCaCert(WebClient webClient) {
         try {
-            FileInputStream pemFileStream = new FileInputStream(caCertFile);
+            InputStream pemInputStream = getInputStreamFromDataOrFile(caCertData, caCertFile);
             CertificateFactory certFactory = CertificateFactory.getInstance("X509");
-            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(pemFileStream);
+            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(pemInputStream);
 
             KeyStore trustStore = KeyStore.getInstance("JKS");
             trustStore.load(null);
