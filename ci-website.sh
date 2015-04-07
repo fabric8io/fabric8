2f42e677e2edc5da1386c443116e0c697391a3d5
James Strachan <james.strachan@gmail.com>
2015-04-07 12:17:53 +0100

    attempt at generating the website via circleci

--- ci-website.sh
+++ ci-website.sh
@@ -3,12 +3,22 @@
 echo ============================================================================
 echo Deploying fabric8 website
 echo ============================================================================
-rm -rf fabric8
+#rm -rf fabric8
 
-git clone git@github.com:fabric8io/fabric8.git fabric8 && \
-cd fabric8 && \
 cd website && \
-mvn clean scalate:sitegen && \
-mkdir target/sitegen/gitbook && \
-gitbook build ../docs --output=target/sitegen/gitbook && \
-mvn scalate:deploy
+npm install && \
+mvn clean && \
+mkdir -p target && \
+pushd target && \
+git clone -b gh-pages git@github.com:fabric8io/fabric8.git sitegen && \
+popd && \
+mvn scalate:sitegen && \
+mkdir -p target/sitegen/gitbook && \
+gitbook build ../docs --output=target/sitegen/guide && \
+pushd target/sitegen && \
+git add *  && \
+git commit -m "generated website" && \
+git push
+
+#gitbook build ../docs --output=target/sitegen/gitbook && \
+#mvn scalate:deploy
