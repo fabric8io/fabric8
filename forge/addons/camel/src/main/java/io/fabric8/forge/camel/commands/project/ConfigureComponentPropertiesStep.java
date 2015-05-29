9f5d64d1759bd3078cead72e20661c2beda458de
Claus Ibsen <claus.ibsen@gmail.com>
2015-05-29 15:36:46 +0200

    Force commands stuff

--- forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/ConfigureComponentPropertiesStep.java
+++ forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/ConfigureComponentPropertiesStep.java
@@ -34,13 +34,17 @@ import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
 import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.context.UINavigationContext;
 import org.jboss.forge.addon.ui.input.InputComponent;
 import org.jboss.forge.addon.ui.input.InputComponentFactory;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.result.NavigationResult;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
 import org.jboss.forge.addon.ui.wizard.UIWizardStep;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
@@ -63,6 +67,13 @@ public class ConfigureComponentPropertiesStep extends AbstractCamelProjectComman
     private DependencyResolver dependencyResolver;
 
     private List<InputComponent> inputs = new ArrayList<>();
+
+    @Override
+    public UICommandMetadata getMetadata(UIContext context) {
+        return Metadata.forCommand(ConfigureEditEndpointPropertiesStep.class).name(
+                "Camel: New Component").category(Categories.create(CATEGORY))
+                .description("Configure the component options to use");
+    }
 
     @Override
     public void initializeUI(UIBuilder builder) throws Exception {
