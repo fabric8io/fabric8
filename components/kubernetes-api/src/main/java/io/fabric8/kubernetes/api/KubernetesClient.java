117a66c55f63f74b0b3f3c2f0e281d170492a5bb
James Strachan <james.strachan@gmail.com>
2015-06-02 20:08:12 +0100

    use correct type for watching deployment configs

--- components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/KubernetesClient.java
+++ components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/KubernetesClient.java
@@ -1266,21 +1266,21 @@ public class KubernetesClient implements Kubernetes, KubernetesExtensions, Kuber
         return watchOpenShiftEntities("routes", namespace, labels, watcher, resourceVersion);
     }
 
-    public WebSocketClient watchDeploymentConfigs(Watcher<Route> watcher) throws Exception {
+    public WebSocketClient watchDeploymentConfigs(Watcher<DeploymentConfig> watcher) throws Exception {
         return watchDeploymentConfigs(null, watcher);
     }
 
-    public WebSocketClient watchDeploymentConfigs(Map<String, String> labels, Watcher<Route> watcher) throws Exception {
+    public WebSocketClient watchDeploymentConfigs(Map<String, String> labels, Watcher<DeploymentConfig> watcher) throws Exception {
         return watchDeploymentConfigs(getNamespace(), labels, watcher);
     }
 
-    public WebSocketClient watchDeploymentConfigs(String namespace, Map<String, String> labels, Watcher<Route> watcher) throws Exception {
+    public WebSocketClient watchDeploymentConfigs(String namespace, Map<String, String> labels, Watcher<DeploymentConfig> watcher) throws Exception {
         DeploymentConfigList currentList = getDeploymentConfigs(namespace);
         return watchDeploymentConfigs(namespace, labels, watcher,
                 currentList.getMetadata().getResourceVersion());
     }
 
-    public WebSocketClient watchDeploymentConfigs(String namespace, Map<String, String> labels, Watcher<Route> watcher, String resourceVersion) throws Exception {
+    public WebSocketClient watchDeploymentConfigs(String namespace, Map<String, String> labels, Watcher<DeploymentConfig> watcher, String resourceVersion) throws Exception {
         return watchOpenShiftEntities("deploymentconfigs", namespace, labels, watcher, resourceVersion);
     }
 
