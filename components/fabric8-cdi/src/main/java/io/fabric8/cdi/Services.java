c6f97ec523bdd5c16fd093ed50003d11d05fb695
Ioannis Canellos <iocanel@gmail.com>
2015-06-03 15:57:50 +0300

    Remove unused stuff from Services.

--- components/fabric8-cdi/src/main/java/io/fabric8/cdi/Services.java
+++ components/fabric8-cdi/src/main/java/io/fabric8/cdi/Services.java
@@ -31,9 +31,6 @@ import static io.fabric8.cdi.KubernetesHolder.KUBERNETES;
 public class Services {
 
     private static final String KUBERNETES_NAMESPACE = "KUBERNETES_NAMESPACE";
-    private static final String HOST_SUFFIX = "_SERVICE_HOST";
-    private static final String PORT_SUFFIX = "_SERVICE_PORT";
-    private static final String PROTO_SUFFIX = "_TCP_PROTO";
     public static final String DEFAULT_PROTO = "tcp";
     public static final String DEFAULT_NAMESPACE = "default";
 
@@ -70,22 +67,5 @@ public class Services {
             }
         }
         return endpoints;
-    }
-
-    public static String serviceToHost(String id) {
-        return Systems.getEnvVarOrSystemProperty(toEnvVariable(id + HOST_SUFFIX), "");
-    }
-
-    public static String serviceToPort(String id) {
-        return Systems.getEnvVarOrSystemProperty(toEnvVariable(id + PORT_SUFFIX), "");
-    }
-
-    public static String serviceToProtocol(String id, String servicePort) {
-        return Systems.getEnvVarOrSystemProperty(toEnvVariable(id + PORT_SUFFIX + "_" + servicePort + PROTO_SUFFIX), DEFAULT_PROTO);
-    }
-
-
-    public static String toEnvVariable(String str) {
-        return str.toUpperCase().replaceAll("-", "_");
     }
 }
