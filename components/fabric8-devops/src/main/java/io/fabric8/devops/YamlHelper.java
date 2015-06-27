20b0a562ceaf11958759f61b70770f67dd3f3770
James Strachan <james.strachan@gmail.com>
2015-06-27 16:57:12 +0100

    first spike of #4086 to add support for parsing a fabric8.yaml

---
+++ components/fabric8-devops/src/main/java/io/fabric8/devops/YamlHelper.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.devops;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ */
+public class YamlHelper {
+
+
+    public static String toYaml(Object dto) throws JsonProcessingException {
+        ObjectMapper mapper = createObjectMapper();
+        return mapper.writeValueAsString(dto);
+    }
+
+    /**
+     * Creates a configured Jackson object mapper for parsing YAML
+     */
+    public static ObjectMapper createObjectMapper() {
+        return new ObjectMapper(new YAMLFactory());
+    }
+
+    public static ProjectConfig parseProjectConfig(File file) throws IOException {
+        return parseYaml(file, ProjectConfig.class);
+    }
+
+    private static <T> T parseYaml(File file, Class<T> clazz) throws IOException {
+        ObjectMapper mapper = createObjectMapper();
+        return mapper.readValue(file, clazz);
+    }
+}
