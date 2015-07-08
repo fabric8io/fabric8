e6e8935e8cf897330f2083131520d060a2a52cd0
Claus Ibsen <claus.ibsen@gmail.com>
2015-07-08 14:11:10 +0200

    Updated lic header

--- components/cxf-utils/src/main/java/io/fabric8/utils/cxf/ExceptionResponseMapper.java
+++ components/cxf-utils/src/main/java/io/fabric8/utils/cxf/ExceptionResponseMapper.java
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
 package io.fabric8.utils.cxf;
 
 import io.fabric8.utils.IOHelpers;
@@ -8,6 +23,7 @@ import javax.ws.rs.core.Response;
 import java.io.InputStream;
 
 public class ExceptionResponseMapper implements ResponseExceptionMapper<Exception> {
+
     @Override
     public Exception fromResponse(Response response) {
         try {
@@ -22,8 +38,8 @@ public class ExceptionResponseMapper implements ResponseExceptionMapper<Exceptio
             message = "HTTP " + response.getStatus() + " " + message;
             return new WebApplicationException(message, response);
         } catch (Exception ex) {
-            return new Exception(
-                    "Could not deserialize server side exception: " + ex.getMessage());
+            return new Exception("Could not deserialize server side exception: " + ex.getMessage());
         }
     }
+
 }
