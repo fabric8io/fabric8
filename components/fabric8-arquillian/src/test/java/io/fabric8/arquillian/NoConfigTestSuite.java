35687690d65d6685dc3da19e92a60162ddb95cc8
Ioannis Canellos <iocanel@gmail.com>
2015-09-25 21:45:44 +0300

    Add kubernetes server mock tests, to test the fabric8 arquillian extension.

---
+++ components/fabric8-arquillian/src/test/java/io/fabric8/arquillian/NoConfigTestSuite.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2005-2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package io.fabric8.arquillian;
+
+import io.fabric8.arquillian.kubernetes.Constants;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.PodListBuilder;
+import io.fabric8.kubernetes.api.model.ReplicationControllerListBuilder;
+import io.fabric8.kubernetes.api.model.ServiceListBuilder;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.openshift.server.mock.OpenShiftMockServer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import java.io.IOException;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(ResourceInjection.class)
+public class NoConfigTestSuite {
+
+    private static final OpenShiftMockServer MOCK = new OpenShiftMockServer();
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        System.out.println("before suite");
+        MOCK.expectAndReturnAsJson("/api/v1/namespaces/arquillian", 200, new NamespaceBuilder()
+                .withNewMetadata()
+                .withName("arquillian")
+                .and().build());
+
+        MOCK.expectAndReturnAsJson("/api/v1/namespaces/arquillian/replicationcontrollers", 200, new ReplicationControllerListBuilder()
+                .addNewItem()
+                .withNewMetadata()
+                .withName("repl1")
+                .endMetadata()
+                .endItem()
+                .build());
+
+
+        MOCK.expectAndReturnAsJson("/api/v1/namespaces/arquillian/pods", 200, new PodListBuilder().addNewItem()
+                .withNewMetadata()
+                .withName("pod1")
+                .endMetadata()
+                .endItem()
+                .build());
+
+        MOCK.expectAndReturnAsJson("/api/v1/namespaces/arquillian/services", 200, new ServiceListBuilder()
+                .addNewItem()
+                .withNewMetadata()
+                .withName("service1")
+                .endMetadata()
+                .endItem()
+                .build());
+
+
+        MOCK.init();
+
+        String masterUrl = "http://localhost:" + MOCK.getServer().getPort();
+        System.setProperty(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY, masterUrl);
+        System.setProperty(Config.KUBERNETES_NAMESPACE_SYSTEM_PROPERTY, "arquillian");
+        System.setProperty(Constants.USE_EXISTING_NAMESPACE, "arquillian");
+        System.setProperty(Constants.LAZY_CREATE_NAMESPACE, "arquillian");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws IOException {
+        MOCK.getServer().shutdown();
+    }
+}
