e5f77e10f9649803eb661071151e1820deff0bb7
Ioannis Canellos <iocanel@gmail.com>
2015-09-26 13:26:01 +0300

    Add support for using fabric8-arquillian with externally provided client. Add tests that use fabric8-arquillian with mock client.

---
+++ components/fabric8-arquillian/src/test/java/io/fabric8/arquillian/client/mock/MockClientCreator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2005-2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package io.fabric8.arquillian.client.mock;
+
+import io.fabric8.arquillian.kubernetes.Configuration;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.api.model.PodListBuilder;
+import io.fabric8.kubernetes.api.model.ReplicationControllerList;
+import io.fabric8.kubernetes.api.model.ReplicationControllerListBuilder;
+import io.fabric8.kubernetes.api.model.ServiceListBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.mock.KubernetesMockClient;
+import org.jboss.arquillian.core.api.InstanceProducer;
+import org.jboss.arquillian.core.api.annotation.ApplicationScoped;
+import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.arquillian.core.api.annotation.Observes;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MockClientCreator {
+
+    @Inject
+    @ApplicationScoped
+    private InstanceProducer<KubernetesClient> kubernetes;
+
+    public void createClient(@Observes Configuration config) throws MalformedURLException {
+        KubernetesMockClient mock = new KubernetesMockClient();
+
+        mock.getMasterUrl().andReturn(new URL("http://mock.client:80")).anyTimes();
+
+        mock.namespaces().withName("arquillian").get().andReturn(new NamespaceBuilder()
+                .withNewMetadata()
+                .withName("arquillian")
+                .endMetadata()
+                .build());
+
+        mock.replicationControllers().inNamespace("arquillian").list().andReturn(new ReplicationControllerListBuilder().build());
+        mock.pods().inNamespace("arquillian").list().andReturn(new PodListBuilder().build());
+        mock.services().inNamespace("arquillian").list().andReturn(new ServiceListBuilder().build());
+
+        kubernetes.set(mock.replay());
+    }
+}
