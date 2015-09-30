10654e46f08971b415116e2c0bcd7c29919420e0
James Strachan <james.strachan@gmail.com>
2015-09-30 10:45:19 +0100

    lets repload and re-execute the forge wizard to make sure everything's warmed up nicely

--- forge/fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/ForgeInitialiser.java
+++ forge/fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/ForgeInitialiser.java
@@ -18,14 +18,17 @@ package io.fabric8.forge.rest.main;
 import io.fabric8.forge.rest.CommandsResource;
 import io.fabric8.forge.rest.dto.CommandInfoDTO;
 import io.fabric8.forge.rest.dto.ExecutionRequest;
+import io.fabric8.forge.rest.hooks.CommandCompletePostProcessor;
 import io.fabric8.forge.rest.producer.FurnaceProducer;
 import org.apache.deltaspike.core.api.config.ConfigProperty;
+import org.jboss.forge.furnace.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +42,7 @@ import java.util.Map;
 @javax.ejb.Startup
 public class ForgeInitialiser {
     private static final transient Logger LOG = LoggerFactory.getLogger(ForgeInitialiser.class);
+    public static final String DEFAULT_ARCHETYPES_VERSION = "2.2.34";
 
     /**
      * @param addOnDir the directory where Forge addons will be stored
@@ -65,24 +69,64 @@ public class ForgeInitialiser {
 
         // lets try preload the archetypes
         LOG.info("Preloading archetypes");
+        String tempDir = "/tmp/startupNewProject";
+        try {
+            File file = File.createTempFile("startupNewProject", "tmp");
+            file.delete();
+            file.mkdirs();
+            tempDir = file.getPath();
+        } catch (IOException e) {
+            LOG.error("Failed to create temp directory: " + e, e);
+        }
+        LOG.info("Preloaded archetypes!");
+
         ExecutionRequest executionRequest = new ExecutionRequest();
-        List<Map<String, String>> inputList = new ArrayList<>();
         Map<String, String> step1Inputs = new HashMap<>();
         step1Inputs.put("buildSystem", "Maven");
-        step1Inputs.put("named", "dummy");
-        step1Inputs.put("targetLocation", "/opt/jboss");
+        String projectName = "dummy";
+        step1Inputs.put("named", projectName);
+        step1Inputs.put("targetLocation", tempDir);
         step1Inputs.put("topLevelPackage", "org.example");
         step1Inputs.put("type", "From Archetype Catalog");
         step1Inputs.put("version", "1.0.0-SNAPSHOT");
 
+
+        Map<String, String> step2Inputs = new HashMap<>();
+        step2Inputs.put("catalog", "fabric8");
+        step2Inputs.put("archetype", "io.fabric8.archetypes:java-camel-cdi-archetype:" + getArchetypesVersion());
+
+        List<Map<String, String>> inputList = new ArrayList<>();
         inputList.add(step1Inputs);
+        inputList.add(step2Inputs);
         executionRequest.setInputList(inputList);
-        executionRequest.setWizardStep(1);
+        executionRequest.setWizardStep(2);
+        UserDetails userDetails = new UserDetails("someAddress", "dummyUser", "dummyPassword", "dummy@doesNotExist.com");
         try {
-            commandsResource.executeCommand("project-new", executionRequest);
+            LOG.info("Now trying to create a new project in: " + tempDir);
+            CommandCompletePostProcessor postProcessor = null;
+            commandsResource.doExecute("project-new", executionRequest, postProcessor, userDetails, commandsResource.createUIContext(new File(tempDir)));
+
+            LOG.info("Created project!");
+            LOG.info("Now lets try validate the devops-edit command");
+            executionRequest = new ExecutionRequest();
+            step1Inputs = new HashMap<>();
+            inputList = new ArrayList<>();
+            inputList.add(step1Inputs);
+            executionRequest.setInputList(inputList);
+            executionRequest.setWizardStep(1);
+
+            commandsResource.doExecute("devops-edit", executionRequest, postProcessor, userDetails, commandsResource.createUIContext(new File(tempDir, projectName)));
+            LOG.info("Validated!");
         } catch (Exception e) {
             LOG.error("Failed to execute command: " + e, e);
         }
-        LOG.info("Preloaded archetypes!");
+    }
+
+    protected String getArchetypesVersion() {
+        String answer = System.getenv("FABRIC8_ARCHETYPES_VERSION");
+        if (Strings.isNullOrEmpty(answer)) {
+            return DEFAULT_ARCHETYPES_VERSION;
+        }
+        return answer;
     }
 }
