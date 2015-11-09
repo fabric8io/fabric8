1edd0d90a4241bb6b98f257d08d1df7a1d9bf8de
Ioannis Canellos <iocanel@gmail.com>
2015-11-09 17:31:01 +0200

    Throw a more reasonable error when there are argument missmatches on @Factory methods.

---
+++ components/fabric8-cdi/src/test/java/io/fabric8/cdi/weld/internal/brokenfactory/BrokenFactoryInternalTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2005-2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package io.fabric8.cdi.weld.internal.brokenfactory;
+
+import org.hamcrest.CoreMatchers;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.matchers.ThrowableMessageMatcher;
+import org.junit.rules.ExpectedException;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+
+public class BrokenFactoryInternalTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testServiceInjection() {
+        expectedException.expect(ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Failed to process @Factory annotated method")));
+        createInstance(MyBean.class);
+    }
+
+
+    void createInstance(Class type) {
+        WeldContainer weld = new Weld().initialize();
+        CreationalContext ctx = weld.getBeanManager().createCreationalContext(null);
+        for (Bean bean : weld.getBeanManager().getBeans(type)) {
+             weld.getBeanManager().getReference(bean, type, ctx);
+        }
+    }
+
+}
