49cee3d7336aca8ba7d6899f7dfa78be8686d057
Claus Ibsen <claus.ibsen@gmail.com>
2015-11-28 11:44:55 +0100

    First spike of #5324. Camel forge - Scan for endpoints in route builder configure methods

---
+++ forge/addons/camel/src/test/java/io/fabric8/forge/camel/RoasterRouteBuilderConfigureTest.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.forge.camel;
+
+import java.io.File;
+import java.util.List;
+import java.util.ServiceLoader;
+
+import io.fabric8.forge.camel.commands.project.helper.CamelJavaParserHelper;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.MethodSource;
+import org.jboss.forge.roaster.spi.JavaParser;
+import org.junit.Ignore;
+
+@Ignore
+public class RoasterRouteBuilderConfigureTest {
+
+    public static void main(String[] args) throws Exception {
+        RoasterRouteBuilderConfigureTest me = new RoasterRouteBuilderConfigureTest();
+        me.parse();
+    }
+
+    public void parse() throws Exception {
+        JavaClassSource clazz = (JavaClassSource) Roaster.parse(new File("src/test/java/io/fabric8/forge/camel/MyRouteBuilder.java"));
+        MethodSource<JavaClassSource> method = clazz.getMethod("configure");
+
+        List<String> list = CamelJavaParserHelper.parseCamelUris(method);
+        for (String uri : list) {
+            System.out.println(uri);
+        }
+    }
+
+}
