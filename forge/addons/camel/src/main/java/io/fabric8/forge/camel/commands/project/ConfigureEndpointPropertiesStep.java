a0b06a34ac9d4b24ea87301ae71b5a4920e09c3d
Claus Ibsen <claus.ibsen@gmail.com>
2015-12-11 10:00:21 +0100

    #5355: camel-edit-endpoint should not reformat the source code.

--- forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/ConfigureEndpointPropertiesStep.java
+++ forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/ConfigureEndpointPropertiesStep.java
@@ -1,20 +1,21 @@
 /**
- *  Copyright 2005-2015 Red Hat, Inc.
- *
- *  Red Hat licenses this file to you under the Apache License, version
- *  2.0 (the "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied.  See the License for the specific language governing
- *  permissions and limitations under the License.
+ * Copyright 2005-2015 Red Hat, Inc.
+ * <p/>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 package io.fabric8.forge.camel.commands.project;
 
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -433,17 +434,18 @@ public class ConfigureEndpointPropertiesStep extends AbstractCamelProjectCommand
         } else {
             MethodSource<JavaClassSource> method = CamelJavaParserHelper.findConfigureMethod(clazz);
             if (method != null) {
+
+                // we do not want to change the current code formatting so we need to search
+                // replace the unformatted class soure code
+                String code = clazz.toUnformattedString();
+
                 String endpointUrl = mandatoryAttributeValue(attributeMap, "endpointUri");
-                String body = method.getBody();
                 // wonder if we should have better way, than a replaceFirst?
                 String find = Pattern.quote(endpointUrl);
-                body = body.replaceFirst(find, uri);
-                method.setBody(body);
+                code = code.replaceFirst(find, uri);
 
-                // TODO: avoid formatting source code
-
-
-                facet.saveJavaSource(clazz);
+                // use this code currently to save content unformatted
+                existing.setContents(new ByteArrayInputStream(code.getBytes()), null);
 
                 return Results.success("Updated endpoint " + endpointUrl + " -> " + uri + " in " + routeBuilder);
             }
