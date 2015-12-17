d2cb2f34c5e63e935c0398af5afe991e8f041c2e
Claus Ibsen <claus.ibsen@gmail.com>
2015-12-17 15:26:16 +0100

    #5478: Firt cut of ComponentDto for richer component picker in camel forge commands

--- forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/CamelNewComponentInstanceSpringCommand.java
+++ forge/addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/CamelNewComponentInstanceSpringCommand.java
@@ -22,8 +22,10 @@ import javax.inject.Inject;
 import io.fabric8.forge.addon.utils.completer.PackageNameCompleter;
 import io.fabric8.forge.addon.utils.validator.ClassNameValidator;
 import io.fabric8.forge.addon.utils.validator.PackageNameValidator;
+import io.fabric8.forge.camel.commands.project.dto.ComponentDto;
 import io.fabric8.forge.camel.commands.project.helper.CamelCatalogHelper;
 import io.fabric8.forge.camel.commands.project.helper.CamelCommandsHelper;
+import org.jboss.forge.addon.convert.Converter;
 import org.jboss.forge.addon.dependencies.DependencyResolver;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
@@ -51,6 +53,8 @@ import org.jboss.forge.addon.ui.util.Metadata;
 import org.jboss.forge.addon.ui.wizard.UIWizard;
 import org.jboss.forge.roaster.model.util.Strings;
 
+import static io.fabric8.forge.camel.commands.project.helper.CamelCatalogHelper.createComponentDto;
+
 @FacetConstraint({JavaSourceFacet.class, ResourcesFacet.class, ClassLoaderFacet.class})
 public class CamelNewComponentInstanceSpringCommand extends AbstractCamelProjectCommand implements UIWizard {
 
@@ -60,7 +64,7 @@ public class CamelNewComponentInstanceSpringCommand extends AbstractCamelProject
 
     @Inject
     @WithAttributes(label = "Component Name", required = true, description = "Name of component type to add")
-    private UISelectOne<String> componentName;
+    private UISelectOne<ComponentDto> componentName;
 
     @Inject
     @WithAttributes(label = "Instance Name", required = true, description = "Name of component instance to add")
@@ -103,7 +107,13 @@ public class CamelNewComponentInstanceSpringCommand extends AbstractCamelProject
 
         componentNameFilter.setValueChoices(CamelCommandsHelper.createComponentLabelValues(project));
         componentNameFilter.setDefaultValue("<all>");
-        componentName.setValueChoices(CamelCommandsHelper.createComponentNameValues(project, componentNameFilter, false));
+        componentName.setValueChoices(CamelCommandsHelper.createComponentDtoValues(project, componentNameFilter, false));
+        componentName.setValueConverter(new Converter<String, ComponentDto>() {
+            @Override
+            public ComponentDto convert(String name) {
+                return createComponentDto(name);
+            }
+        });
         // show note about the chosen component
         componentName.addValueChangeListener(new ValueChangeListener() {
             @Override
@@ -137,7 +147,7 @@ public class CamelNewComponentInstanceSpringCommand extends AbstractCamelProject
     @Override
     public NavigationResult next(UINavigationContext context) throws Exception {
         Map<Object, Object> attributeMap = context.getUIContext().getAttributeMap();
-        attributeMap.put("componentName", componentName.getValue());
+        attributeMap.put("componentName", componentName.getValue().getScheme());
         attributeMap.put("instanceName", instanceName.getValue());
         attributeMap.put("targetPackage", targetPackage.getValue());
         attributeMap.put("className", className.getValue());
