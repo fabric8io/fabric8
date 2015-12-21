e6834de5b8404523b9e0caef36a0427a053fa751
James Strachan <james.strachan@gmail.com>
2015-12-21 16:02:43 +0000

    fix bad logging message

--- forge/fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/UserDetails.java
+++ forge/fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/UserDetails.java
@@ -97,7 +97,7 @@ public class UserDetails {
 
             @Override
             public String toString() {
-                return "UsernamePasswordCredentialsProvider{user: " + getUser() + ", password length: " + (Strings.isNotBlank(password) ? 0 : password.length()) + "}";
+                return "UsernamePasswordCredentialsProvider{user: " + getUser() + ", password length: " + (Strings.isNullOrBlank(password) ? 0 : password.length()) + "}";
             }
         };
     }
