9bb3309a59456c16bf950eb43a08e223267532db
Claus Ibsen <claus.ibsen@gmail.com>
2015-12-31 10:06:54 +0100

    #5411 camel-maven-plugin to validate java route builders. Fixed license headers.

--- forge/addons/camel-maven-plugin/src/main/java/io/fabric8/forge/camel/maven/EndpointMojo.java
+++ forge/addons/camel-maven-plugin/src/main/java/io/fabric8/forge/camel/maven/EndpointMojo.java
@@ -17,31 +17,88 @@
 package io.fabric8.forge.camel.maven;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
-//import org.apache.maven.plugin.AbstractMojo;
-//import org.apache.maven.plugin.MojoExecutionException;
-//import org.apache.maven.plugin.MojoFailureException;
-//import org.apache.maven.plugins.annotations.LifecyclePhase;
-//import org.apache.maven.plugins.annotations.Mojo;
-//import org.apache.maven.plugins.annotations.Parameter;
-//import org.apache.maven.plugins.annotations.ResolutionScope;
-//import org.apache.maven.project.MavenProject;
-import org.jboss.forge.addon.projects.Project;
-import org.jboss.forge.addon.projects.ProjectFactory;
-import org.jboss.forge.addon.resource.Resource;
-import org.jboss.forge.addon.resource.ResourceFactory;
+import io.fabric8.forge.camel.commands.project.helper.RouteBuilderParser;
+import io.fabric8.forge.camel.commands.project.model.CamelEndpointDetails;
+import org.apache.camel.catalog.CamelCatalog;
+import org.apache.camel.catalog.DefaultCamelCatalog;
+import org.apache.camel.catalog.EndpointValidationResult;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
 
 /**
  * Analyses the project source code for Camel routes, and validates the endpoint uris whether there may be invalid uris.
  */
-//@Mojo(name = "validate", defaultPhase = LifecyclePhase.PROCESS_CLASSES,
-//        requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
-public class EndpointMojo { }/*extends AbstractMojo {
+@Mojo(name = "validate", defaultPhase = LifecyclePhase.PROCESS_CLASSES,
+        requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+public class EndpointMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = false)
     private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        CamelCatalog catalog = new DefaultCamelCatalog();
+
+        // find all endpoints
+        final List<CamelEndpointDetails> endpoints = new ArrayList<>();
+
+        // find all route builder classes
+        final Set<File> javaFiles = new LinkedHashSet<File>();
+        for (String dir : project.getCompileSourceRoots()) {
+            findJavaFiles(new File(dir), javaFiles);
+            getLog().info("Found " + javaFiles.size() + " .java files");
+        }
+
+        for (File file : javaFiles) {
+            try {
+                getLog().info("Inspecting " + file + " .java file");
+
+                // parse the java source code and find Camel RouteBuilder classes
+                String fqn = file.getPath();
+                String baseDir = ".";
+                JavaClassSource clazz = (JavaClassSource) Roaster.parse(file);
+                if (clazz != null) {
+                    RouteBuilderParser.parseRouteBuilder(clazz, baseDir, fqn, endpoints);
+                }
+            } catch (Exception e) {
+                getLog().warn("Error parsing java file " + file + " code due " + e.getMessage());
+            }
+        }
+
+        getLog().info("Found " + endpoints.size() + " Camel endpoints");
+
+        for (CamelEndpointDetails detail : endpoints) {
+            EndpointValidationResult result = catalog.validateEndpointProperties(detail.getEndpointUri());
+            if (!result.isSuccess()) {
+                String out = result.summaryErrorMessage();
+                getLog().warn(out);
+            }
+        }
     }
-}*/
+
+    private void findJavaFiles(File dir, Set<File> javaFiles) {
+        File[] files = dir.isDirectory() ? dir.listFiles() : null;
+        if (files != null) {
+            for (File file : files) {
+                if (file.getName().endsWith(".java")) {
+                    javaFiles.add(file);
+                } else if (file.isDirectory()) {
+                    findJavaFiles(file, javaFiles);
+                }
+            }
+        }
+    }
+}
