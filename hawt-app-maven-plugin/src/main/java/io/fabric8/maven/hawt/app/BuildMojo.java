5f5577d028f5e2ab9780201a90384f8d564abeff
Hiram Chirino <hiram@hiramchirino.com>
2016-01-15 11:46:15 -0500

    Improved the hawt-app build mojo so that it only includes runtime scope artifacts in the lib classpath by default.

--- hawt-app-maven-plugin/src/main/java/io/fabric8/maven/hawt/app/BuildMojo.java
+++ hawt-app-maven-plugin/src/main/java/io/fabric8/maven/hawt/app/BuildMojo.java
@@ -15,6 +15,26 @@
  */
 package io.fabric8.maven.hawt.app;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilderException;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.tar.TarArchiver;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.StringUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,26 +43,8 @@ import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
-
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.dependency.fromDependencies.AbstractDependencyFilterMojo;
-import org.apache.maven.plugin.dependency.utils.DependencyStatusSets;
-import org.apache.maven.plugin.dependency.utils.filters.ResolveFileFilter;
-import org.apache.maven.plugin.dependency.utils.markers.SourcesFileMarkerHandler;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProjectHelper;
-import org.apache.maven.shared.artifact.filter.collection.ArtifactsFilter;
-import org.codehaus.plexus.archiver.Archiver;
-import org.codehaus.plexus.archiver.tar.TarArchiver;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.IOUtil;
-import org.codehaus.plexus.util.StringUtils;
 
 import static org.codehaus.plexus.archiver.util.DefaultFileSet.fileSet;
 
@@ -50,38 +52,55 @@ import static org.codehaus.plexus.archiver.util.DefaultFileSet.fileSet;
  * Builds a hawt app assembly.
  *
  */
-@Mojo( name = "build", requiresDependencyResolution = ResolutionScope.RUNTIME,
-       defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true )
-public class BuildMojo extends AbstractDependencyFilterMojo {
+@Mojo(name = "build", requiresDependencyResolution = ResolutionScope.TEST,
+        defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true)
+public class BuildMojo extends AbstractMojo {
+
+    /**
+     * The Maven project.
+     */
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    /**
+     * The dependency tree builder to use.
+     */
+    @Component(hint = "default")
+    private DependencyGraphBuilder dependencyGraphBuilder;
+
+    /**
+     * The scope to filter by when resolving the classpath of the application.
+     *
+     */
+    @Parameter(property = "scope", defaultValue = "runtime")
+    private String scope;
 
     /**
      * Directory of resources use to augment the files in the hawt-app archive.
      *
-     * @since 2.0
      */
-    @Parameter( property = "hawt-app.source",
-                defaultValue = "${basedir}/src/main/hawt-app" )
+    @Parameter(property = "hawt-app.source",
+            defaultValue = "${basedir}/src/main/hawt-app")
     protected File source;
 
     /**
      * Directory used to create the assembly.
      *
-     * @since 2.0
      */
-    @Parameter( property = "hawt-app.assembly",
-                defaultValue = "${project.build.directory}/hawt-app" )
+    @Parameter(property = "hawt-app.assembly",
+            defaultValue = "${project.build.directory}/hawt-app")
     protected File assembly;
 
-    @Parameter( property = "hawt-app.archive",
-                defaultValue = "${project.build.directory}/${project.artifactId}-${project.version}-app.tar.gz" )
+    @Parameter(property = "hawt-app.archive",
+            defaultValue = "${project.build.directory}/${project.artifactId}-${project.version}-app.tar.gz")
     protected File archive;
 
-    @Parameter( property = "hawt-app.archiveClassifier",
-                defaultValue = "app" )
+    @Parameter(property = "hawt-app.archiveClassifier",
+            defaultValue = "app")
     protected String archiveClassifier;
 
-    @Parameter( property = "hawt-app.archivePrefix",
-                defaultValue = "${project.artifactId}-${project.version}-app/" )
+    @Parameter(property = "hawt-app.archivePrefix",
+            defaultValue = "${project.artifactId}-${project.version}-app/")
     protected String archivePrefix;
 
     /**
@@ -90,17 +109,15 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
     @Parameter(property = "hawt-app.javaMainClass")
     protected String javaMainClass;
 
-    @Component(role= Archiver.class, hint = "tar")
+    @Component(role = Archiver.class, hint = "tar")
     protected Archiver archiver;
 
     // Used for attaching the archive to this artefact
     @Component
     private MavenProjectHelper projectHelper;
 
-    protected void doExecute() throws MojoExecutionException {
-
-        // get sets of dependencies
-        DependencyStatusSets results = this.getDependencySets(false, false);
+    public void execute()
+            throws MojoExecutionException, MojoFailureException {
 
         File libDir = new File(assembly, "lib");
         libDir.mkdirs();
@@ -110,8 +127,17 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
 
         ArrayList<String> classpath = new ArrayList<String>();
 
+        // get sets of dependencies
+        ArrayList<Artifact> artifacts = null;
+        try {
+            artifacts = collectClassPath();
+        } catch (DependencyGraphBuilderException e) {
+            throw new MojoExecutionException("Could not get classpath", e);
+        }
+        System.out.println(artifacts);
+
         // Lets first copy this project's artifact.
-        if( project.getArtifact().getFile()!=null ) {
+        if (project.getArtifact().getFile() != null) {
             File target = new File(libDir, project.getArtifact().getFile().getName());
             classpath.add(target.getName());
             try {
@@ -121,8 +147,17 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
             }
         }
 
+        // Artifacts in this map point to resolved files.
+        Map artifactMap = project.getArtifactMap();
+
         // Lets then copy the it's dependencies.
-        for (Artifact artifact : results.getResolvedDependencies()) {
+        for (Artifact x : artifacts) {
+
+            // x is not resolved, so lets look it up in the map.
+            Artifact artifact = (Artifact) artifactMap.get(ArtifactUtils.versionlessKey(x));
+            if( artifact==null || artifact.getFile() == null ) {
+                continue;
+            }
             File file = artifact.getFile().getAbsoluteFile();
             try {
 
@@ -131,13 +166,13 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
                 // just in case we run into an lib name collision, lets
                 // find a non-colliding target name
                 int dupCounter = 1;
-                while( classpath.contains(target.getName()) ) {
-                    target = new File(libDir, "dup"+dupCounter+"-"+file.getName());
+                while (classpath.contains(target.getName())) {
+                    target = new File(libDir, "dup" + dupCounter + "-" + file.getName());
                     dupCounter++;
                 }
 
                 classpath.add(target.getName());
-                FileUtils.copyFile( artifact.getFile(), target);
+                FileUtils.copyFile(artifact.getFile(), target);
 
             } catch (IOException e) {
                 throw new MojoExecutionException("Could not copy artifact to lib dir", e);
@@ -160,7 +195,7 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
         copyResource("bin/run.sh", targetRun, interpolations);
         chmodExecutable(targetRun);
 
-        if( source!=null && source.exists() ) {
+        if (source != null && source.exists()) {
             try {
                 FileUtils.copyDirectoryStructure(source, assembly);
             } catch (IOException e) {
@@ -168,7 +203,7 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
             }
         }
 
-        ((TarArchiver)archiver).setCompression(TarArchiver.TarCompressionMethod.gzip);
+        ((TarArchiver) archiver).setCompression(TarArchiver.TarCompressionMethod.gzip);
         archiver.setDestFile(archive);
         archiver.addFileSet(fileSet(assembly).prefixed(archivePrefix).includeExclude(null, new String[]{"bin/*"}).includeEmptyDirs(true));
         archiver.setFileMode(0755);
@@ -176,7 +211,7 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
         try {
             archiver.createArchive();
         } catch (IOException e) {
-            throw new MojoExecutionException("Could not create the "+archive+" file", e);
+            throw new MojoExecutionException("Could not create the " + archive + " file", e);
         }
         projectHelper.attachArtifact(project, "tar.gz", archiveClassifier, archive);
     }
@@ -193,14 +228,14 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
 
         try {
             String content = loadTextResource(getClass().getResource(source));
-            if( interpolations!=null ) {
+            if (interpolations != null) {
                 content = StringUtils.interpolate(content, interpolations);
             }
             // Safety check
             content = content.replaceAll("\\r?\\n", Matcher.quoteReplacement("\n"));
             FileUtils.fileWrite(target, content);
         } catch (IOException e) {
-            throw new MojoExecutionException("Could create the "+target+" file", e);
+            throw new MojoExecutionException("Could create the " + target + " file", e);
         }
     }
 
@@ -213,7 +248,23 @@ public class BuildMojo extends AbstractDependencyFilterMojo {
         }
     }
 
-    protected ArtifactsFilter getMarkedArtifactFilter() {
-        return new ResolveFileFilter(new SourcesFileMarkerHandler(this.markersDirectory));
+    private ArrayList<Artifact> collectClassPath() throws DependencyGraphBuilderException {
+        ArtifactFilter filter = new ScopeArtifactFilter(scope);
+        DependencyNode rootNode = dependencyGraphBuilder.buildDependencyGraph(project, filter);
+        final ArrayList<Artifact> artifacts = new ArrayList<>();
+        rootNode.accept(new DependencyNodeVisitor() {
+            @Override
+            public boolean visit(DependencyNode dependencyNode) {
+                artifacts.add(dependencyNode.getArtifact());
+                return true;
+            }
+
+            @Override
+            public boolean endVisit(DependencyNode dependencyNode) {
+                return true;
+            }
+        });
+        return artifacts;
     }
+
 }
