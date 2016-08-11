53f00cea46f34eaf777534c7b48eb1b78cb67e08
James Strachan <james.strachan@gmail.com>
2016-08-11 18:52:12 +0100

    lets migrate OpenShift template parameters over to use ConfigMap #6240

--- fabric8-maven-plugin/src/main/java/io/fabric8/maven/MigrateMojo.java
+++ fabric8-maven-plugin/src/main/java/io/fabric8/maven/MigrateMojo.java
@@ -17,7 +17,12 @@ package io.fabric8.maven;
 
 import io.fabric8.kubernetes.api.Annotations;
 import io.fabric8.kubernetes.api.KubernetesHelper;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.EnvVarSource;
+import io.fabric8.kubernetes.api.model.EnvVarSourceBuilder;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.PodSpec;
@@ -41,6 +46,7 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -78,6 +84,8 @@ public class MigrateMojo extends AbstractFabric8Mojo {
 
                 Set<HasMetadata> entities = new TreeSet<>(new HasMetadataComparator());
 
+                ConfigMap parameterConfigMap = null;
+
                 if (dto instanceof Template) {
                     Template template = (Template) dto;
 
@@ -87,6 +95,21 @@ public class MigrateMojo extends AbstractFabric8Mojo {
                     }
 
                     List<io.fabric8.openshift.api.model.Parameter> parameters = template.getParameters();
+                    if (parameters != null && parameters.size() > 0) {
+                        parameterConfigMap = new ConfigMapBuilder().withNewMetadataLike(template.getMetadata()).endMetadata().build();
+
+                        Map<String, String> configMapData = new HashMap<>();
+                        for (io.fabric8.openshift.api.model.Parameter parameter : parameters) {
+                            String name = parameter.getName();
+                            String value = parameter.getValue();
+                            configMapData.put(convertToConfigMapKey(name), value);
+                        }
+                        parameterConfigMap.setData(configMapData);
+
+                        migrateEntity(parameterConfigMap, parameterConfigMap);
+                        entities.add(parameterConfigMap);
+                    }
+
                     // TODO do something with the parameters!
                 } else {
                     entities.addAll(KubernetesHelper.toItemList(dto));
@@ -95,7 +118,7 @@ public class MigrateMojo extends AbstractFabric8Mojo {
 
                 //Apply all items
                 for (HasMetadata entity : entities) {
-                    entity = migrateEntity(entity);
+                    entity = migrateEntity(entity, parameterConfigMap);
                     String name = KubernetesHelper.getName(entity);
                     String kind = shortenKind(KubernetesHelper.getKind(entity).toLowerCase());
 
@@ -110,6 +133,10 @@ public class MigrateMojo extends AbstractFabric8Mojo {
         }
     }
 
+    protected String convertToConfigMapKey(String name) {
+        return name.toLowerCase().replace('_', '-');
+    }
+
     /**
      * Returns the migrated entity
      *
@@ -117,7 +144,7 @@ public class MigrateMojo extends AbstractFabric8Mojo {
      * - remove some annotations which should be generated at real build time
      * - replace groupId, artifactId and version with expressions
      */
-    protected HasMetadata migrateEntity(HasMetadata entity) {
+    protected HasMetadata migrateEntity(HasMetadata entity, ConfigMap parameterConfigMap) {
         migrateMetadata(entity.getMetadata());
 
         if (entity instanceof ReplicationController) {
@@ -130,6 +157,11 @@ public class MigrateMojo extends AbstractFabric8Mojo {
                 DeploymentSpec deploymentSpec = new DeploymentSpec();
 
                 Map<String, String> selector = rcSpec.getSelector();
+                if (selector != null) {
+                    selector = new LinkedHashMap<>(selector);
+                    // Deployment's selector should not have a version as we use that for each RC / RS
+                    selector.remove("version");
+                }
                 migrateLabels(selector);
                 deploymentSpec.setReplicas(rcSpec.getReplicas());
                 if (selector != null) {
@@ -142,7 +174,7 @@ public class MigrateMojo extends AbstractFabric8Mojo {
                         List<Container> containers = podSpec.getContainers();
                         if (containers != null) {
                             for (Container container : containers) {
-                                migrate(container);
+                                migrateContainer(container, parameterConfigMap);
                             }
                         }
                     }
@@ -160,7 +192,7 @@ public class MigrateMojo extends AbstractFabric8Mojo {
         return entity;
     }
 
-    protected void migrate(Container container) {
+    protected void migrateContainer(Container container, ConfigMap parameterConfigMap) {
         String image = container.getImage();
         if (image != null) {
             MavenProject project = getProject();
@@ -172,6 +204,30 @@ public class MigrateMojo extends AbstractFabric8Mojo {
                     container.setImage(image);
                 } else {
                     getLog().warn("Image does not end with " + label + " as image is: " + image);
+                }
+            }
+        }
+        if (parameterConfigMap != null) {
+            Map<String, String> parameters = parameterConfigMap.getData();
+            if (parameters != null) {
+                String configMapName = KubernetesHelper.getName(parameterConfigMap);
+                List<EnvVar> env = container.getEnv();
+                for (EnvVar envVar : env) {
+                    String value = envVar.getValue();
+                    if (value != null) {
+                        String name = envVar.getName();
+                        if (value.startsWith("${") && value.endsWith("}")) {
+                            String variableName = Strings.stripPrefix(Strings.stripSuffix(name, "}"), "${");
+                            String expression = convertToConfigMapKey(variableName);
+                            if (parameters.containsKey(expression)) {
+                                // lets switch to a refer to the config map!
+                                envVar.setValue(null);
+                                envVar.setValueFrom(new EnvVarSourceBuilder().
+                                        withNewConfigMapKeyRef().withName(configMapName).withKey(expression).endConfigMapKeyRef().build());
+                            }
+
+                        }
+                    }
                 }
             }
         }
@@ -195,7 +251,18 @@ public class MigrateMojo extends AbstractFabric8Mojo {
     private void migrateLabels(Map<String, String> labels) {
         if (labels != null) {
             MavenProject project = getProject();
-            replaceKeyValueWith(labels, "project", project.getArtifactId(), "${project.artifactId}");
+
+            // TODO there is a currently a bug when using values other than ${project.artifactId} in fabric8-maven-plugin 3.x
+            // where it overrides the value of the sepc.selector.matchLabels to the artifactId which then breaks
+            // as the template.metadata.labels.project will differ
+            boolean alwaysUseArtifactIdForProject = true;
+            if (alwaysUseArtifactIdForProject) {
+                if (labels.containsKey("project")) {
+                    labels.put("project", "${project.artifactId}");
+                }
+            } else {
+                replaceKeyValueWith(labels, "project", project.getArtifactId(), "${project.artifactId}");
+            }
             replaceKeyValueWith(labels, "version", project.getVersion(), "${project.version}");
         }
     }
@@ -208,6 +275,7 @@ public class MigrateMojo extends AbstractFabric8Mojo {
     }
 
     private void init() {
+        kindAliases.put("configmap", "cm");
         kindAliases.put("deploymentconfig", "dc");
         kindAliases.put("replicationcontroller", "rc");
         kindAliases.put("replicaset", "rs");
