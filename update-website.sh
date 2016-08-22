d68c60f5de662a2b9c6d1af6ac3c46dfc70e63b4
James Strachan <james.strachan@gmail.com>
2016-08-22 12:08:43 +0100

    lets put helm in a folder

--- update-website.sh
+++ update-website.sh
@@ -8,7 +8,7 @@ cd target && \
 git clone -b gh-pages git@github.com:fabric8io/fabric8.git website && \
 cd website && \
 mkdir -p helm && \
-cp ../fabric8/helm-index.yaml index.yaml && \
+cp ../fabric8/helm-index.yaml helm/index.yaml && \
 git add *.yaml && \
 git commit -m "updated helm index" && \
 git push origin gh-pages
