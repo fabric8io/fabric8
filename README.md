9d0a03225e2a7073eaa94652e0d6d3934030a26c
James Strachan <james.strachan@gmail.com>
2016-08-22 12:05:38 +0100

    added helm index build

---
+++ README.md
@@ -0,0 +1,2 @@
+# fabric8-helm-index-build
+CI build to generate the helm index for fabric8 projects
---
+++ update-website.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ============================================================================
+echo Copying the index.yaml to the website
+echo ============================================================================
+
+cd target && \
+git clone -b gh-pages git@github.com:fabric8io/fabric8.git website && \
+cd website && \
+mkdir -p helm && \
+cp ../fabric8/helm-index.yaml index.yaml && \
+git add *.yaml && \
+git commit -m "updated helm index" && \
+git push origin gh-pages
+
+echo ============================================================================
+echo Deployed new helm index to the website
+echo ============================================================================
+
