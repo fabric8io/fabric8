7b5bfd407676f5ea37a9aa763734b1f56a1a318b
James Strachan <james.strachan@gmail.com>
2016-09-07 10:27:54 +0100

    lets try remove the old index in case we get issues trying to clone the repo

--- update-website.sh
+++ update-website.sh
@@ -5,13 +5,13 @@ echo Copying the index.yaml to the website
 echo ============================================================================
 
 cd target && \
+rm -f website/helm/index.* && \    
 git clone -b gh-pages git@github.com:fabric8io/fabric8.git website && \
 cd website && \
 mkdir -p helm && \
 cp ../fabric8/helm-index.yaml helm/index.yaml && \
 cp ../fabric8/helm-index.html helm/index.html && \
-git add *.yaml && \
-git add *.html && \    
+git helm/* && \
 git commit -m "updated helm index" && \
 git push origin gh-pages
 
