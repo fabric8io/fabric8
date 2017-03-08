bd6ad01577692d694915de722f908fa4662ebb5f
James Strachan <james.strachan@gmail.com>
2017-03-08 15:24:00 +0000

    lets add a helper function for converting names to valid kubernetes resource names (optionally excluding dots which are not supported for things like secret names)

---
+++ components/kubernetes-api/src/main/java/io/fabric8/kubernetes/api/KubernetesNames.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.kubernetes.api;
+
+/**
+ */
+public class KubernetesNames {
+    /**
+     * Lets convert the string to btw a valid kubernetes resource name
+     */
+    static String convertToKubernetesName(String text, boolean allowDots) {
+      String lower = text.toLowerCase();
+      StringBuilder builder = new StringBuilder();
+      boolean started = false;
+      char lastCh = ' ';
+      for (int i = 0, last = lower.length() - 1; i <= last; i++) {
+        char ch = lower.charAt(i);
+        if (!(ch >= 'a' && ch <= 'z') && !(ch >= '0' && ch <= '9')) {
+          if (ch == '/') {
+            ch = '.';
+          } else if (ch != '.' && ch != '-') {
+            ch = '-';
+          }
+          if (!allowDots && ch == '.') {
+              ch = '-';
+          }
+          if (!started || lastCh == '-' || lastCh == '.' || i == last) {
+            continue;
+          }
+        }
+        builder.append(ch);
+        started = true;
+        lastCh = ch;
+      }
+      return builder.toString();
+    }
+
+}
